e1c1d89f53d36e89728749e39878527a1d19a547
johnsoncodehk <johnsoncodehk@gmail.com>
2021-01-09 03:25:03 +0800

    fix: require typescript by unix path

--- packages/vscode-builtin-packages/src/index.ts
+++ packages/vscode-builtin-packages/src/index.ts
@@ -1,7 +1,10 @@
+import * as path from 'upath';
+
 let typescript: typeof import('typescript');
 
 export function setTypescript(appRoot: string) {
-    typescript = require(appRoot + '/extensions/node_modules/typescript');
+    const tsPath = path.join(appRoot, 'extensions', 'node_modules', 'typescript');
+    typescript = require(path.toUnix(tsPath));
 }
 export function getTypescript() {
     return typescript;
