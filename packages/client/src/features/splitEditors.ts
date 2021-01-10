4d34db5c6829054091560392881259ec2ce03116
johnsoncodehk <johnsoncodehk@gmail.com>
2021-01-11 00:39:29 +0800

    feat: improve split editors

--- packages/client/src/features/splitEditors.ts
+++ packages/client/src/features/splitEditors.ts
@@ -1,35 +1,47 @@
 import * as vscode from 'vscode';
 import { parse, SFCBlock } from '@vue/compiler-sfc';
+import { notEmpty } from '@volar/shared';
 
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand('volar.action.splitEditors', async _ => {
+
         const editor = vscode.window.activeTextEditor;
         if (!editor) return;
+
         const doc = editor.document;
         const descriptor = parse(doc.getText()).descriptor;
+        const blocksSet: SFCBlock[][] = [];
 
-        let blocks: SFCBlock[] = [];
-        if (descriptor.scriptSetup) {
-            blocks.push(descriptor.scriptSetup);
-        }
-        if (descriptor.script) {
-            blocks.push(descriptor.script);
+        if (descriptor.scriptSetup || descriptor.script) {
+            blocksSet.push([descriptor.scriptSetup, descriptor.script].filter(notEmpty));
         }
         if (descriptor.template) {
-            blocks.push(descriptor.template);
+            blocksSet.push([descriptor.template]);
         }
-        blocks = blocks.concat(descriptor.styles);
-        blocks = blocks.concat(descriptor.customBlocks);
+        if (descriptor.styles.length) {
+            blocksSet.push(descriptor.styles);
+        }
+        if (descriptor.customBlocks.length) {
+            blocksSet.push(descriptor.customBlocks);
+        }
 
-        for (let i = 0; i < blocks.length; i++) {
-            const block = blocks[i];
-            const pos = doc.positionAt(block.loc.start.offset);
-            const _editor = i === 0 ? editor : await vscode.window.showTextDocument(doc.uri, {
-                viewColumn: vscode.ViewColumn.Beside,
-            });
+        for (let i = 0; i < blocksSet.length; i++) {
+            const blocks = blocksSet[i];
+            const firstBlock = blocks.sort((a, b) => a.loc.start.offset - b.loc.start.offset)[0];
+            if (i !== 0 && i % 2 === 1) {
+                await vscode.commands.executeCommand('workbench.action.splitEditorRight');
+            }
+            if (i !== 0 && i % 2 === 0) {
+                await vscode.commands.executeCommand('workbench.action.splitEditorDown');
+            }
+            const editor = vscode.window.activeTextEditor;
+            if (!editor) {
+                continue;
+            }
             await vscode.commands.executeCommand('editor.unfoldAll');
-            _editor.selection = new vscode.Selection(pos, pos);
+            editor.selections = blocks.map(block => new vscode.Selection(doc.positionAt(block.loc.start.offset), doc.positionAt(block.loc.start.offset)));
             await vscode.commands.executeCommand('editor.foldLevel1');
+            editor.revealRange(new vscode.Range(doc.positionAt(firstBlock.loc.start.offset), new vscode.Position(editor.document.lineCount, 0)), vscode.TextEditorRevealType.AtTop);
         }
     }));
 }
