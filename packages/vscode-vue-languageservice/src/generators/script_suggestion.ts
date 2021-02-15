02aff680d68086b1950743eeac4f7050d3de40f1
johnsoncodehk <johnsoncodehk@gmail.com>
2021-02-16 01:52:17 +0800

    fix: `<script setup>` unused report not working if no any `import` statement

--- packages/vscode-vue-languageservice/src/generators/script_suggestion.ts
+++ packages/vscode-vue-languageservice/src/generators/script_suggestion.ts
@@ -22,6 +22,7 @@ export function generate(
     writeScript();
     writeScriptSetup();
     writeTemplate();
+    gen.addText('\n;export { };\n');
 
     function writeScript() {
         if (!script)
