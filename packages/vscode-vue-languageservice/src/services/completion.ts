d57170cec6f8b7e5aa40bbf62b2e45433ef33720
Pig Fang <g-plane@hotmail.com>
2021-04-22 19:20:46 +0800

    fix: don't complete attribute value for `v-else`

--- packages/vscode-vue-languageservice/src/services/completion.ts
+++ packages/vscode-vue-languageservice/src/services/completion.ts
@@ -187,7 +187,7 @@ export function register({ sourceFiles, tsLanguageService }: TsApiRegisterOption
 				const globalAttributes: html.IAttributeData[] = [
 					{ name: 'v-if' },
 					{ name: 'v-else-if' },
-					{ name: 'v-else' },
+					{ name: 'v-else', valueSet: 'v' },
 					{ name: 'v-for' },
 				];
 				const slots: html.IAttributeData[] = [];
