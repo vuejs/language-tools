6a87f79510971b175abecf5a9e09642b7a12e077
johnsoncodehk <johnsoncodehk@gmail.com>
2021-09-01 17:29:48 +0800

    feat: support lsp client which unsupported workspaceFolders

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -6,6 +6,7 @@ import { updateConfigs } from './configs';
 import { createProjects } from './projects';
 import * as tsConfigs from './tsConfigs';
 import { getInferredCompilerOptions } from './inferredCompilerOptions';
+import { URI } from 'vscode-uri';
 
 const connection = vscode.createConnection(vscode.ProposedFeatures.all);
 connection.onInitialize(onInitialize);
@@ -20,12 +21,23 @@ connection.onRequest(shared.DepsRequest.type, () => Object.keys(require.cache));
 async function onInitialize(params: vscode.InitializeParams) {
 
 	const options: shared.ServerInitializationOptions = params.initializationOptions;
-	const folders = params.workspaceFolders
-		? params.workspaceFolders
-			.map(folder => folder.uri)
-			.filter(uri => uri.startsWith('file:/'))
-			.map(uri => shared.uriToFsPath(uri))
-		: [];
+	let folders: string[] = [];
+
+	if (params.capabilities.workspace?.workspaceFolders && params.workspaceFolders) {
+		folders = params.workspaceFolders
+			.map(folder => URI.parse(folder.uri))
+			.filter(uri => uri.scheme === 'file')
+			.map(uri => uri.fsPath);
+	}
+	else if (params.rootPath) {
+		folders = [params.rootPath];
+	}
+	else if (params.rootUri) {
+		const rootUri = URI.parse(params.rootUri);
+		if (rootUri.scheme === 'file') {
+			folders = [rootUri.fsPath];
+		}
+	}
 
 	const result: vscode.InitializeResult = {
 		capabilities: {
