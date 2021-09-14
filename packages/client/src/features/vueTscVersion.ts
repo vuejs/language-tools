5c4cd02b2b613475ad70800e6b65db0eabb7e6d6
johnsoncodehk <johnsoncodehk@gmail.com>
2021-09-14 11:16:12 +0800

    fix: search vscode-vue-languageservice version from vue-tsc dir

--- packages/client/src/features/vueTscVersion.ts
+++ packages/client/src/features/vueTscVersion.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
-import * as fs from 'fs';
 
 export async function activate(context: vscode.ExtensionContext) {
 
@@ -9,28 +8,29 @@ export async function activate(context: vscode.ExtensionContext) {
 
 	if (vscode.workspace.workspaceFolders) {
 		for (const folder of vscode.workspace.workspaceFolders) {
-			const depPath = path.join(folder.uri.fsPath, 'node_modules', 'vscode-vue-languageservice', 'package.json');
-			if (fs.existsSync(depPath)) {
-				try {
-					const packageJsonText = fs.readFileSync(depPath, 'utf8');
-					const packageJson = JSON.parse(packageJsonText);
-					const depVersion = packageJson.version;
-					if (depVersion && depVersion !== context.extension.packageJSON.version) {
+			try {
+				const vueTscPath = require.resolve('vue-tsc/package.json', { paths: [folder.uri.fsPath] });
+				const vueTscVueLsPath = require.resolve('vscode-vue-languageservice/package.json', { paths: [path.dirname(vueTscPath)] });
+				const vueTscVueLsVersion = require(vueTscVueLsPath).version;
+				if (vueTscVueLsVersion !== context.extension.packageJSON.version) {
 
-						const message = `vue-tsc dependency version (${depVersion}) is different to Extension version (${context.extension.packageJSON.version}). Type-checking behavior maybe different.`;
-						const howTo = 'How To Update?';
-						const disable = 'Disable Version Checking';
-						const option = await vscode.window.showInformationMessage(message, howTo, disable);
+					const message = `vue-tsc's dependency version (${vueTscVueLsVersion}) is different to Extension version (${context.extension.packageJSON.version}). Type-checking behavior maybe different.`;
+					const howTo = 'How To Update?';
+					const open = 'Open package.json';
+					const disable = 'Disable Checking';
+					const option = await vscode.window.showInformationMessage(message, howTo, open, disable);
 
-						if (option === howTo) {
-							vscode.env.openExternal(vscode.Uri.parse('https://github.com/johnsoncodehk/volar/discussions/402'));
-						}
-						if (option === disable) {
-							vscode.commands.executeCommand('workbench.action.openSettings', 'volar.checkVueTscVersion');
-						}
+					if (option === howTo) {
+						vscode.env.openExternal(vscode.Uri.parse('https://github.com/johnsoncodehk/volar/discussions/402'));
 					}
-				} catch { }
-			}
+					if (option === open) {
+						vscode.workspace.openTextDocument(vueTscVueLsPath).then(vscode.window.showTextDocument);
+					}
+					if (option === disable) {
+						vscode.commands.executeCommand('workbench.action.openSettings', 'volar.checkVueTscVersion');
+					}
+				}
+			} catch { }
 		}
 	}
 }
