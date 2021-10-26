0428a80b96d15f01289becb5ac0a583b1839259f
johnsoncodehk <johnsoncodehk@gmail.com>
2021-10-26 11:51:42 +0800

    feat: use toggleSplitEditorInGroup instead of splitEditorRight
    close #608

--- packages/client/src/features/splitEditors.ts
+++ packages/client/src/features/splitEditors.ts
@@ -19,56 +19,38 @@ export function activate(context: vscode.ExtensionContext) {
 
 		const doc = editor.document;
 		const descriptor = getDocDescriptor(doc.getText());
-		const blocksSet: shared.SfcBlock[][] = [];
-
-		if (descriptor.scriptSetup || descriptor.script) {
-			blocksSet.push([descriptor.scriptSetup, descriptor.script].filter(shared.notEmpty));
-		}
-		if (descriptor.template) {
-			blocksSet.push([descriptor.template]);
-		}
-		if (descriptor.styles.length) {
-			blocksSet.push(descriptor.styles);
-		}
-		if (descriptor.customBlocks.length) {
-			blocksSet.push(descriptor.customBlocks);
-		}
+		const leftBlocks = [
+			descriptor.scriptSetup,
+			descriptor.script,
+			...descriptor.styles,
+		].filter(shared.notEmpty);
+		const rightBlocks = [
+			descriptor.template,
+			...descriptor.customBlocks,
+		].filter(shared.notEmpty);
 
 		const startViewColumn = vscode.window.activeTextEditor?.viewColumn;
 		if (startViewColumn === undefined)
 			return;
 
-		await splitCurrentEditors(blocksSet.length);
-		for (let i = 0; i < blocksSet.length; i++) {
+		await foldingBlocks(leftBlocks);
+		await vscode.commands.executeCommand('workbench.action.toggleSplitEditorInGroup');
+		await foldingBlocks(rightBlocks);
 
-			const blocks = blocksSet[i];
+		async function foldingBlocks(blocks: shared.SfcBlock[]) {
+
 			const firstBlock = blocks.sort((a, b) => a.startTagEnd - b.startTagEnd)[0];
 
-			const editor = await vscode.window.showTextDocument(doc, startViewColumn + i);
+			const editor = vscode.window.activeTextEditor;
+			if (!editor) return;
+
 			editor.selections = blocks.map(block => new vscode.Selection(doc.positionAt(block.startTagEnd), doc.positionAt(block.startTagEnd)));
 
+			await vscode.commands.executeCommand('editor.unfoldAll');
 			await vscode.commands.executeCommand('editor.foldLevel1');
 			editor.revealRange(new vscode.Range(doc.positionAt(firstBlock.startTagEnd), new vscode.Position(editor.document.lineCount, 0)), vscode.TextEditorRevealType.AtTop);
 		}
 	}
-}
-
-async function splitCurrentEditors(num: number) {
-
-	const actions: ReturnType<typeof vscode.commands.executeCommand>[] = [];
-
-	await vscode.commands.executeCommand('editor.unfoldAll');
-
-	for (let i = 1; i < num; i++) {
-		if (i % 2 === 1) {
-			actions.push(vscode.commands.executeCommand('workbench.action.splitEditorRight'));
-		}
-		if (i % 2 === 0) {
-			actions.push(vscode.commands.executeCommand('workbench.action.splitEditorDown'));
-		}
-	}
-
-	await Promise.all(actions);
 }
 
 function useDocDescriptor() {
