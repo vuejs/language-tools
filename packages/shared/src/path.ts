b21626e274c12cd4207bf0d2598043b178bf18fe
johnsoncodehk <johnsoncodehk@gmail.com>
2021-11-07 22:04:46 +0800

    fix: documentSymbol, foldingRanges not working to some *.ts files on take over mode

--- packages/shared/src/path.ts
+++ packages/shared/src/path.ts
@@ -14,6 +14,10 @@ export function normalizeFileName(fsPath: string) {
 	return upath.toUnix(URI.file(fsPath).fsPath);
 }
 
+export function normalizeUri(uri: string) {
+	return URI.parse(uri).toString();
+}
+
 export function isFileInDir(fileName: string, dir: string) {
 	const relative = upath.relative(dir, fileName);
 	return relative && !relative.startsWith('..') && !upath.isAbsolute(relative);
