5c657c9154ef6862d37427f6cfa6c2a38de13630
Kristoffer K <merceyz@users.noreply.github.com>
2022-01-13 07:32:10 +0100

    fix: resolve dependency issues (#851)
    * fix(shared): declare runtime dependency `vscode-html-languageservice`
    
    * fix(vue-tsc): use absolute path to proxy

--- packages/vue-tsc/bin/vue-tsc.js
+++ packages/vue-tsc/bin/vue-tsc.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 
 const readFileSync = fs.readFileSync;
 const tscPath = require.resolve('typescript/lib/tsc');
+const proxyPath = JSON.stringify(require.resolve('../out/proxy'));
 
 fs.readFileSync = (...args) => {
     if (args[0] === tscPath) {
@@ -17,7 +18,7 @@ fs.readFileSync = (...args) => {
         );
         tsc = tsc.replace(
             `function createProgram(rootNamesOrOptions, _options, _host, _oldProgram, _configFileParsingDiagnostics) {`,
-            `function createProgram(rootNamesOrOptions, _options, _host, _oldProgram, _configFileParsingDiagnostics) { return require('vue-tsc/out/proxy').createProgramProxy(...arguments);`,
+            `function createProgram(rootNamesOrOptions, _options, _host, _oldProgram, _configFileParsingDiagnostics) { return require(${proxyPath}).createProgramProxy(...arguments);`,
         );
         return tsc;
     }
