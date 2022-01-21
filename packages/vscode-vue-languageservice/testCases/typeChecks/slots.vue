c8b9d222e596c592db0df978bd50503ba0ca6e14
johnsoncodehk <johnsoncodehk@gmail.com>
2022-01-21 10:01:52 +0800

    chore: add slots type-check test

---
+++ packages/vscode-vue-languageservice/testCases/typeChecks/slots.vue
@@ -0,0 +1,20 @@
+<template>
+	<Comp value="1">
+		<template #foo="bindings">{{ exactType(bindings, {} as string) }}</template>
+	</Comp>
+	<Comp :value="1">
+		<template #foo="bindings">{{ exactType(bindings, {} as number) }}</template>
+	</Comp>
+</template>
+
+<script lang="ts" setup>
+import { VNode } from 'vue';
+import { exactType } from './shared';
+
+declare const Comp: new <T>(props: { value: T }) => {
+	$props: typeof props;
+	$slots: {
+		foo: (_: T) => VNode[];
+	},
+};
+</script>
