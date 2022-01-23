2189b5604052706bebbfdbca1af14186abddfcb0
yaegassy <yaegassy@users.noreply.github.com>
2022-01-23 17:19:03 +0900

    chore: add "-" to the html completion trigger characters

--- packages/vscode-vue-languageservice/src/services/completion.ts
+++ packages/vscode-vue-languageservice/src/services/completion.ts
@@ -21,7 +21,7 @@ export function getTriggerCharacters(tsVersion: string) {
 	return {
 		typescript: ts2.getTriggerCharacters(tsVersion),
 		jsdoc: ['*'],
-		html: ['.', ':', '<', '"', '=', '/'], // https://github.com/microsoft/vscode/blob/09850876e652688fb142e2e19fd00fd38c0bc4ba/extensions/html-language-features/server/src/htmlServer.ts#L183
+		html: ['.', ':', '<', '"', '=', '/', '-'], // https://github.com/microsoft/vscode/blob/09850876e652688fb142e2e19fd00fd38c0bc4ba/extensions/html-language-features/server/src/htmlServer.ts#L183
 		css: ['/', '-', ':'], // https://github.com/microsoft/vscode/blob/09850876e652688fb142e2e19fd00fd38c0bc4ba/extensions/css-language-features/server/src/cssServer.ts#L97
 		json: ['"', ':'], // https://github.com/microsoft/vscode/blob/09850876e652688fb142e2e19fd00fd38c0bc4ba/extensions/json-language-features/server/src/jsonServer.ts#L150
 	};
