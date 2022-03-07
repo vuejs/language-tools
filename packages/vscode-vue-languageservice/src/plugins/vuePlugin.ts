72380f1e04095f2bdd0e989459cef34dcce99823
johnsoncodehk <johnsoncodehk@gmail.com>
2022-03-07 13:24:28 +0800

    feat: support `lang` attribute intellisense for custom blocks

--- packages/vscode-vue-languageservice/src/plugins/vuePlugin.ts
+++ packages/vscode-vue-languageservice/src/plugins/vuePlugin.ts
@@ -8,50 +8,90 @@ import * as vscode from 'vscode-languageserver-protocol';
 
 export { triggerCharacters } from './htmlPlugin';
 
-const vueTags: html.ITagData[] = [
-    {
-        name: 'template',
-        attributes: [
-            {
-                name: 'lang',
-                values: [
-                    { name: 'html' },
-                    { name: 'pug' },
-                ],
-            },
-        ],
-    },
-    {
-        name: 'script',
-        attributes: [
-            {
-                name: 'lang',
-                values: [
-                    { name: 'js' },
-                    { name: 'ts' },
-                    { name: 'jsx' },
-                    { name: 'tsx' },
-                ],
-            },
-            { name: 'setup', valueSet: 'v' },
-        ],
-    },
-    {
-        name: 'style',
-        attributes: [
-            {
-                name: 'lang',
-                values: [
-                    { name: 'css' },
-                    { name: 'scss' },
-                    { name: 'less' },
-                ],
-            },
-            { name: 'scoped', valueSet: 'v' },
-            { name: 'module', valueSet: 'v' },
-        ],
-    },
-];
+const dataProvider = html.newHTMLDataProvider('vue', {
+    version: 1.1,
+    tags: [
+        {
+            name: 'template',
+            attributes: [
+                {
+                    name: 'lang',
+                    values: [
+                        { name: 'html' },
+                        { name: 'pug' },
+                    ],
+                },
+            ],
+        },
+        {
+            name: 'script',
+            attributes: [
+                {
+                    name: 'lang',
+                    values: [
+                        { name: 'js' },
+                        { name: 'ts' },
+                        { name: 'jsx' },
+                        { name: 'tsx' },
+                    ],
+                },
+                { name: 'setup', valueSet: 'v' },
+            ],
+        },
+        {
+            name: 'style',
+            attributes: [
+                {
+                    name: 'lang',
+                    values: [
+                        { name: 'css' },
+                        { name: 'scss' },
+                        { name: 'less' },
+                        { name: 'stylus' },
+                        { name: 'postcss' },
+                        { name: 'sass' },
+                    ],
+                },
+                { name: 'scoped', valueSet: 'v' },
+                { name: 'module', valueSet: 'v' },
+            ],
+        },
+    ],
+    globalAttributes: [
+        {
+            name: 'src',
+        },
+        {
+            name: 'lang',
+            // all other embedded languages
+            values: [
+                // template
+                { name: 'html' },
+                { name: 'pug' },
+                // script
+                { name: 'js' },
+                { name: 'ts' },
+                { name: 'jsx' },
+                { name: 'tsx' },
+                // style
+                { name: 'css' },
+                { name: 'scss' },
+                { name: 'less' },
+                { name: 'stylus' },
+                { name: 'postcss' },
+                { name: 'sass' },
+                // custom block
+                { name: 'md' },
+                { name: 'json' },
+                { name: 'jsonc' },
+                { name: 'yaml' },
+                { name: 'toml' },
+                { name: 'gql' },
+                { name: 'graphql' },
+            ],
+        }
+    ]
+});
 
 export default definePlugin((host: {
     getVueDocument(document: TextDocument): VueDocument | undefined,
@@ -60,10 +100,6 @@ export default definePlugin((host: {
 
     const htmlDocuments = new WeakMap<TextDocument, [number, html.HTMLDocument]>();
     const htmlLs = html.getLanguageService();
-    const dataProvider = html.newHTMLDataProvider('vue', {
-        version: 1.1,
-        tags: vueTags,
-    });
     htmlLs.setDataProviders(false, [dataProvider]);
 
     return {
