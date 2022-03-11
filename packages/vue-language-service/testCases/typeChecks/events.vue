862f8c8d8ef59e02160cc4c7bff23578cb2beb82
johnsoncodehk <johnsoncodehk@gmail.com>
2022-03-12 05:57:39 +0800

    chore: add event test cases

--- packages/vue-language-service/testCases/typeChecks/events.vue
+++ packages/vue-language-service/testCases/typeChecks/events.vue
@@ -14,6 +14,13 @@
 	<C4 :value="1" @fooBar="exactType($event, {} as number)" />
 
 	<C5 @a="exactType($event, {} as string)"></C5>
+
+	<!-- $emit has high priority then $props -->
+	<C6 @foo="exactType($event, {} as number)" />
+
+	<!-- override original event -->
+	<C7 @click="exactType($event, {} as number)" />
+	<C8 @click="exactType($event, {} as number)" />
 </template>
 
 <script lang="ts" setup>
@@ -22,7 +29,10 @@ import { exactType } from './shared';
 import C5 from './events_union.vue';
 
 const C1 = defineComponent({ emits: { fooBar: (_num: number) => true } });
-const C2 = defineComponent({ props: { onFooBar: Function as PropType<(num: number) => void> } });
+const C2 = defineComponent({ props: { onFooBar: {} as PropType<(num: number) => void> } });
+const C6 = defineComponent({ props: { onFoo: {} as PropType<(_num: string) => void> }, emits: { foo: (_num: number) => true } });
+const C7 = defineComponent({ emits: { click: (_num: number) => true } });
+const C8 = defineComponent({ props: { onClick: {} as PropType<(_num: number) => void> } });
 </script>
 
 <script lang="ts">
