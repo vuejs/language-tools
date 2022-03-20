d3014ed67eea573294c68944da831e16d0b474b7
Dragon <54531807+Dragon-GCS@users.noreply.github.com>
2022-03-21 01:23:33 +0800

    fix: can't recognize viteBinPath on Windows (#1074)
    * fix: can't recognize viteBinPath on Windows
    
    * fix: previewer not working with pnpm
    
    Co-authored-by: johnsoncodehk <johnsoncodehk@gmail.com>

--- extensions/vscode-vue-language-features/bin/vite.js
+++ extensions/vscode-vue-language-features/bin/vite.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const readFileSync = fs.readFileSync;
 
 const workspace = process.cwd();
-const viteBinPath = require.resolve('./node_modules/.bin/vite', { paths: [workspace] });
+const viteBinPath = require.resolve('vite/bin/vite.js', { paths: [workspace] });
 const viteDir = path.dirname(require.resolve('vite/package.json', { paths: [workspace] }));
 const vuePluginPath = require.resolve('@vitejs/plugin-vue', { paths: [workspace] });
 const installCode = `
@@ -155,7 +155,7 @@ function __installPreview(app) {
     }
 }
 `;
-const replaceCode =`async function doTransform(...args) {
+const replaceCode = `async function doTransform(...args) {
     const result = await __doTransformOriginal(...args);
     const createAppText = 'createApp,';
     if (args[0].indexOf('vue.js?') >= 0 && result.code && result.code.indexOf(createAppText) >= 0 && result.code.indexOf('__createAppProxy') === -1) {
