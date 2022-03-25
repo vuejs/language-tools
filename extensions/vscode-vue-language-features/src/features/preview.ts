610ca82d43cab171cf51e5d5a4d8da8c313e1ad3
johnsoncodehk <johnsoncodehk@gmail.com>
2022-03-25 17:12:29 +0800

    chore: add sponsors button

--- extensions/vscode-vue-language-features/src/features/preview.ts
+++ extensions/vscode-vue-language-features/src/features/preview.ts
@@ -459,9 +459,6 @@ export async function activate(context: vscode.ExtensionContext) {
 				document.body.append(preview);
 			})();
 
-			function onClickSvg() {
-				vscode.postMessage({ command: 'openUrl', data: 'https://cdn.jsdelivr.net/gh/johnsoncodehk/sponsors/sponsors.svg' });
-			}
 			function previewFrameLoaded() {
 				preview.style.height = '100vh';
 				document.getElementById('loading').remove();
@@ -479,11 +476,36 @@ export async function activate(context: vscode.ExtensionContext) {
 			</script>
 
 			<div id="loading">
-				<p align="center">
-					<a href="#" onclick="onClickSvg()">
+				<div style="display: flex; align-items: center; flex-direction: column;">
+					<a href="https://cdn.jsdelivr.net/gh/johnsoncodehk/sponsors/sponsors.svg" target="_top">
 						<img src="https://cdn.jsdelivr.net/gh/johnsoncodehk/sponsors/sponsors.svg?time=${Math.round(Date.now() / 1000 / 3600)}" />
 					</a>
-				</p>
+
+					<div style="height: 35px; width: 116px; display: flex;">
+						<a
+							style="box-shadow: none; background-color: rgb(250, 251, 252); border-block: 1px solid rgba(27, 31, 35, 0.15); border-bottom-left-radius: 6px; border-bottom-right-radius: 6px; border-inline: 1px solid rgba(27, 31, 35, 0.15); border-start-end-radius: 6px; border-start-start-radius: 6px; caret-color: rgb(36, 41, 46); color: rgb(36, 41, 46); display: block; font-size: 14px; font-weight: 500; inline-size: 100%; line-height: 20px; padding-block: 5px; padding-inline: 16px; position: relative; text-align: center; transition-duration: 0.2s; transition-property: background-color; transition-timing-function: cubic-bezier(0.3, 0, 0.5, 1); user-select: none; vertical-align: middle; white-space: nowrap; text-decoration: none; display: inline-flex; align-items: center;"
+							aria-label="Sponsor @johnsoncodehk"
+							target="_top"
+							href="https://github.com/sponsors/johnsoncodehk?o=esb"
+						>
+							<svg
+								aria-hidden="true"
+								height="16"
+								viewBox="0 0 16 16"
+								version="1.1"
+								width="16"
+								data-view-component="true"
+								style="border-block-color: rgb(106, 115, 125); border-inline-color: rgb(106, 115, 125); caret-color: rgb(106, 115, 125); color: rgb(106, 115, 125); display: inline-block; fill: rgb(106, 115, 125); margin-inline-end: 8px; overflow: visible; transform: scale(1, 1); transition-duration: 0.15s; transition-property: transform; transition-timing-function: cubic-bezier(0.2, 0, 0.13, 2); vertical-align: text-bottom;"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M4.25 2.5c-1.336 0-2.75 1.164-2.75 3 0 2.15 1.58 4.144 3.365 5.682A20.565 20.565 0 008 13.393a20.561 20.561 0 003.135-2.211C12.92 9.644 14.5 7.65 14.5 5.5c0-1.836-1.414-3-2.75-3-1.373 0-2.609.986-3.029 2.456a.75.75 0 01-1.442 0C6.859 3.486 5.623 2.5 4.25 2.5zM8 14.25l-.345.666-.002-.001-.006-.003-.018-.01a7.643 7.643 0 01-.31-.17 22.075 22.075 0 01-3.434-2.414C2.045 10.731 0 8.35 0 5.5 0 2.836 2.086 1 4.25 1 5.797 1 7.153 1.802 8 3.02 8.847 1.802 10.203 1 11.75 1 13.914 1 16 2.836 16 5.5c0 2.85-2.045 5.231-3.885 6.818a22.08 22.08 0 01-3.744 2.584l-.018.01-.006.003h-.002L8 14.25zm0 0l.345.666a.752.752 0 01-.69 0L8 14.25z"
+								/>
+							</svg>
+							<span>Sponsor</span>
+						</a>
+					</div>
+				</div>
 
 				<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; display: block; shape-rendering: auto;" width="200px" height="100px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
 					<g transform="translate(20 50)">
