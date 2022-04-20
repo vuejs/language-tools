dadf4e1598927af31c3892bc53733e3726c26ccb
johnsoncodehk <johnsoncodehk@gmail.com>
2022-04-20 21:10:34 +0800

    fix: cannot update relate vue document diagnostics
    close #1163

--- packages/vue-language-service/src/languageFeatures/validation.ts
+++ packages/vue-language-service/src/languageFeatures/validation.ts
@@ -103,7 +103,7 @@ export function register(context: LanguageServiceRuntimeContext) {
 
 					const pluginCache = cacheMap.get(plugin.id) ?? cacheMap.set(plugin.id, new Map()).get(plugin.id)!;
 					const cache = pluginCache.get(document.uri);
-					const tsProjectVersion = isTs ? undefined : context.getTsLs().__internal__.host.getProjectVersion?.();
+					const tsProjectVersion = isTs ? context.getTsLs().__internal__.host.getProjectVersion?.() : undefined;
 
 					if (!isTs) {
 						if (cache && cache.documentVersion === document.version) {
