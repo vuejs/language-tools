4845bdfbcdb15dd6ca55cf61d3cc6ea6a203bc4b
johnsoncodehk <johnsoncodehk@gmail.com>
2022-05-13 11:25:04 +0800

    fix: tsconfig status dons't show with ts file on takeover mode

--- extensions/vscode-vue-language-features/src/features/tsconfig.ts
+++ extensions/vscode-vue-language-features/src/features/tsconfig.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import { CommonLanguageClient } from 'vscode-languageclient';
 import * as shared from '@volar/shared';
 import * as path from 'path';
+import { takeOverModeEnabled } from '../common';
 
 export async function activate(context: vscode.ExtensionContext, languageClient: CommonLanguageClient) {
 
@@ -19,7 +20,14 @@ export async function activate(context: vscode.ExtensionContext, languageClient:
 	});
 
 	async function updateStatusBar() {
-		if (vscode.window.activeTextEditor?.document.languageId !== 'vue') {
+		if (
+			vscode.window.activeTextEditor?.document.languageId !== 'vue'
+			&& !(
+				takeOverModeEnabled()
+				&& vscode.window.activeTextEditor
+				&& ['javascript', 'typescript', 'javascriptreact', 'typescriptreact'].includes(vscode.window.activeTextEditor.document.languageId)
+			)
+		) {
 			statusBar.hide();
 		}
 		else {
