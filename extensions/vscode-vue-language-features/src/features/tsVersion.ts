0b7dd8c819624ffcec044578bdf3a54635a657a1
johnsoncodehk <johnsoncodehk@gmail.com>
2022-05-13 11:26:41 +0800

    fix: ts version status dons't show with ts file on takeover mode

--- extensions/vscode-vue-language-features/src/features/tsVersion.ts
+++ extensions/vscode-vue-language-features/src/features/tsVersion.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import { CommonLanguageClient } from 'vscode-languageclient';
 import * as shared from '@volar/shared';
 import { userPick } from './splitEditors';
+import { takeOverModeEnabled } from '../common';
 
 const defaultTsdk = 'node_modules/typescript/lib';
 
@@ -74,7 +75,14 @@ export async function activate(context: vscode.ExtensionContext, clients: Common
 	vscode.window.onDidChangeActiveTextEditor(updateStatusBar, undefined, context.subscriptions);
 
 	function updateStatusBar() {
-		if (vscode.window.activeTextEditor?.document.languageId !== 'vue') {
+		if (
+			vscode.window.activeTextEditor?.document.languageId !== 'vue'
+			&& !(
+				takeOverModeEnabled()
+				&& vscode.window.activeTextEditor
+				&& ['javascript', 'typescript', 'javascriptreact', 'typescriptreact'].includes(vscode.window.activeTextEditor.document.languageId)
+			)
+		) {
 			statusBar.hide();
 		}
 		else {
