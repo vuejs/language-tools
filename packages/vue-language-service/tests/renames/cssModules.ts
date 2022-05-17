1cca3a1fc7e30f76bbb2d25019f1ef8843e80206
Johnson Chu <johnsoncodehk@gmail.com>
2022-05-18 01:33:39 +0800

    feat: add test for vue-tsc (#1229)
    Co-authored-by: ZHAO Jinxiang <xiaoxiangmoe@gmail.com>

--- packages/vue-language-service/tests/renames/cssModules.ts
+++ packages/vue-language-service/tests/renames/cssModules.ts
@@ -2,7 +2,7 @@ import * as path from 'upath';
 import { Position } from 'vscode-languageserver-protocol';
 import { defineRename } from '../utils/defineRename';
 
-const file = path.resolve(__dirname, '../../test-workspace/renames/cssModules.vue');
+const file = path.resolve(__dirname, '../../../vue-test-workspace/renames/cssModules.vue');
 const fileResult = `
 <template>
     <div :class="$style.bar"></div>
