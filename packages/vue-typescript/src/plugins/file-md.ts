9b989203bf530cb5d7f998893148780f7429e0df
johnsoncodehk <johnsoncodehk@gmail.com>
2022-06-07 23:13:03 +0800

    refactor: parse markdown by regex instead of ast

--- packages/vue-typescript/src/plugins/file-md.ts
+++ packages/vue-typescript/src/plugins/file-md.ts
@@ -1,7 +1,4 @@
 import { VueLanguagePlugin } from '../vueFile';
-import * as MarkdownIt from 'markdown-it';
-// @ts-expect-error
-import * as MarkdownItAst from 'markdown-it-ast';
 import { SourceMapBase, Mode } from '@volar/source-map';
 import { CodeGen } from '@volar/code-gen';
 
@@ -13,138 +10,74 @@ export default function (): VueLanguagePlugin {
 
 			if (fileName.endsWith('.md')) {
 
-				let validTemplateBlock: [number, number] | undefined;
-				let validScriptBlock: [number, number] | undefined;
-				let validStyleBlock: [number, number] | undefined;
+				const _content = content;
 
-				const scriptLines: [number, number][] = [];
-				const styleLines: [number, number][] = [];
-				const templateLines: [number, number][] = [];
+				content = content
+					// inline code block
+					.replace(/```[\s\S]*?```/g, match => '```' + ' '.repeat(match.length - 6) + '```')
+					// inline code block
+					.replace(/`[\s\S]*?`/g, match => `\`${' '.repeat(match.length - 2)}\``)
+					// # \<script setup>
+					.replace(/\\<[\s\S]*?\n?/g, match => ' '.repeat(match.length));
 
-				const tokens = MarkdownIt().parse(content, {});
-				const ast = MarkdownItAst.makeAST(tokens);
+				const scriptBlockReg = /\<script[\s\S]*?\>([\s\S]*?)\<\/script\>/g;
+				const styleBlockReg = /\<style[\s\S]*?\>([\s\S]*?)\<\/style\>/g;
 
-				for (const node of ast) {
-					// <script> block start tag
-					if (
-						node.nodeType === 'paragraph'
-						&& node.children.length
-						&& node.children[0].type === 'inline' && (node.children[0].content.startsWith('<script ') || node.children[0].content.startsWith('<script>'))
-					) {
-						breakTemplateBlock();
-						validScriptBlock = node.children[0].map;
+				const scriptBlocks: [number, number][] = [];
+				const styleBlocks: [number, number][] = [];
+
+				for (const match of content.matchAll(scriptBlockReg)) {
+					if (match.index !== undefined) {
+						const matchText = match[0];
+						scriptBlocks.push([match.index, match.index + matchText.length]);
 					}
-					// <script> block end tag
-					if (
-						validScriptBlock
-						&& node.nodeType === 'paragraph'
-						&& node.children.length
-						&& node.children[0].type === 'inline' && node.children[0].content.indexOf('</script>') >= 0
-					) {
-						validScriptBlock[1] = node.children[0].map[1];
-						scriptLines.push(validScriptBlock);
-						validScriptBlock = undefined;
-						continue;
-					}
-					if (validScriptBlock) {
-						continue;
-					}
-					// <style> block start tag
-					if (
-						node.nodeType === 'paragraph'
-						&& node.children.length
-						&& node.children[0].type === 'inline' && (node.children[0].content.startsWith('<style ') || node.children[0].content.startsWith('<style>'))
-					) {
-						breakTemplateBlock();
-						validStyleBlock = node.children[0].map;
-					}
-					// <style> block end tag
-					if (
-						validStyleBlock
-						&& node.nodeType === 'paragraph'
-						&& node.children.length
-						&& node.children[0].type === 'inline' && node.children[0].content.indexOf('</style>') >= 0
-					) {
-						validStyleBlock[1] = node.children[0].map[1];
-						styleLines.push(validStyleBlock);
-						validStyleBlock = undefined;
-						continue;
-					}
-					if (validStyleBlock) {
-						continue;
-					}
-					walkNode(node);
 				}
 
-				breakTemplateBlock();
+				for (const match of content.matchAll(styleBlockReg)) {
+					if (match.index !== undefined) {
+						const matchText = match[0];
+						styleBlocks.push([match.index, match.index + matchText.length]);
+					}
+				}
+
+				content = content
+					.replace(scriptBlockReg, match => ' '.repeat(match.length))
+					.replace(styleBlockReg, match => ' '.repeat(match.length))
+					// angle bracket: <http://foo.com>
+					.replace(/\<\S*\:\S*\>/g, match => ' '.repeat(match.length))
+					// [foo](http://foo.com)
+					.replace(/\[[\s\S]*?\]\([\s\S]*?\)/g, match => ' '.repeat(match.length));
 
 				const codeGen = new CodeGen();
-				const lines = content.split('\n');
-				const lineOffsets: number[] = [];
-				let lineOffset = 0;
 
-				for (const line of lines) {
-					lineOffsets.push(lineOffset);
-					lineOffset += line.length + 1;
-				}
-
-				for (const _lines of scriptLines) {
-					const rangeLines = lines.slice(_lines[0], _lines[1]);
-					const rangeCode = rangeLines.join('\n');
-					const start = lineOffsets[_lines[0]];
+				for (const block of [
+					...scriptBlocks,
+					...styleBlocks,
+				]) {
 					codeGen.addCode(
-						rangeCode,
+						_content.substring(block[0], block[1]),
 						{
-							start: start,
-							end: start + rangeCode.length,
+							start: block[0],
+							end: block[1],
 						},
 						Mode.Offset,
 						undefined,
 					);
+					codeGen.addText('\n\n');
 				}
 
-				for (const _lines of styleLines) {
-					const rangeLines = lines.slice(_lines[0], _lines[1]);
-					const rangeCode = rangeLines.join('\n');
-					const start = lineOffsets[_lines[0]];
-					codeGen.addCode(
-						rangeCode,
-						{
-							start: start,
-							end: start + rangeCode.length,
-						},
-						Mode.Offset,
-						undefined,
-					);
-				}
-
-				if (templateLines.length) {
-					codeGen.addText('\n<template>\n');
-					for (const _lines of templateLines) {
-						const rangeLines = lines.slice(_lines[0], _lines[1]);
-						const rangeCode = rangeLines.join('\n');
-						const start = lineOffsets[_lines[0]];
-						codeGen.addCode(
-							rangeCode
-								// inline code block
-								.replace(/\`([\s\S]*?)\`/g, match => `\`${' '.repeat(match.length - 2)}\``)
-								// # \<script setup>
-								.replace(/\\\<([\s\S]*?)\n?/g, match => ' '.repeat(match.length))
-								// <http://google.com>
-								.replace(/\<\S*\:\S*\>/g, match => ' '.repeat(match.length))
-								// markdown line
-								.replace(/\[([\s\S]*?)\]\(([\s\S]*?)\)/g, match => ' '.repeat(match.length)),
-							{
-								start: start,
-								end: start + rangeCode.length,
-							},
-							Mode.Offset,
-							undefined,
-						);
-						codeGen.addText('\n');
-					}
-					codeGen.addText('\n</template>\n');
-				}
+				codeGen.addText('<template>\n');
+				codeGen.addCode(
+					content,
+					{
+						start: 0,
+						end: content.length,
+					},
+					Mode.Offset,
+					undefined,
+				);
+				codeGen.addText('\n');
+				codeGen.addText('\n</template>\n');
 
 				const sourceMap = new SourceMapBase(codeGen.getMappings());
 
@@ -153,42 +86,7 @@ export default function (): VueLanguagePlugin {
 					mapping: vueRange => sourceMap.getSourceRange(vueRange.start, vueRange.end)?.[0],
 					sourceMap, // for create virtual embedded vue file
 				};
-
-				function walkNode(node: any) {
-					// ignore ``` block
-					if (node.type === 'fence') {
-						breakTemplateBlock();
-						return false;
-					}
-					let shouldAddRange = true;
-					if (node.children) {
-						for (const child of node.children) {
-							shouldAddRange = shouldAddRange && walkNode(child);
-						}
-					}
-					if (shouldAddRange) {
-						const map = node.map ?? node.openNode?.map;
-						if (map) {
-							addValidTemplateBlockRange(map);
-						}
-					}
-					return true;
-				}
-				function breakTemplateBlock() {
-					if (validTemplateBlock) {
-						templateLines.push(validTemplateBlock);
-						validTemplateBlock = undefined;
-					}
-				}
-				function addValidTemplateBlockRange(range: [number, number]) {
-					if (!validTemplateBlock) {
-						validTemplateBlock = [range[0], range[1]];
-					}
-					else {
-						validTemplateBlock[1] = range[1];
-					}
-				}
-			}
+			};
 		}
 	};
 }
