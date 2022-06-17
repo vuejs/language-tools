8bb99d14e0dc5f7b32eba95378c7d8a7e4660bd5
johnsoncodehk <johnsoncodehk@gmail.com>
2022-06-17 19:04:53 +0800

    fix: camel case css class name missing
    close #1447

--- packages/vue-typescript/src/utils/parseCssClassNames.ts
+++ packages/vue-typescript/src/utils/parseCssClassNames.ts
@@ -1,10 +1,9 @@
 import { clearComments } from './parseCssVars';
 
-// https://gist.github.com/Potherca/f2a65491e63338659c3a0d2b07eee382
 export function* parseCssClassNames(styleContent: string) {
 	styleContent = clearComments(styleContent);
-	const reg = /\.[a-z]([a-z0-9-]+)?(__([a-z0-9]+-?)+)?(--([a-z0-9]+-?)+){0,2}/g;
-	const matchs = styleContent.matchAll(reg);
+	const cssClassNameRegex = /\.([\w-]+)/g;
+	const matchs = styleContent.matchAll(cssClassNameRegex);
 	for (const match of matchs) {
 		if (match.index !== undefined) {
 			const matchText = match[0];
