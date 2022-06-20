fc2524873454eaae1463a185d8263e52eca1b645
Lucas Colombo <lucasncolombo@gmail.com>
2022-06-20 11:30:00 -0300

    fix: regex not matching first . of the class

--- packages/vue-typescript/src/utils/parseCssClassNames.ts
+++ packages/vue-typescript/src/utils/parseCssClassNames.ts
@@ -2,7 +2,7 @@ import { clearComments } from './parseCssVars';
 
 export function* parseCssClassNames(styleContent: string) {
 	styleContent = clearComments(styleContent);
-	const cssClassNameRegex = /(?=[\.]{1}([a-zA-Z_]+[\w\_\-]*)[\s\.\+\{\>#\:]{1})/g;
+	const cssClassNameRegex = /(?=([\.]{1}[a-zA-Z_]+[\w\_\-]*)[\s\.\+\{\>#\:]{1})/g;
 	const matchs = styleContent.matchAll(cssClassNameRegex);
 	for (const match of matchs) {
 		if (match.index !== undefined) {
