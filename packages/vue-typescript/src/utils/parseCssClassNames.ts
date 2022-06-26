753a80087f0420f8f8d25256205d412bbc2ed8a9
Lucas Colombo <lucasncolombo@gmail.com>
2022-06-26 06:44:14 -0300

    fix: css classes parsing (#1485)

--- packages/vue-typescript/src/utils/parseCssClassNames.ts
+++ packages/vue-typescript/src/utils/parseCssClassNames.ts
@@ -2,11 +2,11 @@ import { clearComments } from './parseCssVars';
 
 export function* parseCssClassNames(styleContent: string) {
 	styleContent = clearComments(styleContent);
-	const cssClassNameRegex = /\.([\w-]+)/g;
+	const cssClassNameRegex = /(?=([\.]{1}[a-zA-Z_]+[\w\_\-]*)[\s\.\+\{\>#\:]{1})/g;
 	const matchs = styleContent.matchAll(cssClassNameRegex);
 	for (const match of matchs) {
 		if (match.index !== undefined) {
-			const matchText = match[0];
+			const matchText = match[1];
 			if (matchText !== undefined) {
 				yield { start: match.index, end: match.index + matchText.length };
 			}
