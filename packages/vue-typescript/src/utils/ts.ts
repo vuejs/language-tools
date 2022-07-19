0f7a9a3b15925292310cb1c726fd1479e43726ac
johnsoncodehk <johnsoncodehk@gmail.com>
2022-07-20 07:20:42 +0800

    fix: use os path to parse tsconfig
    close #1591, close #1593,

--- packages/vue-typescript/src/utils/ts.ts
+++ packages/vue-typescript/src/utils/ts.ts
@@ -1,5 +1,5 @@
 import type * as ts from 'typescript/lib/tsserverlibrary';
-import { posix as path } from 'path';
+import * as path from 'path';
 import type { VueCompilerOptions } from '../types';
 
 export function createParsedCommandLine(
@@ -17,7 +17,7 @@ export function createParsedCommandLine(
 	content.options.outDir = undefined; // TODO: patching ts server broke with outDir + rootDir + composite/incremental
 
 	let baseVueOptions = {};
-	const folder = path.dirname(tsConfig);
+	const folder = path.dirname(tsConfigPath);
 
 	extendsSet.add(tsConfig);
 
