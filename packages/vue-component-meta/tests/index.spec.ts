a9761a08edd3690aa7e1a133063797f5e42ec28c
johnsoncodehk <johnsoncodehk@gmail.com>
2022-07-25 05:18:42 +0800

    chore: test class slots

--- packages/vue-component-meta/tests/index.spec.ts
+++ packages/vue-component-meta/tests/index.spec.ts
@@ -14,13 +14,13 @@ describe(`vue-component-meta`, () => {
 
 		const a = meta.props.find(prop =>
 			prop.name === 'foo'
-			&& !prop.isOptional
+			&& prop.isOptional === false
 			&& prop.type === 'string'
 			&& prop.documentationComment === 'string foo'
 		);
 		const b = meta.props.find(prop =>
 			prop.name === 'bar'
-			&& prop.isOptional
+			&& prop.isOptional === true
 			&& prop.type === 'number | undefined'
 			&& prop.documentationComment === 'optional number bar'
 		);
@@ -39,13 +39,19 @@ describe(`vue-component-meta`, () => {
 			&& event.parametersType === '[data: { foo: string; }]'
 			&& event.parameters.length === 1
 			&& event.parameters[0].type === '{ foo: string; }'
+			// && event.parameters[0].name === 'data'
+			// && event.parameters[0].isOptional === false
 		);
 		const b = meta.events.find(event =>
 			event.name === 'bar'
 			&& event.parametersType === '[arg1: number, arg2?: any]'
 			&& event.parameters.length === 2
 			&& event.parameters[0].type === 'number'
+			// && event.parameters[0].name === 'arg1'
+			// && event.parameters[0].isOptional === false
 			&& event.parameters[1].type === 'any'
+			// && event.parameters[1].name === 'arg2'
+			// && event.parameters[1].isOptional === true
 		);
 		const c = meta.events.find(event =>
 			event.name === 'baz'
@@ -63,21 +69,39 @@ describe(`vue-component-meta`, () => {
 		const componentPath = path.resolve(__dirname, '../../vue-test-workspace/vue-component-meta/template-slots/component.vue');
 		const meta = checker.getComponentMeta(componentPath);
 
-		const a = meta.slots.find(event =>
-			event.name === 'default'
-			&& event.propsType === '{ num: number; }'
+		const a = meta.slots.find(slot =>
+			slot.name === 'default'
+			&& slot.propsType === '{ num: number; }'
 		);
-		const b = meta.slots.find(event =>
-			event.name === 'named-slot'
-			&& event.propsType === '{ str: string; }'
+		const b = meta.slots.find(slot =>
+			slot.name === 'named-slot'
+			&& slot.propsType === '{ str: string; }'
 		);
-		const c = meta.slots.find(event =>
-			event.name === 'vbind'
-			&& event.propsType === '{ num: number; str: string; }'
+		const c = meta.slots.find(slot =>
+			slot.name === 'vbind'
+			&& slot.propsType === '{ num: number; str: string; }'
 		);
 
 		expect(a).toBeDefined();
 		expect(b).toBeDefined();
 		expect(c).toBeDefined();
+	});
+
+	it('class-slots', () => {
+
+		const componentPath = path.resolve(__dirname, '../../vue-test-workspace/vue-component-meta/class-slots/component.vue');
+		const meta = checker.getComponentMeta(componentPath);
+
+		const a = meta.slots.find(slot =>
+			slot.name === 'default'
+			&& slot.propsType === '{ num: number; }'
+		);
+		const b = meta.slots.find(slot =>
+			slot.name === 'foo'
+			&& slot.propsType === '{ str: string; }'
+		);
+
+		expect(a).toBeDefined();
+		expect(b).toBeDefined();
 	});
 });
