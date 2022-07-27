e7b5ceebb8b8925cfd11a231a00c0e403ea3d556
Sacha STAFYNIAK <sacha.stafyniak@gmail.com>
2022-07-27 04:45:31 +0200

    Vue meta/improve props extraction (#1637)

--- packages/vue-component-meta/src/index.ts
+++ packages/vue-component-meta/src/index.ts
@@ -1,6 +1,121 @@
 import * as vue from '@volar/vue-language-core';
 import * as ts from 'typescript/lib/tsserverlibrary';
 
+export type PropertyMeta = {
+	name: string;
+	description: string;
+	required: boolean;
+	type: string;
+	tags: { name: string, text?: string }[];
+	schema: PropertyMetaSchema;
+}
+
+export type PropertyMetaSchema = string 
+	| { kind: 'enum', type: string, schema: PropertyMetaSchema[] } 
+	| { kind: 'array', type: string, schema: PropertyMetaSchema[] } 
+	| { kind: 'event', type: string, schema: PropertyMetaSchema[] } 
+	| { kind: 'object', type: string, schema: Record<string, PropertyMeta> }
+
+/**
+ * Helper array to map internal properties added by vue to any components
+ * 
+ * @example
+ * ```ts
+ * import { createComponentMetaChecker, ComponentInternalProperties } from 'vue-component-meta'
+ * 
+ * const checker = createComponentMetaChecker('path/to/tsconfig.json')
+ * const meta = checker.getComponentMeta('path/to/component.vue')
+ * const props = meta.props.filter(prop => !ComponentInternalProperties.includes(prop.name))
+ * ```
+ */
+export const ComponentInternalProperties = [
+	'ref',
+	'key',
+	'ref_for',
+	'ref_key',
+	'onVnodeBeforeMount',
+	'onVnodeMounted',
+	'onVnodeBeforeUpdate',
+	'onVnodeBeforeUnmount',
+	'onVnodeUpdated',
+	'onVnodeUnmounted',
+	'class',
+	'style',
+]
+
+function createSchemaResolvers(typeChecker: ts.TypeChecker, symbolNode: ts.Expression) {
+	function reducer (acc: any, cur: any) {
+		acc[cur.name] = cur
+		return acc
+	}
+	function resolveSymbolSchema (prop: ts.Symbol): PropertyMeta {
+		const subtype = typeChecker.getTypeOfSymbolAtLocation(prop, symbolNode!)
+		typeChecker.getDefaultFromTypeParameter(subtype);
+
+		return {
+			name: prop.getEscapedName().toString(),
+			description: ts.displayPartsToString(prop.getDocumentationComment(typeChecker)),
+			tags: prop.getJsDocTags(typeChecker).map(tag => ({
+				name: tag.name,
+				text: tag.text?.map(part => part.text).join(''),
+			})),
+			required: !Boolean((prop.declarations?.[0] as ts.ParameterDeclaration)?.questionToken ?? false),
+			type: typeChecker.typeToString(subtype),
+			schema: resolveSchema(subtype),
+		}
+	}
+	function resolveCallbackSchema (signature: ts.Signature): PropertyMetaSchema {
+		return {
+			kind: 'event',
+			type: typeChecker.signatureToString(signature),
+			schema: typeChecker.getTypeArguments(typeChecker.getTypeOfSymbolAtLocation(signature.parameters[0], symbolNode) as ts.TypeReference).map(resolveSchema)
+		}
+	}
+	function resolveEventSchema (subtype: ts.Type): PropertyMetaSchema {
+		return (subtype.getCallSignatures().length === 1)
+			? resolveCallbackSchema(subtype.getCallSignatures()[0])
+			: typeChecker.typeToString(subtype)
+	}
+	function resolveNestedSchema (subtype: ts.Type): PropertyMetaSchema {
+		// !!(subtype.flags & ts.TypeFlags.Object)
+		return (subtype.isClassOrInterface() || subtype.isIntersection())
+			? {
+				kind: 'object',
+				type: typeChecker.typeToString(subtype),
+				schema: subtype.getProperties().map(resolveSymbolSchema).reduce(reducer, {})
+			}
+			: resolveEventSchema(subtype)
+	}
+	function resolveArraySchema (subtype: ts.Type): PropertyMetaSchema {
+		// @ts-ignore - typescript internal, isArrayLikeType exists
+		return typeChecker.isArrayLikeType(subtype)
+			? {
+				kind: 'array',
+				type: typeChecker.typeToString(subtype),
+				schema: typeChecker.getTypeArguments(subtype as ts.TypeReference).map(resolveSchema)
+			}
+			: resolveNestedSchema(subtype)
+	}
+	function resolveSchema (subtype: ts.Type): PropertyMetaSchema {
+		return subtype.isUnion()
+			? {
+				kind: 'enum',
+				type: typeChecker.typeToString(subtype),
+				schema: subtype.types.map(resolveArraySchema)
+			}
+			: resolveArraySchema(subtype)
+	}
+
+	return {
+		resolveSymbolSchema,
+		resolveCallbackSchema,
+		resolveEventSchema,
+		resolveNestedSchema,
+		resolveArraySchema,
+		resolveSchema,
+	}
+}
+
 export function createComponentMetaChecker(tsconfigPath: string) {
 
 	const parsedCommandLine = vue.tsShared.createParsedCommandLine(ts, {
@@ -99,14 +214,10 @@ export function createComponentMetaChecker(tsconfigPath: string) {
 
 			if ($props) {
 				const type = typeChecker.getTypeOfSymbolAtLocation($props, symbolNode!);
-				const properties = type.getProperties();
-				return properties.map(prop => ({
-					name: prop.escapedName as string,
-					// @ts-ignore
-					isOptional: !!prop.declarations?.[0]?.questionToken,
-					type: typeChecker.typeToString(typeChecker.getTypeOfSymbolAtLocation(prop, symbolNode!)),
-					documentationComment: ts.displayPartsToString(prop.getDocumentationComment(typeChecker)),
-				}));
+				const properties = type.getApparentProperties();
+				const { resolveSymbolSchema } = createSchemaResolvers(typeChecker, symbolNode!);
+
+				return properties.map(resolveSymbolSchema);
 			}
 
 			return [];
@@ -118,17 +229,13 @@ export function createComponentMetaChecker(tsconfigPath: string) {
 			if ($emit) {
 				const type = typeChecker.getTypeOfSymbolAtLocation($emit, symbolNode!);
 				const calls = type.getCallSignatures();
+				const { resolveSchema } = createSchemaResolvers(typeChecker, symbolNode!);
+
 				return calls.map(call => ({
-					// @ts-ignore
-					name: typeChecker.getTypeOfSymbolAtLocation(call.parameters[0], symbolNode!).value,
-					parametersType: typeChecker.typeToString(typeChecker.getTypeOfSymbolAtLocation(call.parameters[1], symbolNode!)),
-					// @ts-ignore
-					parameters: typeChecker.getTypeArguments(typeChecker.getTypeOfSymbolAtLocation(call.parameters[1], symbolNode!)).map(arg => ({
-						name: 'TODO',
-						type: typeChecker.typeToString(arg),
-						isOptional: 'TODO',
-					})),
-					documentationComment: ts.displayPartsToString(call.getDocumentationComment(typeChecker)),
+					name: (typeChecker.getTypeOfSymbolAtLocation(call.parameters[0], symbolNode!) as ts.StringLiteralType).value,
+					type: typeChecker.typeToString(typeChecker.getTypeOfSymbolAtLocation(call.parameters[1], symbolNode!)),
+					signature: typeChecker.signatureToString(call),
+					schema: typeChecker.getTypeArguments(typeChecker.getTypeOfSymbolAtLocation(call.parameters[1], symbolNode!) as ts.TypeReference).map(resolveSchema),
 				}));
 			}
 
@@ -146,7 +253,7 @@ export function createComponentMetaChecker(tsconfigPath: string) {
 					name: prop.escapedName as string,
 					propsType: typeChecker.typeToString(typeChecker.getTypeOfSymbolAtLocation(typeChecker.getTypeOfSymbolAtLocation(prop, symbolNode!).getCallSignatures()[0].parameters[0], symbolNode!)),
 					// props: {}, // TODO
-					documentationComment: ts.displayPartsToString(prop.getDocumentationComment(typeChecker)),
+					description: ts.displayPartsToString(prop.getDocumentationComment(typeChecker)),
 				}));
 			}
 
--- packages/vue-test-workspace/vue-component-meta/reference-type-props/component.vue
+++ packages/vue-test-workspace/vue-component-meta/reference-type-props/component.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { MyProps } from './my-props';
 
+defineEmits(['event']);
 defineProps<MyProps>();
 </script>
