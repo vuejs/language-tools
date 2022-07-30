d37918c11b6246284b07919d1c8e7fdef639770f
johnsoncodehk <johnsoncodehk@gmail.com>
2022-07-30 22:45:16 +0800

    chore: format

--- packages/vue-test-workspace/vue-component-meta/options-api/component.ts
+++ packages/vue-test-workspace/vue-component-meta/options-api/component.ts
@@ -1,64 +1,64 @@
-import { defineComponent } from "vue"
+import { defineComponent } from "vue";
 
-interface SubmitPayload{
+interface SubmitPayload {
 	/**
 	 * email of user
 	 */
-	email: string
+	email: string;
 	/**
 	 * password of same user
 	 */
-	password: string
+	password: string;
 }
 
 export default defineComponent({
 	emits: {
 		// Validate submit event
-    submit: ({ email, password }: SubmitPayload) => {
-      if (email && password) {
-        return true
-      } else {
-        console.warn('Invalid submit event payload!')
-        return false
-      }
-    }
+		submit: ({ email, password }: SubmitPayload) => {
+			if (email && password) {
+				return true;
+			} else {
+				console.warn('Invalid submit event payload!');
+				return false;
+			}
+		}
 	},
-  props: {
+	props: {
 		/**
-     * Default number
-     */
+		 * Default number
+		 */
 		numberDefault: {
-      type: Number,
-      default: 42
-    },
-    /**
-     * Default function Object
-     */
-    objectDefault: {
-      type: Object,
-      default: () => ({
+			type: Number,
+			default: 42
+		},
+		/**
+		 * Default function Object
+		 */
+		objectDefault: {
+			type: Object,
+			default: () => ({
 				foo: 'bar'
 			})
-    },
-    /**
-     * Default function Array
-     */
-    arrayDefault: {
-      type: Array,
-      default: () => [1, 2, 3]
-    },
-    /**
-     * Default function more complex
-     */
-    complexDefault: {
-      type: Array,
-      default: (props: any) => {
-        if (props.arrayDefault.length > props.numberDefault) {
-          return []
-        } else {
-          return undefined
-        }
-      }
-    },
-  },
-})
+		},
+		/**
+		 * Default function Array
+		 */
+		arrayDefault: {
+			type: Array,
+			default: () => [1, 2, 3]
+		},
+		/**
+		 * Default function more complex
+		 */
+		complexDefault: {
+			type: Array,
+			default: (props: any) => {
+				if (props.arrayDefault.length > props.numberDefault) {
+					return [];
+				} else {
+					return undefined;
+				}
+			}
+		},
+	},
+});
