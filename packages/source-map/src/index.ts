cc488383a54ba6262acb6ea26c1fd90f57795ed5
johnsoncodehk <johnsoncodehk@gmail.com>
2022-08-26 14:17:51 +0800

    perf(source-map): memo

--- packages/source-map/src/index.ts
+++ packages/source-map/src/index.ts
@@ -1,3 +1,5 @@
+import { computed, shallowRef as ref } from '@vue/reactivity';
+
 export interface Range {
 	start: number,
 	end: number,
@@ -55,11 +57,93 @@ export type Mapping<T> = MappingBase & {
 
 export class SourceMapBase<Data = undefined> {
 
-	public mappings: Mapping<Data>[];
+	private __mappings = ref<Mapping<Data>[]>([]);
+	private __memo = computed(() => {
+
+		const self = this;
+		const source = createMemo('sourceRange');
+		const mapped = createMemo('mappedRange');
+
+		return {
+			source,
+			mapped,
+		};
+
+		function createMemo(key: 'mappedRange' | 'sourceRange') {
+
+			const offsets = new Set<number>();
+
+			for (const mapping of self.mappings) {
+
+				offsets.add(mapping[key].start);
+				offsets.add(mapping[key].end);
+
+				if (mapping.additional) {
+					for (const addition of mapping.additional) {
+						offsets.add(addition[key].start);
+						offsets.add(addition[key].end);
+					}
+				}
+			}
+
+			const arr: {
+				offset: number,
+				mappings: Set<Mapping<Data>>,
+			}[] = [...offsets].sort((a, b) => a - b).map(offset => ({ offset, mappings: new Set() }));
+
+			for (const mapping of self.mappings) {
+
+				const startIndex = binarySearch(mapping[key].start)!;
+				const endIndex = binarySearch(mapping[key].end)!;
+
+				for (let i = startIndex; i <= endIndex; i++) {
+					arr[i].mappings.add(mapping);
+				}
+
+				if (mapping.additional) {
+					for (const addition of mapping.additional) {
+
+						const startIndex = binarySearch(addition[key].start)!;
+						const endIndex = binarySearch(addition[key].end)!;
+
+						for (let i = startIndex; i <= endIndex; i++) {
+							arr[i].mappings.add(mapping);
+						}
+					}
+				}
+			}
+
+			return arr;
+
+			function binarySearch(start: number) {
+				let low = 0;
+				let high = arr.length - 1;
+				while (low <= high) {
+					const mid = Math.floor((low + high) / 2);
+					const midValue = arr[mid];
+					if (midValue.offset < start) {
+						low = mid + 1;
+					}
+					else if (midValue.offset > start) {
+						high = mid - 1;
+					}
+					else {
+						return mid;
+					}
+				}
+			}
+		}
+	});
+
+	public get mappings() {
+		return this.__mappings.value;
+	}
+	public set mappings(value) {
+		this.__mappings.value = value;
+	}
 
 	constructor(
 		_mappings?: Mapping<Data>[],
-
 	) {
 		this.mappings = _mappings ?? [];
 	}
@@ -83,25 +167,72 @@ export class SourceMapBase<Data = undefined> {
 
 	protected * getRanges(startOffset: number, endOffset: number, sourceToTarget: boolean, filter?: (data: Data) => boolean) {
 
-		for (const mapping of this.mappings) {
+		const memo = this.__memo.value;
+		const _memo = sourceToTarget ? memo.source : memo.mapped;
 
-			if (filter && !filter(mapping.data))
-				continue;
+		if (_memo.length === 0)
+			return;
 
-			const mapped = this.getRange(startOffset, endOffset, sourceToTarget, mapping.mode, mapping.sourceRange, mapping.mappedRange, mapping.data);
-			if (mapped) {
-				yield mapped;
-			}
-			else if (mapping.additional) {
-				for (const other of mapping.additional) {
-					const mapped = this.getRange(startOffset, endOffset, sourceToTarget, other.mode, other.sourceRange, other.mappedRange, mapping.data);
-					if (mapped) {
-						yield mapped;
-						break; // only return first match additional range
+		const {
+			low: start,
+			high: end,
+		} = startOffset === endOffset ? this.binarySearchMemo(_memo, startOffset) : {
+			low: this.binarySearchMemo(_memo, startOffset).low,
+			high: this.binarySearchMemo(_memo, endOffset).high,
+		};
+		const skip = new Set<Mapping<Data>>();
+
+		for (let i = start; i <= end; i++) {
+
+			for (const mapping of _memo[i].mappings) {
+
+				if (skip.has(mapping)) {
+					continue;
+				}
+				skip.add(mapping);
+
+				if (filter && !filter(mapping.data))
+					continue;
+
+				const mapped = this.getRange(startOffset, endOffset, sourceToTarget, mapping.mode, mapping.sourceRange, mapping.mappedRange, mapping.data);
+				if (mapped) {
+					yield mapped;
+				}
+				else if (mapping.additional) {
+					for (const other of mapping.additional) {
+						const mapped = this.getRange(startOffset, endOffset, sourceToTarget, other.mode, other.sourceRange, other.mappedRange, mapping.data);
+						if (mapped) {
+							yield mapped;
+							break; // only return first match additional range
+						}
 					}
 				}
 			}
 		}
+	}
+
+	private binarySearchMemo(array: typeof this.__memo.value.mapped, start: number) {
+		let low = 0;
+		let high = array.length - 1;
+		while (low <= high) {
+			const mid = Math.floor((low + high) / 2);
+			const midValue = array[mid];
+			if (midValue.offset < start) {
+				low = mid + 1;
+			}
+			else if (midValue.offset > start) {
+				high = mid - 1;
+			}
+			else {
+				low = mid;
+				high = mid;
+				break;
+			}
+		}
+		return {
+			low: Math.max(Math.min(low, high, array.length - 1), 0),
+			high: Math.min(Math.max(low, high, 0), array.length - 1),
+		};
 	}
 
 	private getRange(start: number, end: number, sourceToTarget: boolean, mode: Mode, sourceRange: Range, targetRange: Range, data: Data): [{ start: number, end: number; }, Data] | undefined {
