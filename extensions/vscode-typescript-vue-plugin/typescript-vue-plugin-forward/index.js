5963838b0faf5fcd1f9a772c147cc19e5c8dd470
johnsoncodehk <johnsoncodehk@gmail.com>
2022-08-30 18:33:15 +0800

    fix: cannot debug typescript plugin extension with pnpm
    close #1361

--- extensions/vscode-typescript-vue-plugin/typescript-vue-plugin-forward/index.js
+++ extensions/vscode-typescript-vue-plugin/typescript-vue-plugin-forward/index.js
@@ -1,5 +1,7 @@
 try {
-	module.exports = require('../typescript-vue-plugin');
+	// module.exports = require('../typescript-vue-plugin');
+	// pnpm path
+	module.exports = require('../../../../../extensions/vscode-typescript-vue-plugin/node_modules/typescript-vue-plugin');
 }
 catch {
 	module.exports = require('../../dist');
