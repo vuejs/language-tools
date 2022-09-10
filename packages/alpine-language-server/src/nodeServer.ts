ff80707266acf023464bc0245141dae528e7faf9
johnsoncodehk <johnsoncodehk@gmail.com>
2022-09-11 07:32:10 +0800

    refactor: delete alpine-language-core, alpine-language-service

--- packages/alpine-language-server/src/nodeServer.ts
+++ packages/alpine-language-server/src/nodeServer.ts
@@ -1,19 +1,21 @@
-// import * as alpine from '@volar/alpine-language-service';
-// import { createNodeServer } from '@volar/language-server/out/nodeServer';
+import * as vue from '@volar/vue-language-service';
+import * as vuePlugin from '@volar/vue-language-server/out/plugin';
+import { createNodeServer } from '@volar/language-server/out/nodeServer';
+import * as alpineHtmlPlugin from './vueLanguagePlugins/file-html';
 
-// createNodeServer({
-// 	definitelyExts: ['.html'],
-// 	indeterminateExts: [],
-// 	semanticTokenLegend: alpine.getSemanticTokenLegend(),
-// 	languageService: {
-// 		getLanguageModules(host) {
-// 			return [alpine.createEmbeddedLanguageModule(
-// 				host.getTypeScriptModule(),
-// 				host.getCurrentDirectory(),
-// 				host.getCompilationSettings(),
-// 				{},
-// 			)];
-// 		},
-// 	},
-// 	getDocumentService: alpine.getDocumentService,
-// });
+createNodeServer([{
+	...vuePlugin,
+	exts: ['.html'],
+	languageService: {
+		...vuePlugin.languageService,
+		getLanguageModules(host) {
+			return [vue.createEmbeddedLanguageModule(
+				host.getTypeScriptModule(),
+				host.getCurrentDirectory(),
+				host.getCompilationSettings(),
+				{},
+				[alpineHtmlPlugin],
+			)];
+		},
+	},
+}]);
