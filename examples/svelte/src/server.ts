d6c433ca479acca7b5c30d82f0e247fb8f69d6a4
Johnson Chu <johnsoncodehk@gmail.com>
2022-09-13 15:10:13 +0800

    feat: svelte language server example (#1860)

---
+++ examples/svelte/src/server.ts
@@ -0,0 +1,140 @@
+import useCssPlugin from '@volar-plugins/css';
+import useTsPlugin, { getSemanticTokenLegend } from '@volar-plugins/typescript';
+import { createNodeServer, EmbeddedLanguageModule, FileNode } from '@volar/language-server/node';
+
+const blocksReg = /\<(script|style)[\s\S]*?\>([\s\S]*?)\<\/\1\>/g;
+
+const mod: EmbeddedLanguageModule<FileNode & { snapshot: any; }> = {
+	createSourceFile(fileName, snapshot) {
+		if (fileName.endsWith('.svelte')) {
+
+			const text = snapshot.getText(0, snapshot.getLength());
+
+			return {
+				snapshot,
+				fileName,
+				text,
+				isTsHostFile: false,
+				capabilities: {},
+				mappings: [],
+				embeddeds: getEmbeddeds(fileName, text),
+			};
+		}
+	},
+	updateSourceFile(sourceFile, snapshot) {
+
+		const change = snapshot.getChangeRange(sourceFile.snapshot);
+
+		sourceFile.text = snapshot.getText(0, snapshot.getLength());
+		sourceFile.snapshot = snapshot;
+
+		let incrementalUpdateFailed = false;
+
+		if (change) {
+
+			const changeStart = change.span.start;
+			const changeEnd = change.span.start + change.span.length;
+			const newText = snapshot.getText(changeStart, changeStart + change.newLength);
+			const lengthDiff = change.newLength - (changeEnd - changeStart);
+
+			for (const embedded of sourceFile.embeddeds) {
+
+				const blockStart = embedded.mappings[0].sourceRange.start;
+				const blockEnd = embedded.mappings[0].sourceRange.end;
+
+				if (changeStart >= blockStart && changeEnd <= blockEnd) {
+					const a = snapshot.getText(blockStart, changeStart);
+					const b = newText;
+					const c = snapshot.getText(changeEnd + lengthDiff, blockEnd + lengthDiff);
+					embedded.text = a + b + c;
+					embedded.mappings[0].mappedRange.end += lengthDiff;
+					embedded.mappings[0].sourceRange.end += lengthDiff;
+				}
+				else if (changeEnd <= blockStart && changeStart < blockStart) {
+					embedded.mappings[0].sourceRange.start += lengthDiff;
+					embedded.mappings[0].sourceRange.end += lengthDiff;
+				}
+				else if (changeStart >= blockEnd && changeEnd > blockEnd) {
+					// No need update
+				}
+				else {
+					incrementalUpdateFailed = true;
+				}
+			}
+		}
+		else {
+			incrementalUpdateFailed = true;
+		}
+
+		if (incrementalUpdateFailed) {
+			// full update
+			sourceFile.embeddeds = getEmbeddeds(sourceFile.fileName, sourceFile.text);
+		}
+	},
+};
+
+function getEmbeddeds(fileName: string, text: string) {
+	return [...text.matchAll(blocksReg)].map(block => {
+		const content = block[2];
+		const start = block.index! + block[0].indexOf(content);
+		const end = start + content.length;;
+		return {
+			fileName: fileName + (block[1] === 'script' ? '.js' : '.css'),
+			text: block[2],
+			isTsHostFile: block[1] === 'script',
+			capabilities: {
+				diagnostics: true,
+				foldingRanges: true,
+				documentSymbol: true,
+				codeActions: true,
+				inlayHints: true,
+				formatting: { initialIndentBracket: ['{', '}'] as [string, string] },
+				// formatting: true,
+			},
+			mappings: [
+				{
+					sourceRange: { start, end },
+					mappedRange: { start: 0, end: content.length },
+					mode: 1,
+					data: {
+						hover: true,
+						references: true,
+						definitions: true,
+						rename: true,
+						completion: true,
+						diagnostic: true,
+						semanticTokens: true,
+					},
+				}
+			],
+			embeddeds: [],
+		};
+	});
+}
+
+createNodeServer([{
+	exts: ['.svelte'],
+	languageService: {
+		semanticTokenLegend: getSemanticTokenLegend(),
+		getLanguageModules(host) {
+			return [mod];
+		},
+		getLanguageServicePlugins() {
+			return [
+				useCssPlugin(),
+				useTsPlugin(),
+			];
+		},
+	},
+	documentService: {
+		getLanguageModules(host) {
+			return [mod];
+		},
+		getLanguageServicePlugins() {
+			return [
+				useCssPlugin(),
+				useTsPlugin(),
+			];
+		}
+	},
+}]);
