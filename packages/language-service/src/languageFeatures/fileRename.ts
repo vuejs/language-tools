a13eaf1bb858a000b973055b19e96e4787cf864d
johnsoncodehk <johnsoncodehk@gmail.com>
2022-09-19 22:04:15 +0800

    fix: moving components doesn't trigger import updates
    close #1884

--- packages/language-service/src/languageFeatures/fileRename.ts
+++ packages/language-service/src/languageFeatures/fileRename.ts
@@ -2,6 +2,7 @@ import type { LanguageServiceRuntimeContext } from '../types';
 import { embeddedEditToSourceEdit } from './rename';
 import type * as _ from 'vscode-languageserver-protocol';
 import * as dedupe from '../utils/dedupe';
+import { forEachEmbeddeds } from '@volar/language-core';
 
 export function register(context: LanguageServiceRuntimeContext) {
 
@@ -10,8 +11,21 @@ export function register(context: LanguageServiceRuntimeContext) {
 		const vueDocument = context.documents.get(oldUri);
 
 		if (vueDocument) {
-			oldUri += '.ts';
-			newUri += '.ts';
+
+			let tsExt: string | undefined;
+
+			forEachEmbeddeds(vueDocument.file.embeddeds, embedded => {
+				if (embedded.isTsHostFile && embedded.fileName.replace(vueDocument.file.fileName, '').match(/^\.(js|ts)x?$/)) {
+					tsExt = embedded.fileName.substring(embedded.fileName.lastIndexOf('.'));
+				}
+			});
+
+			if (!tsExt) {
+				return;
+			}
+
+			oldUri += tsExt;
+			newUri += tsExt;
 		}
 
 		for (const plugin of context.plugins) {
