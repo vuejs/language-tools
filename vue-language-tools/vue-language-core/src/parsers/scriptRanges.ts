8ea5136be52ec23d6f4f0d0824adc7041978c741
johnsoncodehk <johnsoncodehk@gmail.com>
2022-10-10 01:26:56 +0800

    fix: cannot unwrap parenthesized expression for export assignment
    #1882

--- vue-language-tools/vue-language-core/src/parsers/scriptRanges.ts
+++ vue-language-tools/vue-language-core/src/parsers/scriptRanges.ts
@@ -21,8 +21,8 @@ export function parseScriptRanges(ts: typeof import('typescript/lib/tsserverlibr
 
 		if (ts.isExportAssignment(raw)) {
 
-			let node: ts.AsExpression | ts.ExportAssignment = raw;
-			while (ts.isAsExpression(node.expression)) { // fix https://github.com/johnsoncodehk/volar/issues/1882
+			let node: ts.AsExpression | ts.ExportAssignment | ts.ParenthesizedExpression = raw;
+			while (ts.isAsExpression(node.expression) || ts.isParenthesizedExpression(node.expression)) { // fix https://github.com/johnsoncodehk/volar/issues/1882
 				node = node.expression;
 			}
 
