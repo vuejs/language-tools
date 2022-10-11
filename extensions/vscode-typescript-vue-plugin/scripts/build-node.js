96993563ee849a5ff1981d9a3d5636d03f571806
johnsoncodehk <johnsoncodehk@gmail.com>
2022-10-12 04:52:48 +0800

    chore: make `typescript` external for `vue-component-meta`

--- extensions/vscode-typescript-vue-plugin/scripts/build-node.js
+++ extensions/vscode-typescript-vue-plugin/scripts/build-node.js
@@ -1,26 +1,29 @@
 require('esbuild').build({
-    entryPoints: ['./node_modules/typescript-vue-plugin/out/index.js'],
-    bundle: true,
-    outfile: './dist/index.js',
-    external: ['vscode'],
-    format: 'cjs',
-    platform: 'node',
-    tsconfig: '../../tsconfig.build.json',
-    minify: process.argv.includes('--minify'),
-    watch: process.argv.includes('--watch'),
-    plugins: [{
-        name: 'umd2esm',
-        setup(build) {
-            build.onResolve({ filter: /(vscode-.*|estree-walker|jsonc-parser)/ }, args => {
-                const pathUmdMay = require.resolve(args.path, { paths: [args.resolveDir] })
-                const pathEsm = pathUmdMay.replace('/umd/', '/esm/')
-                return { path: pathEsm }
-            })
-            build.onResolve({ filter: /\@vue\/compiler-sfc/ }, args => {
-                const pathUmdMay = require.resolve(args.path, { paths: [args.resolveDir] })
-                const pathEsm = pathUmdMay.replace('compiler-sfc.cjs.js', 'compiler-sfc.esm-browser.js')
-                return { path: pathEsm }
-            })
-        },
-    }],
+	entryPoints: ['./node_modules/typescript-vue-plugin/out/index.js'],
+	bundle: true,
+	outfile: './dist/index.js',
+	external: [
+		'vscode',
+		'typescript', // vue-component-meta
+	],
+	format: 'cjs',
+	platform: 'node',
+	tsconfig: '../../tsconfig.build.json',
+	minify: process.argv.includes('--minify'),
+	watch: process.argv.includes('--watch'),
+	plugins: [{
+		name: 'umd2esm',
+		setup(build) {
+			build.onResolve({ filter: /(vscode-.*|estree-walker|jsonc-parser)/ }, args => {
+				const pathUmdMay = require.resolve(args.path, { paths: [args.resolveDir] })
+				const pathEsm = pathUmdMay.replace('/umd/', '/esm/')
+				return { path: pathEsm }
+			})
+			build.onResolve({ filter: /\@vue\/compiler-sfc/ }, args => {
+				const pathUmdMay = require.resolve(args.path, { paths: [args.resolveDir] })
+				const pathEsm = pathUmdMay.replace('compiler-sfc.cjs.js', 'compiler-sfc.esm-browser.js')
+				return { path: pathEsm }
+			})
+		},
+	}],
 }).catch(() => process.exit(1))
