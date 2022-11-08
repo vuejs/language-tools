a59ee4eea0761150d7f49e85eab5e7be81665277
johnsoncodehk <johnsoncodehk@gmail.com>
2022-11-09 00:15:29 +0800

    feat(vue-tsc): support for TS v5
    #2095

--- vue-language-tools/vue-tsc/bin/vue-tsc.js
+++ vue-language-tools/vue-tsc/bin/vue-tsc.js
@@ -6,46 +6,32 @@ const tscPath = require.resolve('typescript/lib/tsc');
 const proxyPath = require.resolve('../out/proxy');
 
 fs.readFileSync = (...args) => {
-    if (args[0] === tscPath) {
-        let tsc = readFileSync(...args);
+	if (args[0] === tscPath) {
+		let tsc = readFileSync(...args);
 
-        // add *.vue files to allow extensions
-        tsc = tsc.replace(
-            `ts.supportedTSExtensions = [[".ts", ".tsx", ".d.ts"], [".cts", ".d.cts"], [".mts", ".d.mts"]];`,
-            // `ts.supportedTSExtensions = [[".ts", ".tsx", ".d.ts"], [".cts", ".d.cts"], [".mts", ".d.mts"], [".vue", ".md", ".html"]];`,
-            `ts.supportedTSExtensions = [[".ts", ".tsx", ".d.ts"], [".cts", ".d.cts"], [".mts", ".d.mts"], [".vue"]];`,
-        );
-        tsc = tsc.replace(
-            `ts.supportedJSExtensions = [[".js", ".jsx"], [".mjs"], [".cjs"]];`,
-            // `ts.supportedJSExtensions = [[".js", ".jsx"], [".mjs"], [".cjs"], [".vue", ".md", ".html"]];`,
-            `ts.supportedJSExtensions = [[".js", ".jsx"], [".mjs"], [".cjs"], [".vue"]];`,
-        );
-        tsc = tsc.replace(
-            `var allSupportedExtensions = [[".ts", ".tsx", ".d.ts", ".js", ".jsx"], [".cts", ".d.cts", ".cjs"], [".mts", ".d.mts", ".mjs"]];`,
-            // `var allSupportedExtensions = [[".ts", ".tsx", ".d.ts", ".js", ".jsx"], [".cts", ".d.cts", ".cjs"], [".mts", ".d.mts", ".mjs"], [".vue", ".md", ".html"]];`,
-            `var allSupportedExtensions = [[".ts", ".tsx", ".d.ts", ".js", ".jsx"], [".cts", ".d.cts", ".cjs"], [".mts", ".d.mts", ".mjs"], [".vue"]];`,
-        );
+		// add *.vue files to allow extensions
+		tryReplace(/supportedTSExtensions = .*(?=;)/, s => s + '.concat([[".vue"]])');
+		tryReplace(/supportedJSExtensions = .*(?=;)/, s => s + '.concat([[".vue"]])');
+		tryReplace(/allSupportedExtensions = .*(?=;)/, s => s + '.concat([[".vue"]])');
 
-        // proxy createProgram apis
-        tsc = tsc.replace(
-            `function createProgram(rootNamesOrOptions, _options, _host, _oldProgram, _configFileParsingDiagnostics) {`,
-            `function createProgram(rootNamesOrOptions, _options, _host, _oldProgram, _configFileParsingDiagnostics) { return require(${JSON.stringify(proxyPath)}).createProgramProxy(...arguments);`,
-        );
+		// proxy startTracing, dumpTracingLegend
+		tryReplace(/ = tracingEnabled\./g, ` = require(${JSON.stringify(proxyPath)}).loadTsLib().`);
 
-        // proxy tracing
-        tsc = tsc.replace(
-            `ts.startTracing = tracingEnabled.startTracing;`,
-            `ts.startTracing = require(${JSON.stringify(proxyPath)}).loadTsLib().startTracing;`,
-        );
+		// proxy createProgram apis
+		tryReplace(/function createProgram\(.+\) {/, s => s + ` return require(${JSON.stringify(proxyPath)}).createProgramProxy(...arguments);`);
 
-        tsc = tsc.replace(
-            `ts.dumpTracingLegend = tracingEnabled.dumpLegend;`,
-            `ts.dumpTracingLegend = require(${JSON.stringify(proxyPath)}).loadTsLib().dumpTracingLegend;`,
-        );
+		return tsc;
 
-        return tsc;
-    }
-    return readFileSync(...args);
+		function tryReplace(search, replace) {
+			const before = tsc;
+			tsc = tsc.replace(search, replace);
+			const after = tsc;
+			if (after === before) {
+				throw 'Search string not found: ' + JSON.stringify(search.toString());
+			}
+		}
+	}
+	return readFileSync(...args);
 };
 
 require(tscPath);
