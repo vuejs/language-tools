c6effffd04e3ec6644204eb3b4f8b1ee36f4eced
johnsoncodehk <johnsoncodehk@gmail.com>
2023-02-06 06:02:15 +0800

    feat: remove diagnostics version filter

--- vue-language-tools/vscode-vue/src/middleware.ts
+++ vue-language-tools/vscode-vue/src/middleware.ts
@@ -4,25 +4,6 @@ import * as lsp from 'vscode-languageclient';
 import { attrNameCasings, tagNameCasings } from './features/nameCasing';
 
 export const middleware: lsp.Middleware = {
-	handleDiagnostics: (uri, diagnostics, next) => {
-		const document = vscode.workspace.textDocuments.find(d => d.uri.toString() === uri.toString());
-		if (document) {
-			let outdated = false;
-			for (const diagnostic of diagnostics) {
-				const data = (diagnostic as any).data;
-				if (typeof data === 'object' && 'version' in data) {
-					if (document.version !== data.version) {
-						outdated = true;
-						break;
-					}
-				}
-			}
-			if (outdated) {
-				return;
-			}
-		}
-		next(uri, diagnostics);
-	},
 	workspace: {
 		configuration(params, token, next) {
 			if (params.items.some(item => item.section === 'volar.completion.preferredAttrNameCase' || item.section === 'volar.completion.preferredTagNameCase')) {
