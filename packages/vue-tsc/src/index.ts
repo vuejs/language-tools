fe7bd5d5049b522eb027c69460e4d214716469a7
Johnson Chu <johnsoncodehk@gmail.com>
2023-02-12 07:04:40 +0800

    feat(vue-tsc): unsupport tracing (#2378)

--- packages/vue-tsc/src/index.ts
+++ packages/vue-tsc/src/index.ts
@@ -22,6 +22,9 @@ export function createProgram(options: ts.CreateProgramOptions) {
 	if (!options.options.noEmit && options.options.noEmitOnError)
 		throw toThrow('noEmitOnError is not supported');
 
+	if (options.options.extendedDiagnostics || options.options.generateTrace)
+		throw toThrow('--extendedDiagnostics / --generateTrace is not supported, please run `Write Virtual Files` in VSCode to write virtual files and use `--extendedDiagnostics` / `--generateTrace` via tsc instead of vue-tsc to debug.');
+
 	if (!options.host)
 		throw toThrow('!options.host');
 
@@ -151,10 +154,6 @@ export function createProgram(options: ts.CreateProgramOptions) {
 	}
 
 	return program;
-}
-
-export function loadTsLib() {
-	return ts;
 }
 
 function toThrow(msg: string) {
