5abb2acda2f451b950f5f7cb26723a707e881f7a
johnsoncodehk <johnsoncodehk@gmail.com>
2023-03-14 03:02:12 +0800

    fix: fixed invalid server path

--- packages/vscode-vue/server.js
+++ packages/vscode-vue/server.js
@@ -1,3 +1,3 @@
-let modulePath = './dist/node/server';
+let modulePath = './dist/server';
 try { modulePath = require.resolve('@volar/vue-language-server/bin/vue-language-server'); } catch { }
 module.exports = require(modulePath);
