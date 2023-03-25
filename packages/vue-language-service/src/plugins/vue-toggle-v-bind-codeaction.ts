421a9d9dfdb9555f6c0783001a824852bd0b63ca
Vitaly Turovsky <vital2580@icloud.com>
2023-03-25 20:35:59 +0300

    add remove v-bind code action

--- packages/vue-language-service/src/plugins/vue-toggle-v-bind-codeaction.ts
+++ packages/vue-language-service/src/plugins/vue-toggle-v-bind-codeaction.ts
@@ -30,42 +30,73 @@ export default function (): LanguageServicePlugin {
 					}
 					for (const prop of node.props) {
 						if (
-							prop.type === NodeTypes.ATTRIBUTE
-							&& startOffset - templateStartOffset >= prop.loc.start.offset
+							startOffset - templateStartOffset >= prop.loc.start.offset
 							&& endOffset - templateStartOffset <= prop.loc.end.offset
 						) {
-							const addVBindPos = document.positionAt(templateStartOffset + prop.loc.start.offset);
-							const edits: vscode.TextEdit[] = [];
-							let newPosition: vscode.Position | undefined;
-							edits.push({
-								newText: ':',
-								range: {
-									start: addVBindPos,
-									end: addVBindPos
-								},
-							});
-							if (!prop.value) {
-								const addValuePos = document.positionAt(templateStartOffset + prop.loc.end.offset);
+							if (prop.type === NodeTypes.DIRECTIVE && prop.exp) {
+								const ts = ctx!.typescript!.module;
+								const sourceFile = ts.createSourceFile('/a.ts', prop.exp.loc.source, ts.ScriptTarget.Latest, true);
+								const firstStatement = sourceFile.statements[0];
+								if (sourceFile.statements.length === 1 && ts.isExpressionStatement(firstStatement) && ts.isStringLiteralLike(firstStatement.expression)) {
+									const stringNode = sourceFile.statements[0];
+									const removeTextRanges: [number, number][] = [
+										[prop.loc.start.offset, prop.loc.start.offset + 1],
+										[prop.exp.loc.start.offset, prop.exp.loc.start.offset + stringNode.pos + stringNode.getLeadingTriviaWidth() + 1],
+										[prop.exp.loc.start.offset + stringNode.end - 1, prop.exp.loc.end.offset],
+									];
+									result.push({
+										title: 'Remove v-bind from attribute',
+										kind: 'refactor.rewrite.removeVBind',
+										edit: {
+											changes: {
+												[document.uri]: removeTextRanges.map(range => ({
+													newText: '',
+													range: {
+														start: document.positionAt(templateStartOffset + range[0]),
+														end: document.positionAt(templateStartOffset + range[1]),
+													}
+												}))
+											},
+										},
+									});
+								}
+							}
+							if (
+								prop.type === NodeTypes.ATTRIBUTE
+							) {
+								const addVBindPos = document.positionAt(templateStartOffset + prop.loc.start.offset);
+								const edits: vscode.TextEdit[] = [];
+								let newPosition: vscode.Position | undefined;
 								edits.push({
-									newText: '=""',
+									newText: ':',
 									range: {
-										start: addValuePos,
-										end: addValuePos
+										start: addVBindPos,
+										end: addVBindPos
 									},
 								});
-								newPosition = {
-									line: addValuePos.line,
-									character: addValuePos.character + ':'.length + '="'.length,
-								};
+								if (!prop.value) {
+									const addValuePos = document.positionAt(templateStartOffset + prop.loc.end.offset);
+									edits.push({
+										newText: '=""',
+										range: {
+											start: addValuePos,
+											end: addValuePos
+										},
+									});
+									newPosition = {
+										line: addValuePos.line,
+										character: addValuePos.character + ':'.length + '="'.length,
+									};
+								}
+								result.push({
+									title: 'Add v-bind to attribute',
+									kind: 'refactor.rewrite.addVBind',
+									edit: {
+										changes: { [document.uri]: edits },
+									},
+									command: newPosition ? ctx?.commands.createSetSelectionCommand(newPosition) : undefined,
+								});
 							}
-							result.push({
-								title: 'Add v-bind to attribute',
-								kind: 'refactor.rewrite.addVBind',
-								edit: {
-									changes: { [document.uri]: edits },
-								},
-								command: newPosition ? ctx?.commands.createSetSelectionCommand(newPosition) : undefined,
-							});
 						}
 					}
 				});
