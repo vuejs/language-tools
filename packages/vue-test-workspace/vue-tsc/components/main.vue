8ca19a35d94b7d518aff841b0ce1aa7a7dc66ade
johnsoncodehk <johnsoncodehk@gmail.com>
2023-03-31 20:25:06 +0800

    chore: add component type tests for vuejs.org/api/sfc-script-setup

--- packages/vue-test-workspace/vue-tsc/components/main.vue
+++ packages/vue-test-workspace/vue-tsc/components/main.vue
@@ -1,7 +1,49 @@
 <script lang="ts">
 import { exactType } from '../shared';
-import { defineComponent } from 'vue';
-import EmptyScriptSetup from './empty-script-setup.vue';
+import { defineComponent, ref } from 'vue';
+import ScriptSetup from './script-setup.vue';
+import ScriptSetupExpose from './script-setup-expose.vue';
+import ScriptSetupTypeOnly from './script-setup-type-only.vue';
 
-exactType(EmptyScriptSetup, defineComponent({}));
+// https://vuejs.org/api/sfc-script-setup.html#defineprops-defineemits
+exactType(ScriptSetup, defineComponent({
+	props: {
+		foo: String,
+	},
+	emits: ['change', 'delete'],
+	setup() {
+		return {};
+	},
+}));
+// https://vuejs.org/api/sfc-script-setup.html#defineexpose
+exactType(ScriptSetupExpose, defineComponent({
+	setup() {
+		const a = 1
+		const b = ref(2)
+		return {
+			a,
+			b
+		};
+	},
+}));
+// https://vuejs.org/api/sfc-script-setup.html#typescript-only-features
+exactType(ScriptSetupTypeOnly, defineComponent({
+	props: {
+		foo: {
+			type: String,
+			required: true
+		},
+		bar: {
+			type: Number,
+			required: false
+		},
+	},
+	emits: {
+		change(_id: number) {},
+		update(_value: string) {},
+	},
+	setup() {
+		return {};
+	},
+}));
 </script>
---
+++ packages/vue-test-workspace/vue-tsc/components/script-setup-expose.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+
+const a = 1
+const b = ref(2)
+
+defineExpose({
+	a,
+	b
+})
+</script>
