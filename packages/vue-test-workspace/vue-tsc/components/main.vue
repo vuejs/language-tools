381cb016cb4084e8e52ad065633035dcfe2f93db
johnsoncodehk <johnsoncodehk@gmail.com>
2023-03-31 19:44:47 +0800

    chore: add type test for empty script setup component

---
+++ packages/vue-test-workspace/vue-tsc/components/main.vue
@@ -0,0 +1,7 @@
+<script lang="ts">
+import { exactType } from '../shared';
+import { defineComponent } from 'vue';
+import EmptyScriptSetup from './empty-script-setup.vue';
+
+exactType(EmptyScriptSetup, defineComponent({}));
+</script>
