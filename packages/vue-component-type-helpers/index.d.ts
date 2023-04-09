f1afe50e64deaeee65ba6826c33afc6e8848bba5
johnsoncodehk <johnsoncodehk@gmail.com>
2023-04-10 03:14:51 +0800

    feat(type-helpers): looser functional component type

--- packages/vue-component-type-helpers/index.d.ts
+++ packages/vue-component-type-helpers/index.d.ts
@@ -1,20 +1,20 @@
 export type ComponentProps<T> =
-	T extends (props: infer P) => any ? P :
+	T extends (props: infer P, ...args: any) => any ? P :
 	T extends new () => { $props: infer P } ? NonNullable<P> :
 	{};
 
 export type ComponentSlots<T> =
-	T extends (props: any, ctx: { slots: infer S }) => any ? NonNullable<S> :
+	T extends (props: any, ctx: { slots: infer S }, ...args: any) => any ? NonNullable<S> :
 	T extends new () => { $slots: infer S } ? NonNullable<S> :
 	{};
 
 export type ComponentEmit<T> =
-	T extends (props: any, ctx: { emit: infer E }) => any ? NonNullable<E> :
+	T extends (props: any, ctx: { emit: infer E }, ...args: any) => any ? NonNullable<E> :
 	T extends new () => { $emit: infer E } ? NonNullable<E> :
 	{};
 
 export type ComponentExposed<T> =
-	T extends (props: any, ctx: { expose(exposed: infer E): any }) => any ? NonNullable<E> :
+	T extends (props: any, ctx: { expose(exposed: infer E): any }, ...args: any) => any ? NonNullable<E> :
 	T extends new () => infer E ? E :
 	{};
 
@@ -23,6 +23,6 @@ export type ComponentExposed<T> =
  */
 
 export type Vue2ComponentSlots<T> =
-	T extends (props: any, ctx: { slots: infer S }) => any ? NonNullable<S> :
+	T extends (props: any, ctx: { slots: infer S }, ...args: any) => any ? NonNullable<S> :
 	T extends new () => { $scopedSlots: infer S } ? NonNullable<S> :
 	{};
