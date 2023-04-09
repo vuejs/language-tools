710f9407049ccd860c7206cac3c2597f9fcdba3e
johnsoncodehk <johnsoncodehk@gmail.com>
2023-04-10 00:48:30 +0800

    feat: add vue-type-helpers package

---
+++ packages/vue-test-workspace/vue-tsc/type-helpers/main.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+import type { ComponentProps, ComponentSlots, ComponentEmit, ComponentExposed } from 'vue-type-helpers';
+import { exactType } from '../shared';
+import ScriptSetup from '../components/script-setup.vue';
+
+exactType((new ScriptSetup()).$props, {} as ComponentProps<typeof ScriptSetup>);
+exactType((new ScriptSetup()).$emit, {} as ComponentEmit<typeof ScriptSetup>);
+exactType((new ScriptSetup()).$slots, {} as ComponentSlots<typeof ScriptSetup>);
+exactType((new ScriptSetup()), {} as ComponentExposed<typeof ScriptSetup>);
+</script>
