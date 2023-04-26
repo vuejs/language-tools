32de5847a5d47b98c421ee72b3c18cb20c59069f
Johnson Chu <johnsoncodehk@gmail.com>
2023-04-27 04:16:02 +0800

    fix: avoid props type-checking for `VNode`
    close #2720

---
+++ packages/vue-test-workspace/vue-tsc/#2720/main.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <component :is="component1"></component>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { VNode } from 'vue';
+import { h } from 'vue';
+
+const component1: VNode = h('span', 'Foobar');
+</script>
