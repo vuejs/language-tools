a15d198a292ae0eac124fba8ff7ca4c0d1cce488
kalvens <kalvens@rtvision.com>
2023-05-10 20:11:07 -0500

    fix(language-core): try handle node next module resolution

--- packages/vue-language-core/src/languageModule.ts
+++ packages/vue-language-core/src/languageModule.ts
@@ -2,10 +2,42 @@ import type { Language } from '@volar/language-core';
 import { posix as path } from 'path';
 import { getDefaultVueLanguagePlugins } from './plugins';
 import { VueFile } from './sourceFile';
-import { VueCompilerOptions } from './types';
 import * as sharedTypes from './utils/directorySharedTypes';
 import type * as ts from 'typescript/lib/tsserverlibrary';
 import { resolveVueCompilerOptions } from './utils/ts';
+
+// tried to do something similar to
+// vue compiler-sfc
+// https://github.com/vuejs/core/blob/bdf3492aee2a970ab7e73cf833d729679c731111/packages/compiler-sfc/src/script/resolveType.ts#L845
+// below utils copied from compiler-sfc util script, can it be shared?
+// https://github.com/vuejs/core/blob/bdf3492aee2a970ab7e73cf833d729679c731111/packages/compiler-sfc/src/script/utils.ts#LL83C1-L100C2
+const identity = (str: string) => str;
+const fileNameLowerCaseRegExp = /[^\u0130\u0131\u00DFa-z0-9\\/:\-_\. ]+/g;
+const toLowerCase = (str: string) => str.toLowerCase();
+
+function toFileNameLowerCase(x: string) {
+	return fileNameLowerCaseRegExp.test(x)
+		? x.replace(fileNameLowerCaseRegExp, toLowerCase)
+		: x;
+}
+
+export function createGetCanonicalFileName(useCaseSensitiveFileNames: boolean) {
+	return useCaseSensitiveFileNames ? identity : toFileNameLowerCase;
+}
+let moduleCache: ts.ModuleResolutionCache | null = null;
+function getModuleResolutionCache(
+	ts: typeof import("typescript/lib/tsserverlibrary"),
+	options: ts.CompilerOptions
+) {
+	if (moduleCache === null) {
+		moduleCache = ts.createModuleResolutionCache(
+			process.cwd(),
+			createGetCanonicalFileName(ts.sys.useCaseSensitiveFileNames),
+			options
+		);
+	}
+	return moduleCache;
+}
 
 export function createLanguage(
 	compilerOptions: ts.CompilerOptions = {},
@@ -13,9 +45,6 @@ export function createLanguage(
 	ts: typeof import('typescript/lib/tsserverlibrary') = require('typescript'),
 	codegenStack: boolean = false,
 ): Language {
-
-	patchResolveModuleNames(ts, vueCompilerOptions);
-
 	const vueLanguagePlugin = getDefaultVueLanguagePlugins(
 		ts,
 		compilerOptions,
@@ -69,6 +98,29 @@ export function createLanguage(
 					}
 					return host.getScriptSnapshot(fileName);
 				},
+				resolveModuleNameLiterals(
+					moduleLiterals,
+					containingFile,
+					redirectedReference,
+					options,
+					sourceFile
+				) {
+					return moduleLiterals.map((_, index) => {
+						let moduleName = moduleLiterals[index].text;
+						if (sourceFile.impliedNodeFormat === ts.ModuleKind.ESNext && moduleName.endsWith('.vue')) {
+							moduleName = `${moduleName}.js`;
+						}
+						return ts.resolveModuleName(
+							moduleName,
+							containingFile,
+							options,
+							this,
+							getModuleResolutionCache(ts, options),
+							redirectedReference,
+							sourceFile.impliedNodeFormat
+						)
+					});
+				}
 			};
 		},
 	};
@@ -92,26 +144,4 @@ export function createLanguages(
 		createLanguage(compilerOptions, vueCompilerOptions, ts, codegenStack),
 		...vueCompilerOptions.experimentalAdditionalLanguageModules?.map(module => require(module)) ?? [],
 	];
-}
-
-function patchResolveModuleNames(
-	ts: typeof import('typescript/lib/tsserverlibrary'),
-	vueCompilerOptions: VueCompilerOptions,
-) {
-	try {
-		// from https://github.com/vuejs/language-tools/pull/1543
-		if (!((ts as any).__vuePatchResolveModuleNames)) {
-			(ts as any).__vuePatchResolveModuleNames = true;
-			const resolveModuleNames = ts.resolveModuleName;
-			ts.resolveModuleName = (...args) => {
-				if (args[6] === ts.ModuleKind.ESNext && vueCompilerOptions.extensions.some(ext => args[0].endsWith(ext))) {
-					args[6] = ts.ModuleKind.CommonJS;
-				}
-				return resolveModuleNames(...args);
-			};
-		}
-	}
-	catch (e) {
-		// console.warn('[volar] patchResolveModuleNames failed', e);
-	}
 }
