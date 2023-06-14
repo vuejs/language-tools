9baad669742f09526db0761f2a1a9ba2b6bb9d32
Johnson Chu <johnsoncodehk@gmail.com>
2023-06-14 21:12:10 +0800

    test type

--- packages/vue-test-workspace/vue-tsc/defineEmits-new-syntax/Comp.vue
+++ packages/vue-test-workspace/vue-tsc/defineEmits-new-syntax/Comp.vue
@@ -1,4 +1,3 @@
 <script setup lang="ts">
 defineEmits<{ foo: [string] }>()
 </script>
-
--- packages/vue-test-workspace/vue-tsc/defineEmits-new-syntax/main.vue
+++ packages/vue-test-workspace/vue-tsc/defineEmits-new-syntax/main.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
+import { exactType } from '../shared';
 import Comp from './Comp.vue';
 </script>
 
 <template>
-	<Comp @foo="(x) => x" />
+	<Comp @foo="(x) => exactType(x, {} as string)" />
 </template>
