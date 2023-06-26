410b556c8aea51c0432c8b6d648ad3f7fbba6e1a
Johnson Chu <johnsoncodehk@gmail.com>
2023-06-26 22:50:26 +0800

    fix test
    fix: types
    
    refactor
    
    chore: format
    
    refactor

--- packages/vue-test-workspace/vue-tsc/non-strict-template/#3171/main.vue
+++ packages/vue-test-workspace/vue-tsc/non-strict-template/#3171/main.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { h } from 'vue';
+import { defineComponent, h } from 'vue';
 
 const eventSummary = () => h('div');
 </script>
 
 <template>
 	<div>
-		<component :is="{ render: eventSummary }" />
+		<component :is="defineComponent({ render: eventSummary })" />
 	</div>
 </template>
