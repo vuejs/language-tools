a7300f376171013c0fd86d25bcd50a9d73cb519e
so1ve <i@mk1.io>
2023-06-27 16:24:09 +0800

    test

---
+++ packages/vue-test-workspace/vue-tsc/non-strict-template/#1886/main.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { exactType } from 'vue-tsc/shared';
+
+declare module 'vue' {
+	export interface GlobalComponents {
+		FunctionalComponent: any;
+	}
+}
+
+declare const FunctionalComponent: new () => {
+  $slots: {
+		foo: (props: { bar: string }) => any;
+	}
+};
+</script>
+<template>
+  <FunctionalComponent>
+		<template #foo="{ bar }">
+			{{ exactType(bar, '' as string) }}
+		</template>
+	</FunctionalComponent>
+</template>
