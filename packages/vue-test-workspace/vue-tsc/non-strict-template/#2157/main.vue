33fc9f70079af6a8fecc6cfd501c3e7087f04cf4
so1ve <i@mk1.io>
2023-06-27 16:32:31 +0800

    test: test #2157

---
+++ packages/vue-test-workspace/vue-tsc/non-strict-template/#2157/main.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+declare module 'vue' {
+  export interface GlobalComponents {
+    Transition: typeof import('vue')['Transition'];
+  }
+};
+</script>
+
+<template>
+	<Transition enter-active-class=""></Transition>
+</template>
