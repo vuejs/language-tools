fb501c2ffb531ab74251ce1f7c1190133864ad12
Ray <i@mk1.io>
2023-07-13 11:15:13 +0800

    fix: allow slots to have no arguments (#3376)
    Co-authored-by: Johnson Chu <johnsoncodehk@gmail.com>

---
+++ packages/vue-test-workspace/vue-tsc/non-strict-template/#3371/main.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineSlots<{
+	content(): any;
+	prop(props: { foo: string }): any;
+}>();
+</script>
+
+<template>
+	<slot name="content" />
+	<slot name="prop" :foo="''" />
+</template>
