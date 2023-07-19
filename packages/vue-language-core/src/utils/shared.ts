f9f6fcda584a964d20bd141bb82c5b71f6d04d9c
Ray <i@mk1.io>
2023-07-20 01:04:08 +0800

    simplify

--- packages/vue-language-core/src/utils/shared.ts
+++ packages/vue-language-core/src/utils/shared.ts
@@ -4,19 +4,11 @@ export function getSlotsPropertyName(vueVersion: number) {
 	return vueVersion < 3 ? '$scopedSlots' : '$slots';
 }
 
-const cacheStringFunction = (fn: (str: string) => string) => {
-	const cache: Record<string, string> = Object.create(null);
-	return (str: string) => {
-		const hit = cache[str];
-		return hit || (cache[str] = fn(str));
-	};
-};
-
-export const hyphenate = cacheStringFunction((str: string) => {
+export function hyphenate(str: string) {
 	let hyphencase = _hyphenate(str);
 	// Fix https://github.com/vuejs/core/issues/8811
 	if (str[0] === str[0].toUpperCase()) {
 		hyphencase = '-' + hyphencase;
 	}
 	return hyphencase;
-});
+}
