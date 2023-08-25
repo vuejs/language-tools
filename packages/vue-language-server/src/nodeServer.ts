eceeb92a2750902ada0a6592ca3f2c25fd319468
Colton Donnelly <colton@donn.io>
2023-08-25 16:48:05 +0000

    add --stdio flag for lsp over stdio

--- packages/vue-language-server/src/nodeServer.ts
+++ packages/vue-language-server/src/nodeServer.ts
@@ -1,7 +1,15 @@
 import { createConnection, startLanguageServer } from '@volar/language-server/node';
 import { createServerPlugin } from './languageServerPlugin';
+import * as vscode from 'vscode-languageserver/node';
 
-const connection = createConnection();
+let connection: vscode.Connection;
+if (process.argv.includes('--stdio')) {
+	console.log = (...args: any[]) => console.warn(...args);
+	connection = vscode.createConnection(process.stdin, process.stdout);
+} else {
+	connection = createConnection();
+}
+
 const plugin = createServerPlugin(connection);
 
 startLanguageServer(connection, plugin);
