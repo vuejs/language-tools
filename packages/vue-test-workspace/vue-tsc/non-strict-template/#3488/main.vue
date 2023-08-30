9bc2412716550bfafb8a193ba9f403f776e8e59b
so1ve <i@mk1.io>
2023-08-30 17:11:47 +0800

    test: add tests

---
+++ packages/vue-test-workspace/vue-tsc/non-strict-template/#3488/main.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { exactType } from 'vue-tsc/shared';
+
+const x = '';
+
+defineExpose({ x: 3 });
+</script>
+
+<template>
+  {{ exactType(x, '' as const) }}
+</template>
