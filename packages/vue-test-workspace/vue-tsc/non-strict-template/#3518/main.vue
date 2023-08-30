e101b97a8c620a797a933f6eb94110f3eb1b23b3
so1ve <i@mk1.io>
2023-08-30 23:33:46 +0800

    test: add tests for #3518

---
+++ packages/vue-test-workspace/vue-tsc/non-strict-template/#3518/main.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { exactType } from 'vue-tsc/shared';
+
+const x = '';
+
+defineProps<{ x: number }>();
+</script>
+
+<template>
+  {{ exactType(x, '' as const) }}
+</template>
