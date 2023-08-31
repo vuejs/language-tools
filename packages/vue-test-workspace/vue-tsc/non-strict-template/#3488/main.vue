66d9af312e129e37a62ffd73cb315e7558e65ec8
so1ve <i@mk1.io>
2023-08-31 17:19:59 +0800

    testL update

--- packages/vue-test-workspace/vue-tsc/non-strict-template/#3488/main.vue
+++ packages/vue-test-workspace/vue-tsc/non-strict-template/#3488/main.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import { exactType } from 'vue-tsc/shared';
-
-const x = '';
-
 defineExpose({ x: 3 });
 </script>
 
 <template>
-	{{ exactType(x, '' as const) }}
+	{{ 
+		// @ts-ignore
+		x
+	}}
 </template>
--- packages/vue-test-workspace/vue-tsc/non-strict-template/#3518/main.vue
+++ packages/vue-test-workspace/vue-tsc/non-strict-template/#3518/main.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import { exactType } from 'vue-tsc/shared';
 
-const x = '';
+const foo = '';
 
-defineProps<{ x: number }>();
+defineProps<{ foo: number }>();
+
+const bar = '';
+
+defineExpose({ bar: 1 });
 </script>
 
 <template>
-  {{ exactType(x, '' as const) }}
+  {{ exactType(foo, '' as const) }}
+	{{ exactType(bar, '' as const) }}
 </template>
