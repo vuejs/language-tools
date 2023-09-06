46a3736cb4ad68dc614c6e3cba644d9ab62c068d
Johnson Chu <johnsoncodehk@gmail.com>
2023-09-06 16:25:04 +0800

    chore: remove bun message box

--- packages/vscode-vue/src/nodeClientMain.ts
+++ packages/vscode-vue/src/nodeClientMain.ts
@@ -67,7 +67,6 @@ export async function activate(context: vscode.ExtensionContext) {
 			},
 		};
 		if (config.server.runtime === 'bun') {
-			vscode.window.showInformationMessage('Using experimental Bun runtime.');
 			serverOptions = {
 				run: {
 					transport: {
