6fde01faea2bdc0770b7917e90e8a921f904f765
so1ve <i@mk1.io>
2023-09-18 12:06:13 +0800

    teest

---
+++ packages/vue-test-workspace/vue-tsc/non-strict-template/#3257/Comp.vue
@@ -0,0 +1,3 @@
+<script setup lang="ts" generic="TRow extends DataType = DataType">
+export interface DataType {}
+</script>
---
+++ packages/vue-test-workspace/vue-tsc/non-strict-template/#3257/main.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+import Comp from './Comp.vue';
+
+defineComponent({
+	components: {
+		Comp,
+	}
+});
+</script>
