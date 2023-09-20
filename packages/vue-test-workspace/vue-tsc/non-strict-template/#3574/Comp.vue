f41bffd5f6554671d324f0af47c8f7c33ee3b86a
Johnson Chu <johnsoncodehk@gmail.com>
2023-09-20 21:10:34 +0800

    Revert "fix"
    This reverts commit b438030e4ba2ea300646d901c969377933b50b7a.

--- packages/vue-test-workspace/vue-tsc/non-strict-template/#3574/Comp.vue
+++
@@ -1,5 +0,0 @@
-<script lang="ts" setup>
-defineEmits<{
-  (event: 'change', value: string): void
-}>()
-</script>
--- packages/vue-test-workspace/vue-tsc/non-strict-template/#3574/main.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <Comp @change="(value) => console.log(value)" />
-  <!--            ^^^^^ is inferred as `any` now, should be `string` -->
-</template>
-
-<script lang="ts" setup>
-import Comp from './Comp.vue'
-</script>
