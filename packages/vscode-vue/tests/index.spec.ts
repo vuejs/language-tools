8d523aba0c75f8c8ee2843f96ce08f70877a1955
Johnson Chu <johnsoncodehk@gmail.com>
2023-10-08 23:22:19 +0800

    chore: rollback `vscode-languageclient` to 8.1.0

---
+++ packages/vscode-vue/tests/index.spec.ts
@@ -0,0 +1,13 @@
+import { describe, expect, test } from 'vitest';
+
+describe('vscode', () => {
+	test('vscode versions should be consistent ', () => {
+
+		const languageClientVersion = require('vscode-languageclient/package.json').engines.vscode;
+		const typesVersion = require('../package.json').devDependencies['@types/vscode'];
+		const enginesVersion = require('../package.json').engines.vscode;
+
+		expect(typesVersion).toBe(languageClientVersion);
+		expect(enginesVersion).toBe(languageClientVersion);
+	});
+});
