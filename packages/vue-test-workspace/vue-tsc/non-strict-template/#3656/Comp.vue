d0959d340bcd7ffeafb279fd685aa6f2e1165c9a
so1ve <i@mk1.io>
2023-10-13 17:30:23 +0800

    upd test

--- packages/vue-test-workspace/vue-tsc/non-strict-template/#3656/Comp.vue
+++ packages/vue-test-workspace/vue-tsc/non-strict-template/#3656/Comp.vue
@@ -12,3 +12,29 @@ interface Props {
 
 defineProps<Props>();
 </script>
+
+<template>
+	<table>
+		<thead>
+			<tr>
+				<th v-for="(col, index) in columns" :key="index">
+					<!-- Pay attention to type of `col` here -->
+					<slot :name="`col(${col.key})`" v-bind="col">
+						<template v-if="col.title">{{ col.title }}</template>
+					</slot>
+				</th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<tr v-for="(row, rowIndex) in rows" :key="rowIndex">
+				<td v-for="(item, key) in row" :key="`${row}${key.toString()}`">
+					<!-- And look at the type of `row` here -->
+					<slot :name="`row(${key.toString()})`" v-bind="row">
+						{{ item }}
+					</slot>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</template>
