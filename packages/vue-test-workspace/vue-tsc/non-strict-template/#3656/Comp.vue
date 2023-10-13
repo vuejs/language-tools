ac5c447a3f3bbb14c7ebb698f8491056e16cdecc
so1ve <i@mk1.io>
2023-10-13 17:23:19 +0800

    test

---
+++ packages/vue-test-workspace/vue-tsc/non-strict-template/#3656/Comp.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup generic="T extends Column, K">
+export interface Column {
+	key: string;
+	title: string;
+	[key: string]: any;
+}
+
+interface Props {
+	columns: T[];
+	rows: K[];
+}
+
+defineProps<Props>();
+</script>
---
+++ packages/vue-test-workspace/vue-tsc/non-strict-template/#3656/main.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Comp, { Column } from './Comp.vue';
+
+interface Row {
+	name: string;
+	count: number;
+}
+
+const columns: Column[] = [];
+const rows: Row[] = [];
+</script>
+
+<template>
+	<Comp :columns="columns" :rows="rows">
+		<!-- Expected: `col` should have type `Column` -->
+		<!-- In fact: `col` have type `Row`, but output is right -->
+		<template #col(count)="col">{{ col }}</template>
+		<template #row(name)="row">{{ row.name.toUpperCase() }}</template>
+	</Comp>
+</template>
