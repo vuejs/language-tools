ea47fc5e2df1eedcdcc4f4b0cf4fc2a444def0d9
so1ve <i@mk1.io>
2023-10-14 13:42:30 +0800

    fix

--- packages/vue-language-core/src/generators/template.ts
+++ packages/vue-language-core/src/generators/template.ts
@@ -947,6 +947,7 @@ export function generate(
 				prev = childNode;
 			}
 			resolveComment();
+			generateAutoImportCompletionCode()
 
 			slotBlockVars.forEach(varName => {
 				localVars.set(varName, localVars.get(varName)! - 1);
