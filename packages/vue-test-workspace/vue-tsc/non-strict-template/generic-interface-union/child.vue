3c9fbf5b7208f5c6c12724e3fc4da5a8e4db3585
David Matter <david.matter@me.com>
2023-10-16 23:32:17 +0200

    formatting

--- packages/vue-test-workspace/vue-tsc/non-strict-template/generic-interface-union/child.vue
+++ packages/vue-test-workspace/vue-tsc/non-strict-template/generic-interface-union/child.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts" generic="T extends string | number">
 interface InputProps {
-  type?: 'input'
-  value: (k: T) => void
-  typeDefinition: T 
+	type?: 'input';
+	value: (k: T) => void;
+	typeDefinition: T;
 }
+
 interface SelectProps {
-  type: 'select'
-  value: (k: T[]) => void
-  typeDefinition: T 
+	type: 'select';
+	value: (k: T[]) => void;
+	typeDefinition: T;
 }
+
 defineProps<InputProps | SelectProps>()
 </script>
