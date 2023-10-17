d0073ae32712e901ac8d135094913e2060104d2f
so1ve <i@mk1.io>
2023-10-17 14:30:23 +0800

    init tests

--- packages/vue-tsc/tests/index.spec.ts
+++ packages/vue-tsc/tests/index.spec.ts
@@ -5,39 +5,42 @@ import { fork } from 'child_process';
 
 const binPath = require.resolve('../bin/vue-tsc.js');
 const workspace = path.resolve(__dirname, '../../vue-test-workspace/vue-tsc');
+const workspace27 = path.resolve(__dirname, '../../vue-test-workspace/vue-tsc-2.7');
 
 function prettyPath(path: string, isRoot: boolean) {
 	const segments = path.split('/');
-	return !isRoot ? segments.slice(segments.length - 2, segments.length).join('/') : segments[segments.length - 1];
+	return !isRoot ? segments.slice(segments.length - 3, segments.length).join('/') : segments[segments.length - 1];
 }
 
-function collectTests(dir: string, depth = 2, isRoot: boolean = true): [string, boolean][] {
+function collectTests(dirs: string[], depth = 2, isRoot: boolean = true): [string, boolean][] {
 	const tests: [string, boolean][] = [];
 
 	if (depth <= 0) {
 		return tests;
 	}
 
-	const files = fs.readdirSync(dir);
-	for (const file of files) {
-		const filePath = path.join(dir, file);
-		const stat = fs.statSync(filePath);
-		if (stat.isDirectory()) {
-			const tsconfigPath = path.join(filePath, 'tsconfig.json');
-			if (fs.existsSync(tsconfigPath)) {
-				tests.push([
-					filePath.replace(/\\/g, '/'),
-					isRoot,
-				]);
+	for (const dir of dirs) {
+		const files = fs.readdirSync(dir);
+		for (const file of files) {
+			const filePath = path.join(dir, file);
+			const stat = fs.statSync(filePath);
+			if (stat.isDirectory()) {
+				const tsconfigPath = path.join(filePath, 'tsconfig.json');
+				if (fs.existsSync(tsconfigPath)) {
+					tests.push([
+						filePath.replace(/\\/g, '/'),
+						isRoot,
+					]);
+				}
+				tests.push(...collectTests([filePath], depth - 1, false));
 			}
-			tests.push(...collectTests(filePath, depth - 1, false));
 		}
 	}
 
 	return tests;
 }
 
-const tests = collectTests(workspace);
+const tests = collectTests([workspace, workspace27]);
 
 function runVueTsc(cwd: string) {
 	return new Promise((resolve, reject) => {
