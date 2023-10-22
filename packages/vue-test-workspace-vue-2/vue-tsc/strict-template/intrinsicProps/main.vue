9bedf4b0bea1fd0fdb65fb0902769e663b508249
Rafał Chłodnicki <rchl2k@gmail.com>
2023-10-22 20:05:49 +0200

    test: add tests for Vue 2 (#3351)
    Co-authored-by: Johnson Chu <johnsoncodehk@gmail.com>

---
+++ packages/vue-test-workspace-vue-2/vue-tsc/strict-template/intrinsicProps/main.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<!-- FIXME: bug here -->
+		<!-- @vue-expect-error -->
+		<Foo class="123"></Foo>
+		<div class="123"></div>
+	</div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const Foo = defineComponent({
+	props: {
+		foo: String
+	}
+});
+</script>
+<script setup lang="ts">
+</script>
