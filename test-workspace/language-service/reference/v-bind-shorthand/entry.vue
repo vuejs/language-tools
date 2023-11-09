c17df649b629c14b9c64b07b70fa50a9ef340b42
so1ve <i@mk1.io>
2023-11-09 18:02:59 +0800

    add reference teest

---
+++ test-workspace/language-service/reference/v-bind-shorthand/entry.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Comp from './Comp.vue';
+
+const foo = 1;
+//    ^reference: 1 
+const bar = '';
+//    ^reference: 1 
+</script>
+
+<template>
+	<Comp :foo></Comp>
+	<!-- @vue-expect-error should typecheck -->
+	<Comp :bar></Comp>
+</template>
