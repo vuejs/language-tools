21c3d954761c436511e523f609c8f2951d6e4177
Rafał Chłodnicki <rchl2k@gmail.com>
2023-11-28 11:32:17 +0100

    Update test-workspace/tsc/vue3_strictTemplate/dataAttributes/main.vue
    Co-authored-by: Ray <i@mk1.io>

--- test-workspace/tsc/vue3_strictTemplate/dataAttributes/main.vue
+++ test-workspace/tsc/vue3_strictTemplate/dataAttributes/main.vue
@@ -1,16 +1,16 @@
 <template>
-    <div>
-        <svg><path stroke="#efefef" stroke-width="123" /></svg>
-        <foo stroke-width="123" />
-    </div>
+	<div>
+		<svg><path stroke="#efefef" stroke-width="123" /></svg>
+		<foo stroke-width="123" />
+	</div>
 </template>
 
 <script setup lang="ts">
 import { defineComponent } from 'vue';
 
 const Foo = defineComponent({
-    props: {
-        strokeWidth: { type: String, required: true },
-    }
+	props: {
+		strokeWidth: { type: String, required: true },
+	}
 });
 </script>
