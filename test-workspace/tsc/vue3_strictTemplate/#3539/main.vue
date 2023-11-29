a39c370377e2913f06acead8c958017724ed3b83
Ray <i@mk1.io>
2023-11-29 13:18:26 +0800

    test: add test for #3539 (#3717)
    Co-authored-by: Johnson Chu <johnsoncodehk@gmail.com>

---
+++ test-workspace/tsc/vue3_strictTemplate/#3539/main.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const msg = ref('Hello World!');
+</script>
+
+<template>
+	<!-- @vue-expect-error -->
+	<Hello :msg="msg"></Hello>
+</template>
