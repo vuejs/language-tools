86271d32c2d91e5fbf172a625eda8902f1b6ff12
so1ve <i@mk1.io>
2023-12-04 19:09:36 +0800

    tests

---
+++ test-workspace/tsc/vue3/#3765/main.vue
@@ -0,0 +1,15 @@
+<template>
+	<Foo stroke-width="test" />
+	<component :is="Foo" stroke-width="test"></component>
+	<component :is="Foo" strokeWidth="test"></component>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+
+const Foo = defineComponent({
+	props: {
+		strokeWidth: { type: String, required: true },
+	},
+});
+</script>
