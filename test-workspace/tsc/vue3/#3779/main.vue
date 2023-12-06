cc882fd036d1d1837ed1891d4e5914f1efdcd50e
so1ve <i@mk1.io>
2023-12-06 11:02:06 +0800

    tests

---
+++ test-workspace/tsc/vue3/#3779/main.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { exactType } from '../../shared';
+
+defineProps<{
+  optionalBoolean?: boolean;
+}>();
+</script>
+
+<template>
+  <h1>{{ exactType(optionalBoolean, true as boolean) }}</h1>
+</template>
