0f9957a8f5bfec0eb326093bf007f16968b690a8
Johnson Chu <johnsoncodehk@gmail.com>
2023-12-10 06:04:58 +0800

    chore: update volar to 2.0.0-alpha.3

--- packages/language-server/src/nodeServer.ts
+++ packages/language-server/src/nodeServer.ts
@@ -1,7 +1,7 @@
-import { TypeScriptServerPlugin, createConnection, startTypeScriptServer } from '@volar/language-server/node';
+import { createConnection, startTypeScriptServer } from '@volar/language-server/node';
 import { createServerPlugin } from './languageServerPlugin';
 
 const connection = createConnection();
 const plugin = createServerPlugin(connection);
 
-startTypeScriptServer(connection, plugin as TypeScriptServerPlugin);
+startTypeScriptServer(connection, plugin);
