30fe8f468f1111e289241a6b8a2410c6fdb4573d
so1ve <i@mk1.io>
2024-01-18 12:07:46 +0800

    tests

---
+++ test-workspace/tsc/vue3/#3845/main.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { exactType } from '../../shared';
+
+const foo = true ? 2 : [1];
+</script>
+
+<template>
+	<div v-for="f in foo" :key="f">{{ exactType(f, 1 as number) }}</div>
+</template>
