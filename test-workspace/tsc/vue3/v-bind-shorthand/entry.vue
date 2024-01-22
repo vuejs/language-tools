6580d440df7a7d0a5439f61f9f64b1fe35c1fc66
so1ve <i@mk1.io>
2024-01-22 09:45:40 +0800

    add test

--- test-workspace/tsc/vue3/v-bind-shorthand/entry.vue
+++ test-workspace/tsc/vue3/v-bind-shorthand/entry.vue
@@ -4,10 +4,12 @@ const foo = 1;
 //    ^reference: 1 
 const bar = '';
 //    ^reference: 1 
+const fooBar = 1;
 </script>
 
 <template>
 	<child :foo></child>
 	<!-- @vue-expect-error should typecheck -->
 	<child :bar></child>
+	<child :foo-bar></child>
 </template>
