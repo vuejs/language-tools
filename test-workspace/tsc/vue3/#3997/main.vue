ad0ea3d46a5092c32515d6afa0956d952f33a99b
so1ve <i@mk1.io>
2024-03-06 13:23:08 +0800

    test

---
+++ test-workspace/tsc/vue3/#3997/main.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const values = ref([1, 2, 3]);
+const abc = ref('abc');
+</script>
+
+<template>
+  <div>
+    <input v-for="value of values" :key="value" type="number" :value />
+    <span :abc></span>
+  </div>
+</template>
