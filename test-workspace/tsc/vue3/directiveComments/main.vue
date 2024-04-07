7e160b144252ea1e0269cfb375caf6fc5dc6663c
Johnson Chu <johnsoncodehk@gmail.com>
2024-04-07 16:57:03 +0800

    chore: format

--- test-workspace/tsc/vue3/directiveComments/main.vue
+++ test-workspace/tsc/vue3/directiveComments/main.vue
@@ -1,26 +1,25 @@
 <template>
 	<div>
-	  <!-- @vue-ignore -->
-	  <div :foo="foo">
+		<!-- @vue-ignore -->
+		<div :foo="foo">
+			<!-- @vue-expect-error -->
+			{{ foo }}
+		</div>
+		<!-- @vue-expect-error -->
+		<div :foo="foo">
+			<!-- @vue-expect-error -->
+			{{ foo }}
+		</div>
+		<!-- @vue-skip -->
+		<div :foo="foo">
+			{{ foo }}
+		</div>
+
+		<!-- @vue-ignore -->
+		{{ foo }}
 		<!-- @vue-expect-error -->
 		{{ foo }}
-	  </div>
-	  <!-- @vue-expect-error -->
-	  <div :foo="foo">
-		<!-- @vue-expect-error -->
+		<!-- @vue-skip -->
 		{{ foo }}
-	  </div>
-	  <!-- @vue-skip -->
-	  <div :foo="foo">
-		{{ foo }}
-	  </div>
-  
-	  <!-- @vue-ignore -->
-	  {{ foo }}
-	  <!-- @vue-expect-error -->
-	  {{ foo }}
-	  <!-- @vue-skip -->
-	  {{ foo }}
 	</div>
-  </template>
-  
+</template>
