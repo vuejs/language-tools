6580721bfafd603b8bf84f3d5de4476a330a3641
Johnson Chu <johnsoncodehk@gmail.com>
2024-05-10 15:49:48 +0800

    Update scriptSetup.ts

--- packages/language-core/lib/codegen/script/scriptSetup.ts
+++ packages/language-core/lib/codegen/script/scriptSetup.ts
@@ -46,13 +46,19 @@ export function* generateScriptSetup(
 			+ `	__VLS_expose?: NonNullable<Awaited<typeof __VLS_setup>>['expose'],${newLine}`
 			+ `	__VLS_setup = (async () => {${newLine}`;
 		yield* generateSetupFunction(options, ctx, scriptSetup, scriptSetupRanges, undefined, definePropMirrors);
-		const emitName = scriptSetupRanges.emits.define ? `typeof ${scriptSetupRanges.emits.name ?? '__VLS_emit'} & ` : '';
+
+		const emitTypes = ['typeof __VLS_modelEmitsType'];
+
+		if (scriptSetupRanges.emits.define) {
+			emitTypes.unshift(`typeof ${scriptSetupRanges.emits.name ?? '__VLS_emit'}`);
+		}
+
 		yield `		return {} as {${newLine}`
 			+ `			props: ${ctx.helperTypes.Prettify.name}<typeof __VLS_functionalComponentProps & __VLS_PublicProps> & __VLS_BuiltInPublicProps,${newLine}`
 			+ `			expose(exposed: import('${options.vueCompilerOptions.lib}').ShallowUnwrapRef<${scriptSetupRanges.expose.define ? 'typeof __VLS_exposed' : '{}'}>): void,${newLine}`
 			+ `			attrs: any,${newLine}`
 			+ `			slots: ReturnType<typeof __VLS_template>,${newLine}`
-			+ `			emit:  ${emitName}typeof __VLS_modelEmitsType,${newLine}`
+			+ `			emit: ${emitTypes.join(' & ')},${newLine}`
 			+ `		}${endOfLine}`;
 		yield `	})(),${newLine}`; // __VLS_setup = (async () => {
 		yield `) => ({} as import('${options.vueCompilerOptions.lib}').VNode & { __ctx?: Awaited<typeof __VLS_setup> }))`;
