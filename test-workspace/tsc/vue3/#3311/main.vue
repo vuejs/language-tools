e98439241ac0b37c24c014a1e12ed06e9a16141a
Johnson Chu <johnsoncodehk@gmail.com>
2024-05-14 03:20:30 +0800

    chore: add test for #3311

---
+++ test-workspace/tsc/vue3/#3311/main.vue
@@ -0,0 +1,12 @@
+<template>
+	<HelloWorld :multiple="true" :modelValue="['a']" />
+	<HelloWorld :multiple="false" :modelValue="'a'" />
+	<!-- @vue-expect-error -->
+	<HelloWorld :multiple="false" :modelValue="['a']" />
+	<!-- @vue-expect-error -->
+	<HelloWorld :multiple="true" :modelValue="'a'" />
+</template>
+
+<script lang="ts" setup>
+import HelloWorld from './component.vue'
+</script>
