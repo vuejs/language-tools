38ed005b1b87d2234538d3100c8be1d0eae2c95f
Johnson Chu <johnsoncodehk@gmail.com>
2024-05-14 00:55:40 +0800

    chore: add test for #3123

---
+++ test-workspace/tsc/vue3/#3123/main.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { FunctionalComponent, SVGAttributes } from 'vue';
+
+declare const VueSvg: FunctionalComponent<SVGAttributes>;
+</script>
+
+<template>
+	<div>
+		<a href="https://vitejs.dev" target="_blank">
+			<img src="/vite.svg" class="logo" alt="Vite logo" />
+		</a>
+		<a href="https://vuejs.org/" target="_blank">
+			<VueSvg width="96" class="logo vue" fill-opacity="0.5" />
+		</a>
+	</div>
+</template>
