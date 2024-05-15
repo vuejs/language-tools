57480602083b62ecfff356c22c315a5873be6cc1
Johnson Chu <johnsoncodehk@gmail.com>
2024-05-15 19:25:55 +0800

    chore(vscode): auto enabling hybrid mode allows `runem.lit-plugin`
    #4206

--- extensions/vscode/src/common.ts
+++ extensions/vscode/src/common.ts
@@ -56,6 +56,7 @@ function isExtensionCompatibleWithHybridMode(extension: vscode.Extension<any>) {
 		|| extension.id === 'jenkey2011.string-highlight'
 		|| extension.id === 'mxsdev.typescript-explorer'
 		|| extension.id === 'miaonster.vscode-tsx-arrow-definition'
+		|| extension.id === 'runem.lit-plugin'
 	) {
 		return true;
 	}
