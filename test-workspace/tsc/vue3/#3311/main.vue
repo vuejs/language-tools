018d607ddf5a2907d9b663f1b9abb98b4b672b93
Johnson Chu <johnsoncodehk@gmail.com>
2024-05-15 19:32:42 +0800

    chore: update #3311 test case

--- test-workspace/tsc/vue3/#3311/main.vue
+++ test-workspace/tsc/vue3/#3311/main.vue
@@ -1,10 +1,13 @@
 <template>
 	<HelloWorld :multiple="true" :modelValue="['a']" />
 	<HelloWorld :multiple="false" :modelValue="'a'" />
+	<HelloWorld :modelValue="'a'" />
+	<!-- @vue-expect-error -->
+	<HelloWorld :multiple="true" :modelValue="'a'" />
 	<!-- @vue-expect-error -->
 	<HelloWorld :multiple="false" :modelValue="['a']" />
 	<!-- @vue-expect-error -->
-	<HelloWorld :multiple="true" :modelValue="'a'" />
+	<HelloWorld :modelValue="['a']" />
 </template>
 
 <script lang="ts" setup>
