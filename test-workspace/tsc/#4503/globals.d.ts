cb99cbca8c13ccc4859de85ec696bbaa2d41dcff
Ray <i@mk1.io>
2024-07-08 07:59:58 +0800

    chore: format

--- test-workspace/tsc/#4503/globals.d.ts
+++ test-workspace/tsc/#4503/globals.d.ts
@@ -1,10 +1,9 @@
-
 import type globalcomp from './globalcomp.vue';
 
 declare module 'vue' {
-  export interface GlobalComponents {
-	  globalcomp: typeof globalcomp
-  }
+	export interface GlobalComponents {
+		globalcomp: typeof globalcomp
+	}
 }
 
 export { };
