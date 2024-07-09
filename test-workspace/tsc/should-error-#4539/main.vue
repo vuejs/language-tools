a5295f1264590bf866351b698174c64d1d7119f7
so1ve <i@mk1.io>
2024-07-09 17:41:53 +0800

    tests: update test

--- test-workspace/tsc/should-error-#4539/main.vue
+++ test-workspace/tsc/should-error-#4539/main.vue
@@ -1,6 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const key = {};
+</script>
 
 <template>
+  <template v-for="_i in 1" :key></template>
   <template v-for="_i in 1" :key="{}"></template>
+  <template v-if="true" :key></template>
   <template v-if="true" :key="{}"></template>
+  <div v-if="true" :key="{}"></div>
 </template>
