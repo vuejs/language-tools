49ecc4cd7b63653165cfcddae9d5294e9b3633d8
gaokefei <260480378@qq.com>
2024-07-19 14:04:22 +0800

    fix(language-core): add type checked for v-model without argument

---
+++ test-workspace/tsc/vue3/#4540/main.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Comp from "./component.vue";
+
+const model = ref(1);
+</script>
+
+<template>
+  <Comp v-model="model" :foo="1" />
+	<Comp v-model:model-value="model" :foo="1" />
+</template>
