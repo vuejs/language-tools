12c7a9f745b60d73c1d60118d4f3abbc4ac0c17d
Johnson Chu <johnsoncodehk@gmail.com>
2024-07-20 19:58:55 +0800

    test: correctly reproduce #3819

--- test-workspace/tsc/#3819/main.vue
+++ test-workspace/tsc/#3819/main.vue
@@ -1,19 +1,19 @@
 <script setup>
-import Comp from './comp.vue';
+import Comp from './component.vue';
 
 const config = {
 	a: 'a',
 	b: 'b',
 	c: 'c',
 	d: 'd',
-};
+}
 </script>
 
 <template>
 	<Comp :config="config">
 		<template #last-columns></template>
 		<!-- @vue-expect-error -->
-		<template #last-columnsasd></template>
+		<template #last-columnsasd></template> <!-- should be error  -->
 		<template #lastColumns></template>
 		<!-- @vue-expect-error -->
 		<template #lastColumnsasd></template>
--- test-workspace/tsc/vue3/#3819/comp.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts" setup generic="T">
-type ISlots = {
-	"last-columns": (props: string) => void;
-	"lastColumns": (props: string) => void;
-};
-defineSlots<ISlots>()
-</script>
