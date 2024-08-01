d918db764acdbbdfdcd9290128ebe24560b309a9
KazariEX <1364035137@qq.com>
2024-08-01 13:34:35 +0800

    chore: tab indent

--- test-workspace/tsc/vue3.5/components/script-setup-destructured-props.vue
+++ test-workspace/tsc/vue3.5/components/script-setup-destructured-props.vue
@@ -3,31 +3,31 @@
 import { watch } from 'vue';
 
 const { foo, bar, ...props } = defineProps<{
-    foo: string;
-    bar: string;
-    baz: string;
+	foo: string;
+	bar: string;
+	baz: string;
 }>();
 
 type foo = foo & typeof foo;
 
 interface foo {
-    foo: typeof foo;
+	foo: typeof foo;
 }
 
 const obj = {
-    foo: foo,
-    foo,
-    foo(foo) {},
-    foo: function(foo) {},
-    get bar() { return this.foo; },
-    set bar(val) { this.foo = val; }
+	foo: foo,
+	foo,
+	foo(foo) {},
+	foo: function(foo) {},
+	get bar() { return this.foo; },
+	set bar(val) { this.foo = val; }
 }
 
 function func(foo) {}
 
 class cls {
-    foo: string = foo;
-    constructor(foo) {}
+	foo: string = foo;
+	constructor(foo) {}
 }
 
 for (const char of foo) {}
@@ -35,10 +35,10 @@ for (const char of foo) {}
 try {} catch (foo) {}
 
 watch(() => foo, (foo) => {
-    console.log(foo, bar, props.baz);
+	console.log(foo, bar, props.baz);
 });
 </script>
 
 <template>
-    {{ foo }}
+	{{ foo }}
 </template>
