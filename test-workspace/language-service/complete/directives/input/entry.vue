d3cd654f0f5b993db43ac1058e56adfef5e82ffb
KazariEX <1364035137@qq.com>
2024-08-03 22:59:58 +0800

    test: match text by single line

---
+++ test-workspace/language-service/complete/directives/input/entry.vue
@@ -0,0 +1,10 @@
+<template>
+  <div v-cl></div>
+  <!--     ^complete: v-cloak -->
+  <div v-onc></div>
+  <!--      ^complete: v-once -->
+  <div v-p></div>
+  <!--    ^complete: v-pre -->
+  <div v-on></div>
+  <!--     ^complete: v-on: -->
+</template>
---
+++ test-workspace/language-service/complete/directives/output/entry.vue
@@ -0,0 +1,10 @@
+<template>
+  <div v-cloak></div>
+  <!--     ^complete: v-cloak -->
+  <div v-once></div>
+  <!--      ^complete: v-once -->
+  <div v-pre></div>
+  <!--    ^complete: v-pre -->
+  <div v-on:></div>
+  <!--     ^complete: v-on: -->
+</template>
