89a35d9954104dc6af8e507113cac2d48b2b7fb3
_Kerman <kermanx@qq.com>
2024-08-13 16:53:48 +0800

    fix: should preserve generic info in directive

--- packages/language-core/lib/codegen/template/elementDirectives.ts
+++ packages/language-core/lib/codegen/template/elementDirectives.ts
@@ -43,7 +43,7 @@ export function* generateElementDirectives(
 				prop.loc.start.offset,
 				prop.loc.end.offset,
 				ctx.codeFeatures.verification,
-				`__VLS_directiveFunction(__VLS_ctx.`,
+				`__VLS_directiveAsFunction(__VLS_ctx.`,
 				...generateCamelized(
 					'v-' + prop.name,
 					prop.loc.start.offset,
@@ -60,27 +60,37 @@ export function* generateElementDirectives(
 						},
 					}
 				),
-				`)(`,
+				`)(null!, { ...__VLS_directiveBindingRestFields, `,
 				...(
 					prop.exp?.type === CompilerDOM.NodeTypes.SIMPLE_EXPRESSION
-						? wrapWith(
-							prop.exp.loc.start.offset,
-							prop.exp.loc.end.offset,
-							ctx.codeFeatures.verification,
-							...generateInterpolation(
-								options,
-								ctx,
-								prop.exp.content,
-								prop.exp.loc,
+						? [
+							...wrapWith(
 								prop.exp.loc.start.offset,
-								ctx.codeFeatures.all,
-								'(',
-								')'
+								prop.exp.loc.end.offset,
+								ctx.codeFeatures.verification,
+								'["value"',
+								[' '.repeat(prop.exp.content.length - 1) + ']', 'template', prop.exp.loc.start.offset, ctx.codeFeatures.verification] satisfies Code,
+								': ',
+							),
+							...wrapWith(
+								prop.exp.loc.start.offset,
+								prop.exp.loc.end.offset,
+								ctx.codeFeatures.verification,
+								...generateInterpolation(
+									options,
+									ctx,
+									prop.exp.content,
+									prop.exp.loc,
+									prop.exp.loc.start.offset,
+									ctx.codeFeatures.all,
+									'(',
+									')'
+								),
 							)
-						)
+						]
 						: [`undefined`]
 				),
-				`)`
+				`}, null!, null!)`
 			);
 			yield endOfLine;
 		}
---
+++ test-workspace/tsc/vue3/#4682/main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-object="[1, (_a: 1) => {}]"></div>
+  <!-- @vue-expect-error -->
+  <div v-object="2"></div>
+
+	<div v-function="[1, (_a: 1) => {}]"></div>
+	<!-- @vue-expect-error -->
+	<div v-function="2"></div>
+</template>
+
+<script lang="ts" setup>
+const vObject = {
+  created<T extends number>(
+    _el: HTMLElement,
+    _binding: { value: [T, (a: T) => void] }
+  ) { },
+  mounted<T extends number>(
+    _el: HTMLElement,
+    _binding: { value: [T, (a: T) => void] },
+		_vNode: any,
+  ) { },
+};
+
+function vFunction<T extends number>(_el: any, _binding: { value: [T, (a: T) => void] }) { }
+</script>
