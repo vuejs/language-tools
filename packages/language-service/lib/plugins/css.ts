934cd9c27e93c78a0c0e24db3dcd54594d3232b5
KazariEX <1364035137@qq.com>
2024-08-13 12:52:16 +0800

    fix: determine scoped

--- packages/language-service/lib/plugins/css.ts
+++ packages/language-service/lib/plugins/css.ts
@@ -1,7 +1,9 @@
 import type { LanguageServicePlugin, LanguageServicePluginInstance } from '@volar/language-service';
 import type { TextDocument } from 'vscode-languageserver-textdocument';
-import * as css from 'vscode-css-languageservice';
+import { VueVirtualCode } from '@vue/language-core';
 import { create as baseCreate, type Provide } from 'volar-service-css';
+import * as css from 'vscode-css-languageservice';
+import { URI } from 'vscode-uri';
 
 const cssClassNameReg = /(?=(\.[a-z_][-\w]*)[\s.,+~>:#[{])/gi;
 const vBindCssVarReg = /\bv-bind\(\s*(?:'([^']+)'|"([^"]+)"|([a-z_]\w+))\s*\)/gi;
@@ -29,6 +31,25 @@ export function create(): LanguageServicePlugin {
 					return diagnostics;
 				},
 				provideRenameRange(document, position) {
+
+					const decoded = context.decodeEmbeddedDocumentUri(URI.parse(document.uri));
+					const sourceScript = decoded && context.language.scripts.get(decoded[0]);
+					const virtualCode = decoded && sourceScript?.generated?.embeddedCodes.get(decoded[1]);
+
+					const regexps: RegExp[] = [];
+
+					if (virtualCode?.id.startsWith('style_')) {
+						const i = Number(virtualCode.id.slice('style_'.length));
+						if (sourceScript?.generated?.root instanceof VueVirtualCode) {
+							const style = sourceScript.generated.root.sfc.styles[i];
+							const option = sourceScript.generated.root.vueCompilerOptions.experimentalResolveStyleCssClasses;
+							if (option === 'always' || (option === 'scoped' && style.scoped)) {
+								regexps.push(cssClassNameReg);
+							}
+						}
+					}
+					regexps.push(vBindCssVarReg);
+
 					return worker(document, (stylesheet, cssLs) => {
 						const text = document.getText();
 						const offset = document.offsetAt(position);
@@ -41,10 +62,7 @@ export function create(): LanguageServicePlugin {
 						return cssLs.prepareRename(document, position, stylesheet);
 
 						function* forEachRegExp() {
-							for (const reg of [
-								cssClassNameReg,
-								vBindCssVarReg
-							]) {
+							for (const reg of regexps) {
 								for (const match of text.matchAll(reg)) {
 									const matchText = match.slice(1).find(t => t);
 									if (matchText) {
