cb9e020b25f336799bb9f4ec679fe8921b429111
KazariEX <1364035137@qq.com>
2024-08-13 11:31:46 +0800

    fix: optimize matching of `v-bind()`

--- packages/language-core/lib/utils/parseCssClassNames.ts
+++ packages/language-core/lib/utils/parseCssClassNames.ts
@@ -6,11 +6,9 @@ export function* parseCssClassNames(styleContent: string) {
 	styleContent = clearComments(styleContent);
 	const matches = styleContent.matchAll(cssClassNameReg);
 	for (const match of matches) {
-		if (match.index !== undefined) {
-			const matchText = match[1];
-			if (matchText !== undefined) {
-				yield { offset: match.index, text: matchText };
-			}
+		const matchText = match[1];
+		if (matchText) {
+			yield { offset: match.index, text: matchText };
 		}
 	}
 }
