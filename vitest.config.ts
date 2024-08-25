c3dffde5758632efd3c756d62e9193cbbe5e2b51
Johnson Chu <johnsoncodehk@gmail.com>
2024-08-25 14:57:01 +0800

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
-		include: ['packages/language-server/tests/**/inlayHints.spec.ts'],
 		poolOptions: {
 			forks: {
 				singleFork: true,
