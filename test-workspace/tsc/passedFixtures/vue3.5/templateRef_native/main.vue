401a65e42a9a77e54e74175d109a2f7c94ff504c
Johnson Chu <johnsoncodehk@gmail.com>
2024-09-02 21:10:38 +0800

    Update main.vue

--- test-workspace/tsc/passedFixtures/vue3.5/templateRef_native/main.vue
+++ test-workspace/tsc/passedFixtures/vue3.5/templateRef_native/main.vue
@@ -4,29 +4,29 @@ import { exactType } from '../../../passedFixtures/shared';
 
 const audioEl = useTemplateRef('audio');
 if (audioEl.value) {
-    exactType(audioEl.value, {} as HTMLAudioElement);
+	exactType(audioEl.value, {} as HTMLAudioElement);
 }
 
 const imgEl = useTemplateRef('img');
 if (imgEl.value) {
-    exactType(imgEl.value, {} as HTMLImageElement);
+	exactType(imgEl.value, {} as HTMLImageElement);
 }
 
 const svgEl = useTemplateRef('svg');
 if (svgEl.value) {
-    exactType(svgEl.value, {} as SVGSVGElement);
+	exactType(svgEl.value, {} as SVGSVGElement);
 }
 
 const feImageEl = useTemplateRef('feImage');
 if (feImageEl.value) {
-    exactType(feImageEl.value, {} as SVGFEImageElement);
+	exactType(feImageEl.value, {} as SVGFEImageElement);
 }
 </script>
 
 <template>
-    <audio ref="audio"></audio>
-    <img ref="img"/>
-    <svg ref="svg">
-        <feImage ref="feImage"></feImage>
-    </svg>
+	<audio ref="audio"></audio>
+	<img ref="img" />
+	<svg ref="svg">
+		<feImage ref="feImage"></feImage>
+	</svg>
 </template>
