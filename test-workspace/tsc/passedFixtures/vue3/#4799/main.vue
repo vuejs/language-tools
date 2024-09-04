e90ea65c1acb7fc07dc4297196af8e4ff96c4fbe
山吹色御守 <85992002+KazariEX@users.noreply.github.com>
2024-09-04 20:05:03 +0800

    fix(language-core): avoid using `__typeProps` with runtime props (#4800)

---
+++ test-workspace/tsc/passedFixtures/vue3/#4799/main.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { exactType } from '../../shared';
+
+defineProps({
+	foo: String
+});
+
+defineModel();
+</script>
+
+<template>
+	{{ exactType(foo, {} as string | undefined) }}
+</template>
