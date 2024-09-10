be6dee83768deb169904927ad3345f80c0c0f697
zhiyuanzmj <260480378@qq.com>
2024-09-10 19:22:16 +0800

    chore: add test

---
+++ test-workspace/tsc/passedFixtures/vue3/#4836/main.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { exactType } from 'tsc/passedFixtures/shared';
+import Child from './child.vue';
+import { useTemplateRef } from 'vue';
+
+const child = useTemplateRef('child');
+exactType(child.value?.a, {} as unknown);
+</script>
+
+<template>
+	<Child ref="child" />
+</template>
