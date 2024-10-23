a1592fb0d0c547f0f14c3e8da972afe2d09447ec
Johnson Chu <johnsoncodehk@gmail.com>
2024-10-23 15:21:00 +0800

    Update typecheck.spec.ts

--- packages/tsc/tests/typecheck.spec.ts
+++ packages/tsc/tests/typecheck.spec.ts
@@ -9,6 +9,7 @@ describe(`vue-tsc`, () => {
 			getTscOutput('stable')
 		).toMatchInlineSnapshot(`
 			[
+			  "test-workspace/tsc/failureFixtures/#3632/main.vue(2,13): error TS1109: Expression expected.",
 			  "test-workspace/tsc/failureFixtures/directives/main.vue(4,6): error TS2339: Property 'notExist' does not exist on type 'CreateComponentPublicInstanceWithMixins<ToResolvedProps<{}, {}>, { exist: typeof exist; }, {}, {}, {}, ComponentOptionsMixin, ComponentOptionsMixin, ... 18 more ..., {}>'.",
 			  "test-workspace/tsc/failureFixtures/directives/main.vue(9,6): error TS2339: Property 'notExist' does not exist on type 'CreateComponentPublicInstanceWithMixins<ToResolvedProps<{}, {}>, { exist: typeof exist; }, {}, {}, {}, ComponentOptionsMixin, ComponentOptionsMixin, ... 18 more ..., {}>'.",
 			  "test-workspace/tsc/failureFixtures/directives/main.vue(12,2): error TS2578: Unused '@ts-expect-error' directive.",
@@ -24,6 +25,7 @@ describe(`vue-tsc`, () => {
 			getTscOutput('next')
 		).toMatchInlineSnapshot(`
 			[
+			  "test-workspace/tsc/failureFixtures/#3632/main.vue(2,13): error TS1109: Expression expected.",
 			  "test-workspace/tsc/failureFixtures/directives/main.vue(4,6): error TS2339: Property 'notExist' does not exist on type 'CreateComponentPublicInstanceWithMixins<ToResolvedProps<{}, {}>, { exist: typeof exist; }, {}, {}, {}, ComponentOptionsMixin, ComponentOptionsMixin, ... 18 more ..., {}>'.",
 			  "test-workspace/tsc/failureFixtures/directives/main.vue(9,6): error TS2339: Property 'notExist' does not exist on type 'CreateComponentPublicInstanceWithMixins<ToResolvedProps<{}, {}>, { exist: typeof exist; }, {}, {}, {}, ComponentOptionsMixin, ComponentOptionsMixin, ... 18 more ..., {}>'.",
 			  "test-workspace/tsc/failureFixtures/directives/main.vue(12,2): error TS2578: Unused '@ts-expect-error' directive.",
