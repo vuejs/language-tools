8d08da8e1b61efe315e70a01368435ee4419cce0
山吹色御守 <85992002+KazariEX@users.noreply.github.com>
2024-10-24 14:04:56 +0800

    fix(language-core): correct type inference of `defineModel` & `defineEmits` in generic (#4823)
    Co-authored-by: Ray <i@mk1.io>
    Co-authored-by: Johnson Chu <johnsoncodehk@gmail.com>

---
+++ test-workspace/tsc/passedFixtures/vue3/#4822/main.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { exactType } from '../../shared';
+import Comp from './comp.vue';
+
+export default defineComponent({
+	components: {
+		Comp
+	}
+});
+</script>
+
+<script setup lang="ts">
+let foo!: number;
+</script>
+
+<template>
+	<Comp @foo="(arg) => exactType(arg, {} as number)" v-model="foo"></Comp>
+</template>
