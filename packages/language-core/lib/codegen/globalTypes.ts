e1ab05aeb9d7815364879155ba4d9f59122f4a05
KazariEX <1364035137@qq.com>
2024-10-26 18:47:12 +0800

    fix: vue2

--- packages/language-core/lib/codegen/globalTypes.ts
+++ packages/language-core/lib/codegen/globalTypes.ts
@@ -91,7 +91,7 @@ export function generateGlobalTypes(lib: string, target: number, strictTemplates
 	function __VLS_getVForSourceType(source: number): [number, number, number][];
 	function __VLS_getVForSourceType(source: string): [string, number, number][];
 	function __VLS_getVForSourceType<T extends any[]>(source: T): [
-		item: import('${lib}').Reactive<T[number]>,
+		item: ${target >= 2.7 ? `import('${lib}').${target >= 3 ? `Reactive` : `UnwrapNestedRefs`}<T[number]>` : `T<number>`},
 		key: number,
 		index: number,
 	][];
