3557d8b17a9e5e78c18013262823649410effada
Johnson Chu <johnsoncodehk@gmail.com>
2024-10-26 19:14:16 +0800

    add a failed test case

---
+++ test-workspace/tsc/passedFixtures/vue3/#4876/regression.vue
@@ -0,0 +1,17 @@
+<script setup>
+// @ts-check
+import { exactType } from '../../shared';
+import Self from './regression.vue';
+
+const props = defineProps({
+	msg: {
+		type: String,
+		default: null
+	},
+	msgExact: {
+		type: String,
+	},
+});
+
+exactType((new Self()).$props.msg, props.msgExact);
+</script>
