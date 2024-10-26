87008bd5a634766335fe48d4d0190512bb4baaff
Johnson Chu <johnsoncodehk@gmail.com>
2024-10-26 18:55:08 +0800

    Update globalTypes.ts

--- packages/language-core/lib/codegen/globalTypes.ts
+++ packages/language-core/lib/codegen/globalTypes.ts
@@ -92,7 +92,11 @@ export function generateGlobalTypes(lib: string, target: number, strictTemplates
 	function __VLS_getVForSourceType(source: number): [number, number, number][];
 	function __VLS_getVForSourceType(source: string): [string, number, number][];
 	function __VLS_getVForSourceType<T extends any[]>(source: T): [
-		item: ${target >= 2.7 ? `import('${lib}').${target >= 3 ? `Reactive` : `UnwrapNestedRefs`}<T[number]>` : `T<number>`},
+		item: ${(
+			target >= 3 ? `import('${lib}').Reactive<T[number]>` :
+				target >= 2.7 ? `import('${lib}').UnwrapNestedRefs<T[number]>` :
+					`T[number]`
+		)},
 		key: number,
 		index: number,
 	][];
