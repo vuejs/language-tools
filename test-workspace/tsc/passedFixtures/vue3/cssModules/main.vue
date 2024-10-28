967170525c6ff5bda2fd3cb1c235810aa4efb446
KazariEX <1364035137@qq.com>
2024-10-28 16:06:31 +0800

    test: move

---
+++ test-workspace/tsc/passedFixtures/vue3/cssModules/main.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { useCssModule } from 'vue';
+import { exactType } from '../../shared';
+
+const style = useCssModule();
+exactType(style, {} as Record<string, string> & {
+    style: string;
+});
+
+const foo = useCssModule('foo');
+exactType(foo, {} as Record<string, string> & {
+    foo: string;
+});
+
+const bar = useCssModule('bar');
+exactType(bar, {} as Record<string, string> & {
+    bar: string;
+});
+</script>
+
+<style module>
+.style { }
+</style>
+
+<style module="foo">
+.foo { }
+</style>
+
+<style module="bar">
+.bar { }
+</style>
