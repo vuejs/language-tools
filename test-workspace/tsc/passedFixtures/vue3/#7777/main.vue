d66f54de4d0fcaae0d17e8eed9864352c971e7b6
KazariEX <1364035137@qq.com>
2024-11-02 13:03:48 +0800

    test: add

---
+++ test-workspace/tsc/passedFixtures/vue3/#7777/main.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Comp from './comp.vue';
+
+let condition!: 'A' | 'B';
+</script>
+
+<template>
+	<div v-if="condition === 'A'"></div>
+	<Comp v-else :condition />
+</template>
