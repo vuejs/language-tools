46a38bddeaac987759b94bd0693789900dacee06
_Kerman <kermanx@qq.com>
2024-11-09 12:34:53 +0800

    add sonda report

--- extensions/vscode/build.config.ts
+++ extensions/vscode/build.config.ts
@@ -2,6 +2,7 @@ import { defineBuildConfig } from "unbuild";
 import { fileURLToPath } from "url";
 import { join } from "path";
 import { readFileSync, writeFileSync } from "fs";
+import { SondaRollupPlugin } from "sonda";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 const languageServiceDataRE = /\/language-service\/data\/.*\.json$/;
@@ -65,9 +66,9 @@ export default defineBuildConfig([
 
 		hooks: {
 			"rollup:options"(_ctx, options) {
-				// Load language-service data as `JSON.parse(serialized)`
-				// See https://v8.dev/blog/cost-of-javascript-2019#:~:text=A%20good%20rule%20of%20thumb%20is%20to%20apply%20this%20technique%20for%20objects%20of%2010%20kB%20or%20larger
 				options.plugins = [
+					// Load language-service data as `JSON.parse(serialized)`
+					// See https://v8.dev/blog/cost-of-javascript-2019#:~:text=A%20good%20rule%20of%20thumb%20is%20to%20apply%20this%20technique%20for%20objects%20of%2010%20kB%20or%20larger
 					{
 						name: 'language-service-data',
 						transform: {
@@ -83,15 +84,17 @@ export default defineBuildConfig([
 							}
 						}
 					},
+
+					SondaRollupPlugin(),
+
 					...options.plugins,
 				];
 
 				// Only emit CJS
 				if (!Array.isArray(options.output)) throw "Unreachable";
-				options.output = options.output.filter(
-					(option) => option.format === "cjs"
-				);
+				options.output = options.output.filter((option) => option.format === "cjs");
 				if (options.output.length !== 1) throw "Unreachable";
+				options.output[0].sourcemap = true;
 			},
 
 			"build:done"(ctx) {
