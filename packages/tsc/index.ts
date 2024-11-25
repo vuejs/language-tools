35b95363ff8d580c73e8205d83574100f8e3da53
山吹色御守 <85992002+KazariEX@users.noreply.github.com>
2024-11-25 17:04:01 +0800

    Update packages/tsc/index.ts
    Co-authored-by: Danila Rodichkin <daniluk4000@outlook.com>

--- packages/tsc/index.ts
+++ packages/tsc/index.ts
@@ -51,7 +51,7 @@ export function run(tscPath = getTscPath()) {
 function getTscPath() {
 	const version = require('typescript/package.json').version as string;
 
-	if (semver.gte(version, '5.7.2')) {
+	if (semver.gte(version, '5.7')) {
 		return require.resolve('typescript/lib/_tsc');
 	}
 	else {
