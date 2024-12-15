9f9a7589b9e1573224735ce225c729e776a69716
_Kerman <kermanx@qq.com>
2024-12-15 20:37:47 +0800

    chore: update

--- extensions/vscode/build.config.ts
+++ extensions/vscode/build.config.ts
@@ -1,46 +1,43 @@
-import { defineBuildConfig } from "unbuild";
-import { fileURLToPath } from "url";
-import { join } from "path";
-import { readFileSync, writeFileSync } from "fs";
-import { SondaRollupPlugin } from "sonda";
+import { readFileSync, writeFileSync } from 'node:fs';
+import { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { SondaRollupPlugin } from 'sonda';
+import { defineBuildConfig } from 'unbuild';
 
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 const languageServiceDataRE = /\/language-service\/data\/.*\.json$/;
 
 export default defineBuildConfig([
 	{
 		entries: [
 			{
-				builder: "rollup",
-				input: "src/nodeClientMain.ts",
-				name: "client",
+				builder: 'rollup',
+				input: 'src/nodeClientMain.ts',
+				name: 'client',
 			},
 			{
-				builder: "rollup",
-				input: "./node_modules/@vue/language-server/bin/vue-language-server.js",
-				name: "server",
+				builder: 'rollup',
+				input: './node_modules/@vue/language-server/bin/vue-language-server.js',
+				name: 'server',
 			},
 			{
-				builder: "copy",
-				input: "./node_modules/@vue/language-core/schemas",
-				outDir: "./dist/schemas",
+				builder: 'copy',
+				input: './node_modules/@vue/language-core/schemas',
+				outDir: './dist/schemas',
 			},
 		],
 		failOnWarn: false,
-		alias: {
-			// https://github.com/microsoft/vscode-emmet-helper/issues/79
-			"@vscode/emmet-helper": "@vscode/emmet-helper/lib/cjs/emmetHelper.js",
-		},
-		outDir: "dist",
-		externals: ["vscode"],
+		outDir: 'dist',
+		externals: ['vscode'],
 		replace: {
-			"globalThis.__VOLAR_DEV_FS__": "undefined",
-			"process.env.NODE_ENV": "'production'",
+			'globalThis.__VOLAR_DEV_FS__': 'undefined',
+			'process.env.NODE_ENV': '"production"',
 		},
+		sourcemap: true,
 		rollup: {
 			emitCJS: true,
 			esbuild: {
-				target: "ES2021",
+				target: 'ES2021',
 			},
 			commonjs: {
 				transformMixedEsModules: true,
@@ -56,25 +53,26 @@ export default defineBuildConfig([
 		stubOptions: {
 			jiti: {
 				alias: {
-					vscode: join(__dirname, "vscode.js"),
+					vscode: join(__dirname, 'vscode.js'),
 				},
 				sourceMaps: true,
 			},
 		},
 		hooks: {
-			"rollup:options"(_ctx, options) {
+			'rollup:options'(_ctx, options) {
 				options.plugins = [
 					// Load language-service data as `JSON.parse(serialized)`
-					// See https://v8.dev/blog/cost-of-javascript-2019#:~:text=A%20good%20rule%20of%20thumb%20is%20to%20apply%20this%20technique%20for%20objects%20of%2010%20kB%20or%20larger
+					// See https://v8.dev/blog/cost-of-javascript-2019#json
 					{
 						name: 'language-service-data',
 						transform: {
-							order: "pre",
+							order: 'pre',
 							handler(code: string, id: string) {
 								if (languageServiceDataRE.test(id.replaceAll('\\', '/'))) {
-									const serialized = JSON.stringify(JSON.parse(code)).replaceAll('\\', '\\\\').replaceAll('`', '\\`');
+									const minimal = JSON.stringify(JSON.parse(code));
+									const escaped =	minimal.replaceAll(/[$`\\]/g, c => `\\${c}`);
 									return {
-										code: `export default JSON.parse(\`${serialized}\`)`,
+										code: `export default JSON.parse(\`${escaped}\`)`,
 										map: { mappings: '' },
 									};
 								}
@@ -88,22 +86,22 @@ export default defineBuildConfig([
 				];
 
 				// Only emit CJS
-				if (!Array.isArray(options.output)) throw "Unreachable";
-				options.output = options.output.filter((option) => option.format === "cjs");
-				if (options.output.length !== 1) throw "Unreachable";
-				options.output[0].sourcemap = true;
+				if (!Array.isArray(options.output)) throw new Error('Unreachable');
+				options.output = options.output.filter((option) => option.format === 'cjs');
+				if (options.output.length !== 1) throw new Error('Unreachable');
+				options.output[0].entryFileNames = '[name].cjs';
 			},
 
-			"build:done"(ctx) {
+			'build:done'(ctx) {
 				if (ctx.options.stub) {
 					// Patch the stub file
-					const stubFilePath = join(__dirname, "dist/client.cjs");
-					const originalStub = readFileSync(stubFilePath, "utf-8");
+					const stubFilePath = join(__dirname, 'dist/client.js');
+					const originalStub = readFileSync(stubFilePath, 'utf-8');
 					const newStub = [
 						`globalThis.__VOLAR_DEV_VSCODE__ = require('vscode');`,
 						`globalThis.__VOLAR_DEV_FS__ = require('node:fs');`,
 						originalStub,
-					].join("\n");
+					].join('\n');
 					writeFileSync(stubFilePath, newStub);
 				}
 			},
