ad728afe2e213b3042e0329099a0f7195668867d
KazariEX <1364035137@qq.com>
2024-12-18 03:02:58 +0800

    test: update [skip ci]

--- test-workspace/tsc/passedFixtures/vue3/defineModelModifiers/main.vue
+++ test-workspace/tsc/passedFixtures/vue3/defineModelModifiers/main.vue
@@ -5,7 +5,8 @@ const fooBar = 'fooBar';
 </script>
 
 <template>
-    <Comp v-model:foo-bar.foo.bar="fooBar"></Comp>
+    <Comp v-model:foo-bar.foo="fooBar"></Comp>
+    <Comp v-model:[fooBar].bar="fooBar"></Comp>
     <!-- @vue-expect-error -->
-    <Comp v-model:[fooBar].unknown="fooBar"></Comp>
+    <Comp v-model:foo-bar.unknown="fooBar"></Comp>
 </template>
