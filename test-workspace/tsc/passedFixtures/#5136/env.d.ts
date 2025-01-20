e7697ebfebba9d99227ed4f8a1ad068a8ba71cdf
KazariEX <1364035137@qq.com>
2025-01-20 20:28:21 +0800

    test: add

---
+++ test-workspace/tsc/passedFixtures/#5136/env.d.ts
@@ -0,0 +1,6 @@
+declare module '*.css' {
+  const classes: {
+    foo: string;
+  }
+  export default classes
+}
---
+++ test-workspace/tsc/passedFixtures/#5136/src.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useCssModule } from 'vue';
+import { exactType } from '../shared';
+
+const $src = useCssModule("$src");
+exactType($src, {} as Record<string, string> & {
+    foo: string;
+});
+
+const $import = useCssModule("$import");
+exactType($import, {} as Record<string, string> & {
+    foo: string;
+    bar: string;
+});
+</script>
+
+<style module="$src" src="./main.css"></style>
+
+<style module="$import">
+@import "./main.css";
+
+.bar { }
+</style>
