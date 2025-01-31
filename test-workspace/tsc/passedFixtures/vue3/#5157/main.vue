083d4605d2f896bd3e1efa07c0f28667eb825fc2
山吹色御守 <85992002+KazariEX@users.noreply.github.com>
2025-02-01 01:04:39 +0800

    fix(language-core): map interpolation error with multiple variables correctly (#5158)

---
+++ test-workspace/tsc/passedFixtures/vue3/#5157/main.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const foo = ref<{ bar: string } | undefined>();
+</script>
+
+<template>
+	<!-- @vue-expect-error -->
+	<div v-if="foo.bar || foo"></div> 
+</template>
