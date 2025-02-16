e36b3f0773f7ac22f1e0ba4be761aad700a714ef
KazariEX <1364035137@qq.com>
2025-02-16 16:14:19 +0800

    fix(language-core): handle edge case of default slot name mismatch

--- packages/language-core/lib/codegen/template/slotOutlet.ts
+++ packages/language-core/lib/codegen/template/slotOutlet.ts
@@ -15,6 +15,7 @@ export function* generateSlotOutlet(
 	node: CompilerDOM.SlotOutletNode
 ): Generator<Code> {
 	const startTagOffset = node.loc.start.offset + options.template.content.slice(node.loc.start.offset).indexOf(node.tag);
+	const startTagEndOffset = startTagOffset + node.tag.length;
 	const propsVar = ctx.getInternalVariable();
 	const nameProp = node.props.find(prop => {
 		if (prop.type === CompilerDOM.NodeTypes.ATTRIBUTE) {
@@ -30,7 +31,7 @@ export function* generateSlotOutlet(
 	});
 
 	if (options.hasDefineSlots) {
-		yield `__VLS_normalizeSlot(`;
+		yield `__VLS_asFunctionalSlot(`;
 		if (nameProp) {
 			let codes: Generator<Code> | Code[];
 			if (nameProp.type === CompilerDOM.NodeTypes.ATTRIBUTE && nameProp.value) {
@@ -78,16 +79,23 @@ export function* generateSlotOutlet(
 		}
 		else {
 			yield* wrapWith(
-				node.loc.start.offset,
-				node.loc.end.offset,
+				startTagOffset,
+				startTagEndOffset, 
 				ctx.codeFeatures.verification,
-				`${options.slotsAssignName ?? '__VLS_slots'}['default']`
+				`${options.slotsAssignName ?? '__VLS_slots'}[`,
+				...wrapWith(
+					startTagOffset,
+					startTagEndOffset, 
+					ctx.codeFeatures.verification,
+					`'default'`
+				),
+				`]`
 			);
 		}
-		yield `)?.(`;
+		yield `)(`;
 		yield* wrapWith(
 			startTagOffset,
-			startTagOffset + node.tag.length,
+			startTagEndOffset,
 			ctx.codeFeatures.verification,
 			`{${newLine}`,
 			...generateElementProps(
@@ -154,7 +162,7 @@ export function* generateSlotOutlet(
 		else {
 			ctx.slots.push({
 				name: 'default',
-				tagRange: [startTagOffset, startTagOffset + node.tag.length],
+				tagRange: [startTagOffset, startTagEndOffset],
 				nodeLoc: node.loc,
 				propsVar,
 			});
