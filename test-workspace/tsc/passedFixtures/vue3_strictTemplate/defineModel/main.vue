5b65895b72a66270d594090529c75e805b6ed468
KazariEX <1364035137@qq.com>
2025-03-02 17:07:01 +0800

    test: add and fix

---
+++ test-workspace/tsc/passedFixtures/vue3_strictTemplate/defineModel/main.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Comp from './comp.vue';
+
+let foo!: {
+    bar?: number;
+}
+</script>
+
+<template>
+    <!-- @vue-expect-error -->
+    <Comp v-model:foo="foo.bar" />
+
+    <!-- @vue-expect-error -->
+    <Comp v-model:bar="foo.bar" />
+    <Comp v-if="foo.bar" v-model:bar="foo.bar" />
+</template>
