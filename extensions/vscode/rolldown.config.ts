5baadf46e0bd954857d9690ea2dc14f3f97219cf
KazariEX <1364035137@qq.com>
2025-04-26 02:05:24 +0800

    refactor: use `dist/client.js` as entry

--- extensions/vscode/rolldown.config.ts
+++ extensions/vscode/rolldown.config.ts
@@ -1,17 +1,17 @@
-import fs from 'node:fs';
-import path from 'node:path';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 import { defineConfig } from 'rolldown';
 
 export default defineConfig({
 	input: {
-		'client': './out/nodeClientMain.js',
+		'client': './src/nodeClientMain.ts',
 		'server': './node_modules/@vue/language-server/node.ts',
-		'../node_modules/vue-language-core-pack/index': './node_modules/@vue/language-core/index.ts',
 		'../node_modules/vue-typescript-plugin-pack/index': './node_modules/@vue/typescript-plugin/index.ts',
 	},
 	output: {
 		format: 'cjs',
 		minify: process.argv.includes('--minify'),
+		sourcemap: !process.argv.includes('--minify')
 	},
 	define: {
 		'process.env.NODE_ENV': '"production"',
@@ -31,22 +31,11 @@ export default defineConfig({
 			}
 		},
 		{
-			name: 'resolve-share-module',
-			resolveId(source) {
-				if (source === '@vue/language-core') {
-					return {
-						id: 'vue-language-core-pack',
-						external: true,
-					};
-				}
-			},
-		},
-		{
 			name: 'typescript-plugin-development',
 			closeBundle() {
 				if (!process.argv.includes('--minify')) {
 					fs.writeFileSync(
-						path.resolve(import.meta.dirname, './node_modules/vue-typescript-plugin-pack/index.js'),
+						path.resolve(__dirname, './node_modules/vue-typescript-plugin-pack/index.js'),
 						'module.exports = require(\'@vue/typescript-plugin\');'
 					);
 				}
@@ -55,12 +44,12 @@ export default defineConfig({
 		{
 			name: 'schemas',
 			closeBundle() {
-				if (!fs.existsSync(path.resolve(import.meta.dirname, './dist/schemas'))) {
-					fs.mkdirSync(path.resolve(import.meta.dirname, './dist/schemas'));
+				if (!fs.existsSync(path.resolve(__dirname, './dist/schemas'))) {
+					fs.mkdirSync(path.resolve(__dirname, './dist/schemas'));
 				}
 				fs.cpSync(
-					path.resolve(import.meta.dirname, './node_modules/@vue/language-core/schemas/vue-tsconfig.schema.json'),
-					path.resolve(import.meta.dirname, './dist/schemas/vue-tsconfig.schema.json')
+					path.resolve(__dirname, './node_modules/@vue/language-core/schemas/vue-tsconfig.schema.json'),
+					path.resolve(__dirname, './dist/schemas/vue-tsconfig.schema.json')
 				);
 			}
 		},
--- extensions/vscode/src/nodeClientMain.ts
+++ extensions/vscode/src/nodeClientMain.ts
@@ -1,12 +1,13 @@
 import { createLabsInfo } from '@volar/vscode';
 import * as lsp from '@volar/vscode/node';
 import * as protocol from '@vue/language-server/protocol';
-import * as fs from 'node:fs';
 import { defineExtension, executeCommand, extensionContext, onDeactivate } from 'reactive-vscode';
 import * as vscode from 'vscode';
 import { config } from './config';
 import { activate as activateLanguageClient, deactivate as deactivateLanguageClient } from './languageClient';
 import { middleware } from './middleware';
+
+const fs = require('node:fs');
 
 export const { activate, deactivate } = defineExtension(async () => {
 	const volarLabs = createLabsInfo(protocol);
@@ -50,7 +51,7 @@ export const { activate, deactivate } = defineExtension(async () => {
 				}
 			}
 
-			let serverModule = vscode.Uri.joinPath(context.extensionUri, 'server.js');
+			let serverModule = vscode.Uri.joinPath(context.extensionUri, 'dist', 'server.js');
 
 			const serverOptions: lsp.ServerOptions = {
 				run: {
@@ -143,7 +144,6 @@ try {
 	// @ts-expect-error
 	fs.readFileSync = (...args) => {
 		if (args[0] === extensionJsPath) {
-			// @ts-expect-error
 			let text = readFileSync(...args) as string;
 
 			// patch readPlugins
@@ -196,7 +196,6 @@ try {
 
 			return text;
 		}
-		// @ts-expect-error
 		return readFileSync(...args);
 	};
 
