6cba5abc1a5438509d3fed7be299627e8ff32098
zhiyuanzmj <260480378@qq.com>
2025-06-07 16:20:05 +0800

    chore: update

--- packages/language-core/lib/plugins.ts
+++ packages/language-core/lib/plugins.ts
@@ -34,7 +34,7 @@ export function createPlugins(pluginContext: Parameters<VueLanguagePlugin>[0]) {
 		...pluginContext.vueCompilerOptions.plugins,
 	];
 
-	const pluginInstances = plugins.flat()
+	const pluginInstances = plugins
 		.flatMap(plugin => {
 			try {
 				const instance = plugin(pluginContext);
