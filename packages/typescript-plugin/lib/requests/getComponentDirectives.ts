058de13468f96346bec582b2277eeca5389ea648
山吹色御守 <85992002+KazariEX@users.noreply.github.com>
2025-06-11 01:22:30 +0800

    fix(typescript-plugin): filter out builtin directives starting with lowercase `v`

--- packages/typescript-plugin/lib/requests/getComponentDirectives.ts
+++ packages/typescript-plugin/lib/requests/getComponentDirectives.ts
@@ -2,6 +2,15 @@ import { VueVirtualCode } from '@vue/language-core';
 import type { RequestContext } from './types';
 import { getVariableType } from './utils';
 
+const builtInDirectives = new Set([
+	'vBind',
+	'vIf',
+	'vOn',
+	'vOnce',
+	'vShow',
+	'vSlot',
+]);
+
 export function getComponentDirectives(
 	this: RequestContext,
 	fileName: string
@@ -20,5 +29,5 @@ export function getComponentDirectives(
 	return directives.type.getProperties()
 		.map(({ name }) => name)
 		.filter(name => name.startsWith('v') && name.length >= 2 && name[1] === name[1].toUpperCase())
-		.filter(name => !['vBind', 'vIf', 'vOn', 'VOnce', 'vShow', 'VSlot'].includes(name));
+		.filter(name => !builtInDirectives.has(name));
 }
