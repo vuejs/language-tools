52e9771cfe43fc25d4ba49937c5af89adcd538ea
so1ve <i@mk1.io>
2025-06-15 14:36:10 +0800

    chore: use tabs to indent

--- test-workspace/tsc/passedFixtures/vue3/#5159/child.vue
+++ test-workspace/tsc/passedFixtures/vue3/#5159/child.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts" generic="T extends keyof HTMLElementTagNameMap = 'h1'">
 defineProps<
-  Partial<HTMLElementTagNameMap[T]> & {
-    as?: T;
-  }
+	Partial<HTMLElementTagNameMap[T]> & {
+		as?: T;
+	}
 >();
 </script>
