bddd22a9b1591bd01f2ab2ae77ee838b0207a392
山吹色御守 <85992002+KazariEX@users.noreply.github.com>
2025-06-26 03:22:55 +0800

    fix(vscode): restore `isAsync` to avoid continuous request stuck

--- extensions/vscode/index.ts
+++ extensions/vscode/index.ts
@@ -131,7 +131,7 @@ function launch(context: vscode.ExtensionContext) {
 			'typescript.tsserverRequest',
 			command,
 			args,
-			{ lowPriority: true, requireSemantic: true },
+			{ isAsync: true, lowPriority: true, requireSemantic: true },
 		);
 		client.sendNotification('tsserver/response', [seq, res?.body]);
 	});
