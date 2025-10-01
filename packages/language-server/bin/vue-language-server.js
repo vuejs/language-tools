16d58b8efc8094278513407df6f799163f39220d
Johnson Chu <johnsoncodehk@gmail.com>
2025-10-02 02:01:54 +0800

    move bin code

--- packages/language-server/bin/vue-language-server.js
+++ packages/language-server/bin/vue-language-server.js
@@ -1,19 +1,2 @@
 #!/usr/bin/env node
-
-if (process.argv.includes('--version')) {
-	console.log(require('../package.json').version);
-	return;
-}
-
-let ts;
-for (const arg of process.argv) {
-	if (arg.startsWith('--tsdk=')) {
-		const tsdk = arg.substring('--tsdk='.length);
-		const tsPath = require.resolve('./typescript.js', { paths: [tsdk] });
-		ts = require(tsPath);
-		break;
-	}
-}
-ts ??= require('typescript');
-
-require('../lib/server').startServer(ts);
+require('../index');
--- packages/language-server/index.ts
+++ packages/language-server/index.ts
@@ -1,4 +1,18 @@
-import * as ts from 'typescript';
 import { startServer } from './lib/server';
 
-startServer(ts);
+if (process.argv.includes('--version')) {
+	console.log(require('../package.json').version);
+}
+else {
+	let ts;
+	for (const arg of process.argv) {
+		if (arg.startsWith('--tsdk=')) {
+			const tsdk = arg.substring('--tsdk='.length);
+			const tsPath = require.resolve('./typescript.js', { paths: [tsdk] });
+			ts = require(tsPath);
+			break;
+		}
+	}
+	ts ??= require('typescript');
+	startServer(ts);
+}
