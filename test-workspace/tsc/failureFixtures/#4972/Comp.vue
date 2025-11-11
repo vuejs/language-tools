5b7567b4fc20a19a33e42e2364d7ee09a817af84
SerKo <serko.dev@gmail.com>
2025-11-12 02:22:18 +0800

    chore: add failure fixture for #4972

---
+++ test-workspace/tsc/failureFixtures/#4972/Comp.vue
@@ -0,0 +1,3 @@
+<script setup lang="ts">
+defineModel<string>({ required: true });
+</script>
---
+++ test-workspace/tsc/failureFixtures/#4972/main.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import GenericComp from './GenericComp.vue';
+import Comp from './Comp.vue';
+</script>
+
+<template>
+	<Comp />
+	<GenericComp />
+	<GenericComp whatever="''" />"
+</template>
