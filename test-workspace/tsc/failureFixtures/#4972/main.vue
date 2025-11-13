1d82efb2b06fa5c9ed0686cde230d4c44d70c3b9
SerKo <serko.dev@gmail.com>
2025-11-13 21:23:50 +0800

    chore: update fixture

--- test-workspace/tsc/failureFixtures/#4972/main.vue
+++ test-workspace/tsc/failureFixtures/#4972/main.vue
@@ -1,8 +1,11 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
 import GenericComp from './GenericComp.vue';
+const n = ref(0)
 </script>
 
 <template>
+	<GenericComp v-model="n" />
 	<GenericComp />
 	<GenericComp whatever="''" />"
 </template>
