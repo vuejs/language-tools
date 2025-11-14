584320a0f80b3b3c0915ac30e2fbbff8c0447fba
SerKo <serko.dev@gmail.com>
2025-11-15 02:02:50 +0800

    chore: add fixtures

---
+++ test-workspace/tsc/passedFixtures/vue-expect-error/Comp.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+defineProps<{
+    n: number
+}>()
+</script>
---
+++ test-workspace/tsc/passedFixtures/vue-expect-error/main.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import Comp from './Comp.vue';
+const n = ref(0)
+</script>
+
+<template>
+	<!-- component with error -->
+	<Comp :n="nx" />
+	<!-- @vue-expect-error -->
+	<Comp :n="nx" />
+
+	<!-- div without error -->
+	<div :data-n="n" />
+	<!-- @vue-expect-error | expect prompt Unused `@ts-expected-error` directive -->
+	<div :data-n="n" />
+
+	<!-- component without error -->
+	<Comp :n="n" />
+	<!-- @vue-expect-error | expect prompt Unused `@ts-expected-error` directive -->
+	<Comp :n="n" />
+</template>
