a8fd37c1bf5d9592fd286c0f8a173ea243f6d89e
SerKo <serko.dev@gmail.com>
2025-11-17 06:09:34 +0800

    refactor: simplify codegen

--- packages/language-core/lib/codegen/script/template.ts
+++ packages/language-core/lib/codegen/script/template.ts
@@ -53,11 +53,9 @@ function* generateTemplateCtx(
 		exps.push(`globalThis`);
 	}
 	if (options.sfc.script?.src || options.scriptRanges?.exportDefault) {
+		yield `type __VLS_SelfSetupReturn = NonNullable<Awaited<ReturnType<NonNullable<typeof __VLS_self['setup']>>>>${endOfLine}`
 		exps.push(`{} as InstanceType<__VLS_PickNotAny<typeof __VLS_self, new () => {}>>`);
-		const setupReturn = `NonNullable<Awaited<ReturnType<NonNullable<typeof __VLS_self['setup']>>>`;
-		exps.push(
-			`{} as ${`import('${options.vueCompilerOptions.lib}').ShallowUnwrapRef<${setupReturn}>`}>`,
-		);
+		exps.push(`{} as import('${options.vueCompilerOptions.lib}').ShallowUnwrapRef<__VLS_SelfSetupReturn>`);
 	}
 	else {
 		exps.push(`{} as import('${options.vueCompilerOptions.lib}').ComponentPublicInstance`);
