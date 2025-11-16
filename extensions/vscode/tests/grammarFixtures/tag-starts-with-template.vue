103e047a7ad8a57308dc6f38f22d47aaf102e33a
KazariEX <zinkawaii@qq.com>
2025-11-16 19:32:21 +0800

    refactor: merge two template tag patterns into one

--- extensions/vscode/tests/grammarFixtures/tag-starts-with-template.vue
+++ extensions/vscode/tests/grammarFixtures/tag-starts-with-template.vue
@@ -1,9 +1,17 @@
 <template>
+    <template>
+        <template></template>
+    </template>
+    <template></template>
+    <template> </template>
+    <template bar></template>
+    <template>{{ 0 }}</template>
+    <template />
+    <template/>
     <template-foo></template-foo>
-    <template:foo></template:foo>
+    <template-foo> </template-foo>
     <template-foo bar></template-foo>
     <template-foo>{{ 0 }}</template-foo>
-    <template.foo[0].bar></template.foo[0].bar>
     <template-foo />
     <template-foo/>
 </template>
