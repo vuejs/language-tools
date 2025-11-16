b77159b614965e29a8cb794f76972b26deeeccf4
KazariEX <zinkawaii@qq.com>
2025-11-16 20:31:18 +0800

    test: move

--- extensions/vscode/tests/grammarFixtures/tag-starts-with-template.vue
+++ extensions/vscode/tests/grammarFixtures/tag-starts-with-template.vue
@@ -1,17 +1,8 @@
 <template>
-    <template>
-        <template></template>
-    </template>
-    <template></template>
-    <template> </template>
-    <template bar></template>
-    <template>{{ 0 }}</template>
-    <template />
-    <template/>
-    <template-foo></template-foo>
-    <template-foo> </template-foo>
-    <template-foo bar></template-foo>
-    <template-foo>{{ 0 }}</template-foo>
-    <template-foo />
-    <template-foo/>
+	<template-foo></template-foo>
+	<template-foo> </template-foo>
+	<template-foo bar></template-foo>
+	<template-foo>{{ 0 }}</template-foo>
+	<template-foo />
+	<template-foo/>
 </template>
