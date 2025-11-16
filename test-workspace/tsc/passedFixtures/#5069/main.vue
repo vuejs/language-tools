5377ddab9da93852c2a1bfac425da319a9090b74
SerKo <serko.dev@gmail.com>
2025-11-17 06:15:52 +0800

    chore: add fixture

---
+++ test-workspace/tsc/passedFixtures/#5069/main.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+    setup: () => ({
+        msg: ref('hello'),
+        msg2: ref('world')
+    }),
+    expose: ['msg']
+});
+</script>
+
+<template>
+    <div>
+        {{ msg }}
+        {{ msg2 }}
+    </div>
+</template>
