5d841f4209fef36314f77e2cd057fafd57cce02a
SerKo <serko.dev@gmail.com>
2025-11-17 15:41:08 +0800

    fix(language-core): `defineModel` for generic component types (#5748)

--- packages/language-core/lib/codegen/script/scriptSetup.ts
+++ packages/language-core/lib/codegen/script/scriptSetup.ts
@@ -97,7 +97,8 @@ export function* generateScriptSetup(
 						+ ` & import('${options.vueCompilerOptions.lib}').AllowedComponentProps`
 						+ ` & import('${options.vueCompilerOptions.lib}').ComponentCustomProps`
 					: `globalThis.JSX.IntrinsicAttributes`
-			}${endOfLine}`
+			}${newLine}`
+			+ ` & (typeof globalThis extends { __VLS_CheckUnknownProps: infer P } ? P : {})${endOfLine}`
 			+ `	expose: (exposed: ${
 				scriptSetupRanges.defineExpose
 					? `import('${options.vueCompilerOptions.lib}').ShallowUnwrapRef<typeof __VLS_exposed>`
