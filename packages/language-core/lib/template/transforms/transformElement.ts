2195dce021c736d48e6f9a6379febcb47a650386
KazariEX <zinkawaii@qq.com>
2025-11-24 00:45:02 +0800

    fix: add component names in exit function

--- packages/language-core/lib/template/transforms/transformElement.ts
+++ packages/language-core/lib/template/transforms/transformElement.ts
@@ -1,7 +1,9 @@
 import { ElementTypes, type NodeTransform, NodeTypes } from '@vue/compiler-dom';
 
 export const transformElement: NodeTransform = (node, context) => {
-	if (node.type === NodeTypes.ELEMENT && node.tagType === ElementTypes.COMPONENT) {
-		context.components.add(node.tag);
-	}
+	return () => {
+		if (node.type === NodeTypes.ELEMENT && node.tagType === ElementTypes.COMPONENT) {
+			context.components.add(node.tag);
+		}
+	};
 };
