c9d5d7e8f02243193cddb7593a71813f12a6e475
SerKo <serko.dev@gmail.com>
2025-12-04 06:50:11 +0800

    test: add fixtures

---
+++ extensions/vscode/tests/grammarFixtures/leading-operator.vue
@@ -0,0 +1,45 @@
+<script>
+if (false
+< true
+) { }
+(
+<br />
+);
+</script>
+
+<script lang="js">
+if (false
+< true
+) {}
+(
+<br />
+);
+</script>
+
+<script lang="jsx">
+if (false
+< true
+) {}
+(
+<br />
+);
+</script>
+
+<script lang="ts">
+if (false
+< true
+) {}
+interface Foo
+<T> { value: T; }
+</script>
+
+<script lang="tsx">
+if (false
+< true
+) {}
+interface Foo
+<T> { value: T; }
+(
+<br />
+);
+</script>
