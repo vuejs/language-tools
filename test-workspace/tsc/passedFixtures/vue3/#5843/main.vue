388dc6c91c0a47eefad022115a99b39ea9b92444
SerKo <serko.dev@gmail.com>
2025-12-10 16:14:23 +0800

    test: add reproduction fixture

---
+++ test-workspace/tsc/passedFixtures/vue3/#5843/main.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+declare const Comp: (props: {}, ctx: {
+    slots: {
+        counter: (props: { value: number }) => void;
+    }
+}) => {};
+</script>
+
+<template>
+    <Comp>
+        <template v-if="false" #counter="{ value: counterValue }">
+            {{ counterValue }}
+        </template>
+        <template v-else-if="true" #counter="{ value: counterValue }">
+            {{ counterValue }}
+        </template>
+    </Comp>
+</template>
