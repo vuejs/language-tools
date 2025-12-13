3586b07cd5debb78debfb330ecb8831fbfe54ef8
SerKo <serko.dev@gmail.com>
2025-12-14 03:49:33 +0800

    test(vscode): add test for embedded grammars (#5861)

---
+++ extensions/vscode/tests/embeddedGrammarFixtures/basic.vue
@@ -0,0 +1,21 @@
+<script setup>
+const a = 0;
+</script>
+
+<script setup lang="ts">
+type a = 0;
+</script>
+
+<template>
+    <div />
+</template>
+
+<style scoped>
+:root {
+    color: red;
+}
+</style>
+
+<style scoped lang="scss">
+$color: red;
+</style>
