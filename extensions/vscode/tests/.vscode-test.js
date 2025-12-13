c9ee803f04d1430bfc12e8c7135569e3583427c9
zcf0508 <zcf0508@live.com>
2025-12-13 18:21:07 +0800

    chore: format

--- extensions/vscode/tests/.vscode-test.js
+++ extensions/vscode/tests/.vscode-test.js
@@ -2,16 +2,16 @@ const path = require('node:path');
 const { defineConfig } = require('@vscode/test-cli');
 
 module.exports = defineConfig({
-  extensionDevelopmentPath: path.join(__dirname, '../'),
-  workspaceFolder: path.join(__dirname, './workspace'),
+	extensionDevelopmentPath: path.join(__dirname, '../'),
+	workspaceFolder: path.join(__dirname, './workspace'),
 
-  // Use a dedicated out dir for test JS files
-  files: ['out/**/*.test.js'],
+	// Use a dedicated out dir for test JS files
+	files: ['out/**/*.test.js'],
 
-  // Mocha options
-  mocha: {
-    ui: 'tdd',
-    timeout: 0,
-    color: true
-  }
+	// Mocha options
+	mocha: {
+		ui: 'tdd',
+		timeout: 0,
+		color: true,
+	},
 });
