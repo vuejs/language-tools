11f2378b8e10b75fdf87c5c527e75f21d50cca71
SerKo <serko.dev@gmail.com>
2025-12-22 10:00:29 +0800

    test(language-core): simplify test fixture for #5896

--- test-workspace/tsc/#5895/main.vue
+++ test-workspace/tsc/#5895/main.vue
@@ -1,20 +1,13 @@
-<script setup lang="ts" generic="TValue extends string, TGrid extends readonly (readonly TValue[])[]">
-import { type MaybeRef, unref } from 'vue';
-
-const getGridKey = (rowIndex: number, columnIndex: number) =>
-  `${rowIndex}|${columnIndex}`;
-
-class Grid<TValue, TGrid extends readonly (readonly TValue[])[]> {
-  grid!: MaybeRef<TGrid>;
-}
-
-const { grid } = defineProps<{
-  grid: Grid<TValue, TGrid>;
-}>();
+<script setup lang="ts" generic="T extends string, TGrid extends readonly (readonly T[])[]">
+import { exactType } from '../shared';
 </script>
 
 <template>
-  <template v-for="(row, rowIndex) of unref(grid.grid)" :key="rowIndex">
-    <div v-for="(_text, columnIndex) of row" :key="getGridKey(rowIndex, columnIndex)" />
+  <template v-for="(row, rowIndex) of ({} as TGrid)">
+    {{ exactType(rowIndex, {} as number) }}
+    <template v-for="(col, columnIndex) of row">
+			{{ exactType(col, {} as T) }}
+			{{ exactType(columnIndex, {} as number) }}
+    </template>  
   </template>
 </template>
