ef226468a6afd29b46f8697a346198523a12d642
SerKo <serko.dev@gmail.com>
2026-01-09 01:12:48 +0800

    test: add test unit

---
+++ test-workspace/tsc/#5927/main.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+enum EnumA {
+  Value1 = 'v1',
+  Value2 = 'v2',
+}
+
+const definitions: {
+  [key in EnumA]: string;
+} = {
+  [EnumA.Value1]: 'test1',
+  [EnumA.Value2]: 'test2',
+};
+
+const test = (key: EnumA, value: string) => `${key}: ${value}`;
+</script>
+
+<template>
+  <template v-for="(row, rowIndex) of definitions" :key="rowIndex">
+    {{ test(rowIndex, row) }}
+  </template>
+</template>
