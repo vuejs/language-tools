661bc17538f353ad703e6697648ca1dcff0e97b8
KazariEX <zinkawaii@qq.com>
2026-01-23 10:39:12 +0800

    chore: indent

--- test-workspace/tsc/#5927/main.vue
+++ test-workspace/tsc/#5927/main.vue
@@ -2,22 +2,22 @@
 import { exactType } from '../shared';
 
 enum EnumA {
-  Foo = 'foo',
-  Bar = 'bar',
+	Foo = 'foo',
+	Bar = 'bar',
 }
 
 const enumKeyRecord: {
-  [key in EnumA]: string;
+	[key in EnumA]: string;
 } = {
-  [EnumA.Foo]: 'a',
-  [EnumA.Bar]: 'b',
+	[EnumA.Foo]: 'a',
+	[EnumA.Bar]: 'b',
 };
 </script>
 
 <template>
-  <template v-for="(row, rowIndex, index) of enumKeyRecord" :key="rowIndex">
-    {{ exactType(row, {} as string) }}
-    {{ exactType(rowIndex, {} as EnumA) }}
-    {{ exactType(index, {} as number) }}
-  </template>
+	<template v-for="(row, rowIndex, index) of enumKeyRecord" :key="rowIndex">
+		{{ exactType(row, {} as string) }}
+		{{ exactType(rowIndex, {} as EnumA) }}
+		{{ exactType(index, {} as number) }}
+	</template>
 </template>
