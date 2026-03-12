ef4dccb5601010e3078bdbb83867be25342ab889
山吹色御守 <85992002+KazariEX@users.noreply.github.com>
2026-03-11 21:45:53 -0700

    chore: format

--- test-workspace/tsc/#5986/main.vue
+++ test-workspace/tsc/#5986/main.vue
@@ -1,17 +1,17 @@
-<template>
-  <div v-for="item in customArray">
-    {{ exactType(item, {} as string) }}
-  </div>
-</template>
-
 <script setup lang="ts">
 import { exactType } from "../shared";
 
 class CustomArray extends Array<string> {
-  override push(...items: (string | number)[]) {
-    return super.push(...items.map((item) => String(item)));
-  }
+	override push(...items: (string | number)[]) {
+		return super.push(...items.map((item) => String(item)));
+	}
 }
 
 const customArray = new CustomArray();
 </script>
+
+<template>
+	<div v-for="item in customArray">
+		{{ exactType(item, {} as string) }}
+	</div>
+</template>
