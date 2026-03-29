4079f4b26777537d804690aa8f67b5fbcfe9c740
jak-ch-ll <jak-ch-ll@mailbox.org>
2026-03-29 14:27:22 +0200

    feat(vscode): ✨ add settings enum for `vue.hover.rich`

--- packages/language-service/lib/plugins/vue-template.ts
+++ packages/language-service/lib/plugins/vue-template.ts
@@ -409,7 +409,7 @@ export function create(
 					const templateAst = info.root.ir.template?.ast;
 					const enabledRichMessage = await context.env.getConfiguration?.('vue.hover.rich');
 
-					if (!templateAst || !enabledRichMessage || (htmlHover && hasContents(htmlHover.contents))) {
+					if (!templateAst || enabledRichMessage === 'off' || (htmlHover && hasContents(htmlHover.contents))) {
 						return htmlHover;
 					}
 
