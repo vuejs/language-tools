4760dd01c99b4f47fd57ee3b18362e9ece50bb69
jak-ch-ll <jak-ch-ll@mailbox.org>
2026-03-29 14:27:22 +0200

    test(test-workspace): ✨ add hover scenario to test playground

---
+++ test-workspace/tsc/hover/main.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import child from './child.vue'
+</script>
+
+<template>
+  <div>
+    <child
+      required-model=""
+      required-prop=""
+    />
+  </div>
+</template>
