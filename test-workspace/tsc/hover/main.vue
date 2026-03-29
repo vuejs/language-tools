316e7e00e21a32ac1a377750d0aa2cb3e444fc83
jak-ch-ll <jak-ch-ll@mailbox.org>
2026-03-29 14:27:23 +0200

    test(test-workspace): ✨ add minimal hover scenario

--- test-workspace/tsc/hover/main.vue
+++ test-workspace/tsc/hover/main.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import child from './child.vue'
+import minimal from './minimal.vue'
 </script>
 
 <template>
   <div>
     <child
       required-model=""
+      required-prop=""
+    />
+    <minimal 
+      another-model=""
       required-prop=""
     />
   </div>
