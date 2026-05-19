846157a4965e7e4f314fa3f3ae0cc21283b5bc60
山吹色御守 <85992002+KazariEX@users.noreply.github.com>
2026-05-18 22:03:33 -0700

    fix(language-core): avoid extraneous children error for conditional slots (#6056)

--- packages/language-core/lib/template/transforms/transformElement.ts
+++ packages/language-core/lib/template/transforms/transformElement.ts
@@ -72,13 +72,21 @@ export const transformElement: NodeTransform = (node, context) => {
 		}
 
 		if (isComponent) {
-			let hasTemplateSlots = false;
 			let hasNamedDefaultSlot = false;
 			const implicitDefaultChildren: TemplateChildNode[] = [];
 			const seenSlotNames = new Set<string>();
 			const onComponentSlot = findDir(node, 'slot', true);
 
-			for (const child of node.children) {
+			for (let child of node.children) {
+				// <template v-for> -> for > template
+				if (child.type === NodeTypes.FOR) {
+					child = child.children[0]!;
+				}
+				// <template v-if> -> if > branch > template
+				else if (child.type === NodeTypes.IF) {
+					child = child.branches[0]!.children[0]!;
+				}
+
 				let slotDir: DirectiveNode | undefined;
 				if (!isTemplateNode(child) || !(slotDir = findDir(child, 'slot', true))) {
 					if (child.type !== NodeTypes.COMMENT) {
@@ -94,11 +102,6 @@ export const transformElement: NodeTransform = (node, context) => {
 					break;
 				}
 
-				if (findDir(child, /^(?:if|else-if|else|for)$/, true)) {
-					continue;
-				}
-
-				hasTemplateSlots = true;
 				const staticSlotName = slotDir.arg
 					? isStaticExp(slotDir.arg)
 						? slotDir.arg.content
@@ -120,7 +123,7 @@ export const transformElement: NodeTransform = (node, context) => {
 			}
 
 			if (
-				hasTemplateSlots && hasNamedDefaultSlot
+				hasNamedDefaultSlot
 				&& implicitDefaultChildren.some(node => node.type !== NodeTypes.TEXT || !!node.content.trim())
 			) {
 				context.onError(
