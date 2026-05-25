d35b1ba1149fb1376a82ce7913e3f8d25475f585
Konstantinos Kesidis <kesidiskostas@gmail.com>
2026-05-25 11:01:46 +0300

    feat(component-meta): parse slots in v-for

--- test-workspace/component-meta/reference-type-slots/component.vue
+++ test-workspace/component-meta/reference-type-slots/component.vue
@@ -3,4 +3,7 @@
 	<slot :num="123"></slot>
 	<slot name="named-slot" str="str"></slot>
 	<slot name="vbind" v-bind="{ num: 123, str: 'str' }"></slot>
+	<template v-for="key in ['str', 123]" :key="key">
+		<slot :name="`loop-${key}`" v-bind="{str: 'str', key}"></slot>
+	</template>
 </template>
