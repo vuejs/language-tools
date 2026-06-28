5b450ceb39d3e4c3af532ce84d8735317982c705
KazariEX <zinkawaii@qq.com>
2026-06-28 17:58:16 +0800

    fix(language-core): match upstream CSS `v-bind` parsing behavior

--- packages/language-core/lib/plugins/vue-style-css.ts
+++ packages/language-core/lib/plugins/vue-style-css.ts
@@ -17,46 +17,80 @@ const plugin: VueLanguagePlugin = () => {
 export default plugin;
 
 const cssImportReg = /(?<=@import\s+url\()(["']?).*?\1(?=\))|(?<=@import\b\s*)(["']).*?\2/g;
-const cssBindingReg = /\bv-bind\(\s*(?:'([^']+)'|"([^"]+)"|([a-z_]\w*))\s*\)/gi;
-const cssClassNameReg = /(?=(\.[a-z_][-\w]*)[\s.,+~>:#)[{])/gi;
+const cssBindingReg = /\bv-bind\s*\(/g;
+const cssClassNameReg = /\.[a-z_][-\w]*(?=[\s.,+~>:#)[{])/gi;
 const commentReg = /(?<=\/\*)[\s\S]*?(?=\*\/)|(?<=\/\/)[\s\S]*?(?=\n)/g;
 const fragmentReg = /(?<={)[^{]*(?=(?<!\\);)/g;
 
 function* parseCssImports(css: string) {
-	const matches = css.matchAll(cssImportReg);
-	for (const match of matches) {
-		let text = match[0];
-		let offset = match.index;
-		if (text.startsWith("'") || text.startsWith('"')) {
-			text = text.slice(1, -1);
-			offset += 1;
-		}
-		if (text) {
-			yield { text, offset };
-		}
+	for (const match of css.matchAll(cssImportReg)) {
+		yield trimQuotes(match[0], match.index);
 	}
 }
 
 function* parseCssBindings(css: string) {
 	css = fillBlank(css, commentReg);
-	const matchs = css.matchAll(cssBindingReg);
-	for (const match of matchs) {
-		const matchText = match.slice(1).find(t => t);
-		if (matchText) {
-			const offset = match.index + css.slice(match.index).indexOf(matchText);
-			yield { offset, text: matchText };
+	for (const match of css.matchAll(cssBindingReg)) {
+		const start = match.index + match[0].length;
+		const end = lexBinding(css, start);
+		if (end !== null) {
+			yield trimQuotes(css.slice(start, end), start, false);
 		}
 	}
 }
 
+enum LexerState {
+	inParens,
+	inSingleQuoteString,
+	inDoubleQuoteString,
+}
+
+// https://github.com/vuejs/core/blob/c0606e91798c8dca4f33d101e1dd836d672592c1/packages/compiler-sfc/src/style/cssVars.ts#L93
+function lexBinding(content: string, start: number) {
+	let state: LexerState = LexerState.inParens;
+	let parenDepth = 0;
+
+	for (let i = start; i < content.length; i++) {
+		const char = content.charAt(i);
+		switch (state) {
+			case LexerState.inParens:
+				if (char === `'`) {
+					state = LexerState.inSingleQuoteString;
+				}
+				else if (char === `"`) {
+					state = LexerState.inDoubleQuoteString;
+				}
+				else if (char === `(`) {
+					parenDepth++;
+				}
+				else if (char === `)`) {
+					if (parenDepth > 0) {
+						parenDepth--;
+					}
+					else {
+						return i;
+					}
+				}
+				break;
+			case LexerState.inSingleQuoteString:
+				if (char === `'`) {
+					state = LexerState.inParens;
+				}
+				break;
+			case LexerState.inDoubleQuoteString:
+				if (char === `"`) {
+					state = LexerState.inParens;
+				}
+				break;
+		}
+	}
+	return null;
+}
+
 function* parseCssClassNames(css: string) {
 	css = fillBlank(css, commentReg, fragmentReg);
-	const matches = css.matchAll(cssClassNameReg);
-	for (const match of matches) {
-		const matchText = match[1];
-		if (matchText) {
-			yield { offset: match.index, text: matchText };
-		}
+	for (const match of css.matchAll(cssClassNameReg)) {
+		yield { text: match[0], offset: match.index };
 	}
 }
 
@@ -65,4 +99,31 @@ function fillBlank(css: string, ...regs: RegExp[]) {
 		css = css.replace(reg, match => ' '.repeat(match.length));
 	}
 	return css;
+}
+
+function trimQuotes(text: string, offset: number, trim = true) {
+	let start = 0;
+	let end = text.length;
+
+	if (trim || text.includes('"') || text.includes("'")) {
+		while (start < text.length && !text[start]?.trim()) {
+			start++;
+		}
+		while (end >= 0 && !text[end - 1]?.trim()) {
+			end--;
+		}
+	}
+
+	if (
+		text[start] === '"' && text[end - 1] === '"'
+		|| text[start] === "'" && text[end - 1] === "'"
+	) {
+		start++;
+		end--;
+	}
+
+	return {
+		text: text.slice(start, end),
+		offset: offset + start,
+	};
 }
