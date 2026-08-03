8e78c5dc3017a9bae511123d49ec92d5719668f5
SerKo <serko.dev@gmail.com>
2026-08-03 22:08:08 +0800

    fix(language-core): resolve `v-for` item type over generic sources (#6141)

---
+++ test-workspace/tsc/#6040/main.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts" generic="TGroup extends {}, TGroups extends Record<string, TGroup> = Record<string, TGroup>">
+import { exactType } from "../shared";
+
+const props = defineProps<{
+	groups: TGroups;
+}>();
+
+for (const [type, group] of Object.entries(props.groups)) {
+	exactType(group, {} as TGroup);
+	exactType(type, {} as string);
+}
+</script>
+
+<template>
+	<template v-for="(group, type) of groups" :key="type">
+		{{ exactType(group, {} as TGroup) }}
+		{{ exactType(type, {} as string) }}
+	</template>
+</template>
