9f5d47f3af705466106b41180dfd28731a353a95
SerKo <serko.dev@gmail.com>
2026-08-03 17:50:50 +0800

    test: update `v-for` generic test

--- test-workspace/tsc/v-for/generic.vue
+++ test-workspace/tsc/v-for/generic.vue
@@ -3,8 +3,16 @@
 		{{ exactType(val, {} as string | boolean) }}
 		{{ exactType(key, {} as string | number) }}
 	</div>
+	<div v-for="(val, key) in ({} as TArr)">
+		{{ exactType(val, {} as number) }}
+		{{ exactType(key, {} as number) }}
+	</div>
+	<div v-for="(val, key) in ({} as TIter)">
+		{{ exactType(val, {} as string) }}
+		{{ exactType(key, {} as number) }}
+	</div>
 </template>
 
-<script lang="ts" setup generic="T extends Record<string, string> | boolean[]">
+<script lang="ts" setup generic="T extends Record<string, string> | boolean[], TArr extends number[], TIter extends Iterable<string>">
 import { exactType } from '../shared';
 </script>
