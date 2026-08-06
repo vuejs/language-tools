4d31832552995ec6943fb1aa56182840634f7f69
SerKo <serko.dev@gmail.com>
2026-08-06 18:51:15 +0800

    fix(language-core): pass vue ts plugin path via env

--- extensions/vscode/scripts/write-plugins.ts
+++ extensions/vscode/scripts/write-plugins.ts
@@ -6,14 +6,18 @@ const isDev = process.argv.includes('--dev');
 write(
 	'typescript-plugin',
 	isDev ? '@vue/typescript-plugin' : '../../dist/typescript-plugin.js',
+	'VUE_TYPESCRIPT_PLUGIN_PATH',
 );
 write(
 	'reactivity-analysis-plugin',
 	isDev ? '../../out/reactivityAnalysisPlugin.js' : '../../dist/reactivity-analysis-plugin.js',
 );
 
-function write(name: string, specifier: string) {
+function write(name: string, specifier: string, pathEnvName?: string) {
 	const dir = path.join(import.meta.dirname, `../node_modules/vue-${name}-pack`);
 	fs.mkdirSync(dir, { recursive: true });
-	fs.writeFileSync(path.join(dir, `index.js`), `module.exports = require('${specifier}');\n`);
+	const moduleExports = pathEnvName
+		? `process.env.${pathEnvName} ? require(process.env.${pathEnvName}) : require('${specifier}')`
+		: `require('${specifier}')`;
+	fs.writeFileSync(path.join(dir, `index.js`), `module.exports = ${moduleExports};\n`);
 }
