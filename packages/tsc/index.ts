9b7c1708575921a9fa0c6454e20de6fc541597e2
Amulet Kerria <85992002+KazariEX@users.noreply.github.com>
2026-08-17 05:06:57 -0700

    Apply suggestion from @KazariEX

--- packages/tsc/index.ts
+++ packages/tsc/index.ts
@@ -3,7 +3,7 @@ import * as core from '@vue/language-core';
 import * as path from 'node:path';
 
 const windowsPathRE = /\\/g;
-const retryToken = new Error('vue-tsc: extensions changed, retrying');
+const retryToken = new Error('[Vue] Extensions changed');
 
 export function run(tscPath?: string) {
 	const runExtensions = new Set(['vue']);
