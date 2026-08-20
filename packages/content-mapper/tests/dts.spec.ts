92c5a084fae11bd8790162fadc6a4b3ad704f106
Johnson Chu <johnsoncodehk@gmail.com>
2026-08-21 04:12:44 +0800

    test(content-mapper): converge corpus/fixture tests on vitest + installed tsc

---
+++ packages/content-mapper/tests/dts.spec.ts
@@ -0,0 +1,50 @@
+import * as fs from 'node:fs';
+import * as os from 'node:os';
+import * as path from 'node:path';
+import { expect, test } from 'vitest';
+import { repositoryRoot, runTsc } from './utils';
+
+test('content mapper declaration emit', () => {
+	const outDir = fs.mkdtempSync(path.join(os.tmpdir(), 'vue-content-mapper-dts-'));
+	try {
+		const result = runTsc([
+			'-p',
+			path.join(repositoryRoot, 'test-workspace/component-meta/tsconfig.json'),
+			'--runExternalCode',
+			'--pretty',
+			'false',
+			'--emitDeclarationOnly',
+			'--noEmit',
+			'false',
+			'--outDir',
+			outDir,
+		]);
+		expect(result.status, result.stdout + result.stderr).toBe(0);
+
+		const outputs = readFilesRecursive(outDir)
+			.map(file => path.relative(outDir, file).replaceAll('\\', '/'))
+			.sort();
+		expect(outputs).toMatchSnapshot('declaration output files');
+
+		for (const file of outputs) {
+			expect(fs.readFileSync(path.join(outDir, file), 'utf8')).toMatchSnapshot(file);
+		}
+	}
+	finally {
+		fs.rmSync(outDir, { recursive: true, force: true });
+	}
+});
+
+function readFilesRecursive(dir: string): string[] {
+	const result: string[] = [];
+	for (const entry of fs.readdirSync(dir, { withFileTypes: true })) {
+		const filepath = path.join(dir, entry.name);
+		if (entry.isDirectory()) {
+			result.push(...readFilesRecursive(filepath));
+		}
+		else {
+			result.push(filepath);
+		}
+	}
+	return result;
+}
---
+++ packages/content-mapper/tests/utils.ts
@@ -0,0 +1,28 @@
+import { spawnSync } from 'node:child_process';
+import * as path from 'node:path';
+
+export const repositoryRoot = path.resolve(__dirname, '../../..');
+
+const tscScript = path.join(repositoryRoot, 'node_modules/typescript/bin/tsc');
+
+export function runTsc(args: string[]) {
+	return spawnSync(process.execPath, [tscScript, ...args], {
+		cwd: repositoryRoot,
+		encoding: 'utf8',
+		env: {
+			...process.env,
+			VUE_CONTENT_MAPPER_WORKERS: '2',
+		},
+	});
+}
+
+export function normalizeCompilerOutput(output: { stdout: string; stderr: string }) {
+	return (output.stdout + output.stderr)
+		.replace(/^.*TNB ACTIVE.*\n/gm, '')
+		.replace(/\r\n/g, '\n')
+		.split('\n')
+		.map(line => line.trimEnd())
+		.filter(Boolean)
+		.sort()
+		.join('\n');
+}
