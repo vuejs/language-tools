3ee611d5696462868813199c5930740a946b4786
Johnson Chu <johnsoncodehk@gmail.com>
2026-08-22 20:16:37 +0800

    test(language-server): guard against cutting global leading char

---
+++ packages/language-server/tests/hover.spec.ts
@@ -0,0 +1,31 @@
+import { expect, test } from 'vitest';
+import { URI } from 'vscode-uri';
+import { getLanguageServer, testWorkspacePath } from './server';
+
+test('global at leading code is not cut', async () => {
+	const content = `<template>
+	{{Math.max(foo)}}
+</template>
+
+<script setup lang="ts">
+const foo = 1;
+</script>`;
+	const offset = content.indexOf('Math');
+
+	const server = await getLanguageServer();
+	const document = await server.open(URI.file(`${testWorkspacePath}/tsconfigProject/fixture.vue`).toString(), 'vue', content);
+
+	const res = await server.tsserver.message({
+		seq: server.nextSeq(),
+		command: 'quickinfo',
+		arguments: {
+			file: URI.parse(document.uri).fsPath,
+			position: offset,
+		},
+	});
+
+	await server.close(document.uri);
+
+	expect(res.success).toBe(true);
+	expect(res.body?.displayString).toBe('var Math: Math');
+});
