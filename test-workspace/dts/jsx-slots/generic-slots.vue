3167acfd69b98d1442e9cd21df8ba3c13f054451
SerKo <serko.dev@gmail.com>
2026-08-31 22:58:03 +0800

    test(tsc): assert emitted declarations are self-contained and add jsxSlots test (#6196)

---
+++ test-workspace/dts/jsx-slots/generic-slots.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts" generic="T">
+defineProps<{
+	items: T[];
+}>();
+
+defineSlots<{
+	default?: (props: { item: T }) => any;
+}>();
+</script>
+
+<template>
+	<slot v-for="item of items" :item="item" />
+</template>
