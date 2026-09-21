084f30f6b764d4da97d4bc3be9ff3020d74e42d5
Xia Chao <shapirolutts@gmail.com>
2026-09-20 22:43:15 -0700

    fix(language-core): keep union props when using withDefaults (#6180) (#6183)

---
+++ test-workspace/tsc/#6180/main.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { exactType } from '../shared';
+import Button from './Button.vue';
+
+type Props = InstanceType<typeof Button>['$props'];
+
+// #6180: withDefaults + discriminated union must not collapse to Record<string, any>
+exactType({} as string extends keyof Props ? true : false, false);
+</script>
+
+<template>
+	<Button as="a" href="/" />
+</template>
