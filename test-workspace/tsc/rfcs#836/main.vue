f9671a499305fe0cb55ac4e36f3f5fd70539984f
KazariEX <zinkawaii@qq.com>
2026-09-22 18:06:49 +0800

    chore: lint

--- test-workspace/tsc/rfcs#836/main.vue
+++ test-workspace/tsc/rfcs#836/main.vue
@@ -1,13 +1,13 @@
 <script lang="ts" setup>
 import { exactType } from '../shared';
 
-	defineProps<{
-		foo: string;
-	}>();
+defineProps<{
+	foo: string;
+}>();
 
-	defineModel("bar", {
-		default(props) {
-			exactType(props.foo, {} as string);
-		}
-	});
+defineModel("bar", {
+	default(props) {
+		exactType(props.foo, {} as string);
+	}
+});
 </script>
